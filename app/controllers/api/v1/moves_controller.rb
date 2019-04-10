38f189af4554591211ddcff9b7549d8a3e57f700
Steve Hook <steve.hook@gmail.com>
2019-04-10 16:47:33 +0100

    Factor out query logic into PORO from controller

--- app/controllers/api/v1/moves_controller.rb
+++ app/controllers/api/v1/moves_controller.rb
@@ -4,28 +4,10 @@ module Api
   module V1
     class MovesController < ApplicationController
       def index
-        render json: scope(filter_params)
+        render json: MoveFinder.new(filter_params).scope
       end
 
       private
-
-      # TODO: Refactor into a separate class
-      def scope(filter_params)
-        filters(Move, filter_params)
-      end
-
-      def filters(scope, filter_params)
-        scope = scope.where(filter_params.slice(:from_location_id, :to_location_id, :status))
-        scope = scope.where('date >= ?', filter_params[:date_from]) if filter_params.key?(:date_from)
-        scope = scope.where('date <= ?', filter_params[:date_to]) if filter_params.key?(:date_to)
-        if filter_params.key?(:location_type)
-          scope =
-            scope
-            .joins(:to_location)
-            .where(locations: { location_type: filter_params[:location_type] })
-        end
-        scope
-      end
 
       def filter_params
         params.fetch(:filter, {}).permit(
---
+++ app/services/move_finder.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class MoveFinder
+  attr_accessor :filter_params
+
+  def initialize(filter_params)
+    self.filter_params = filter_params
+  end
+
+  def scope
+    filters(Move)
+  end
+
+  private
+
+  def filters(scope)
+    scope = scope.where(filter_params.slice(:from_location_id, :to_location_id, :status))
+    scope = scope.where('date >= ?', filter_params[:date_from]) if filter_params.key?(:date_from)
+    scope = scope.where('date <= ?', filter_params[:date_to]) if filter_params.key?(:date_to)
+    if filter_params.key?(:location_type)
+      scope =
+        scope
+        .joins(:to_location)
+        .where(locations: { location_type: filter_params[:location_type] })
+    end
+    scope
+  end
+end
