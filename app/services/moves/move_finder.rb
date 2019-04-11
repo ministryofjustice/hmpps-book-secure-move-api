9ffc5559b7ea9645f86ef15decaf17e064eb3165
Steve Hook <steve.hook@gmail.com>
2019-04-11 14:02:19 +0100

    Appease Rubocop

--- app/services/moves/move_finder.rb
+++ app/services/moves/move_finder.rb
@@ -16,15 +16,23 @@ module Moves
 
     def apply_filters(scope)
       scope = scope.where(filter_params.slice(:from_location_id, :to_location_id, :status))
+      scope = apply_date_range_filters(scope)
+      scope = apply_location_type_filters(scope)
+      scope
+    end
+
+    def apply_date_range_filters(scope)
       scope = scope.where('date >= ?', filter_params[:date_from]) if filter_params.key?(:date_from)
       scope = scope.where('date <= ?', filter_params[:date_to]) if filter_params.key?(:date_to)
-      if filter_params.key?(:location_type)
-        scope =
-          scope
-          .joins(:to_location)
-          .where(locations: { location_type: filter_params[:location_type] })
-      end
       scope
+    end
+
+    def apply_location_type_filters(scope)
+      return scope unless filter_params.key?(:location_type)
+
+      scope
+        .joins(:to_location)
+        .where(locations: { location_type: filter_params[:location_type] })
     end
   end
 end
