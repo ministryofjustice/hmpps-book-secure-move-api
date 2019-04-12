a36d36e123ee3512ad236434e2cac406b6d18e65
Steve Hook <steve.hook@gmail.com>
2019-04-12 11:22:21 +0100

    Additional properties for MoveSerializer

--- app/serializers/move_serializer.rb
+++ app/serializers/move_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MoveSerializer < ActiveModel::Serializer
-  attributes :id, :type, :status, :updated_at
+  attributes :id, :type, :status, :updated_at, :time_due, :date
 
   def type
     object.move_type
