ad77834b589ec8ee65227a07daaa7a17d443c7f3
Steve Hook <steve.hook@gmail.com>
2019-05-17 07:16:21 +0100

    Corrections to the schema
    Required properties for reference types were wrong
    Use `anyOf` rather than `oneOf` for included objects

--- spec/requests/api/v1/religions_controller_spec.rb
+++ spec/requests/api/v1/religions_controller_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe Api::V1::ReligionsController do
         it 'returns a valid 200 JSON response with move data' do
           pending 'not implemented yet'
           get '/api/v1/reference/religions', headers: headers
-          expect(JSON::Validator.validate!(schema, response_json, strict: true, fragment: '#/200')).to be true
+          expect(JSON::Validator.validate!(schema, response_json, fragment: '#/200')).to be true
         end
       end
 
@@ -72,7 +72,7 @@ RSpec.describe Api::V1::ReligionsController do
         it 'returns a valid 415 JSON response' do
           pending 'not implemented yet'
           get '/api/v1/reference/religions', headers: headers
-          expect(JSON::Validator.validate!(schema, response_json, strict: true, fragment: '#/415')).to be true
+          expect(JSON::Validator.validate!(schema, response_json, fragment: '#/415')).to be true
         end
       end
     end
