af5ac26742c8f28ae5ee730de9824afaa8bc7c84
Steve Hook <steve.hook@gmail.com>
2019-05-21 08:51:58 +0100

    Move reference API controllers to their own directory

---
+++ app/controllers/api/v1/reference/ethnicities_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Reference
+      class EthnicitiesController < ApiController
+        def index; end
+      end
+    end
+  end
+end
