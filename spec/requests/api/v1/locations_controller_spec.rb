3fcd52f0edd01eb5e5b3330f4e7cdea0dd45fe1a
Steve Hook <steve.hook@gmail.com>
2019-05-22 09:45:14 +0100

    Remove `pending` from request specs

--- spec/requests/api/v1/locations_controller_spec.rb
+++ spec/requests/api/v1/locations_controller_spec.rb
@@ -24,19 +24,16 @@ RSpec.describe Api::V1::LocationsController do
 
     context 'with the correct CONTENT_TYPE header' do
       it 'returns a success code' do
-        pending 'not implemented yet'
         get '/api/v1/reference/locations', headers: headers
         expect(response).to be_successful
       end
 
       it 'returns the correct data' do
-        pending 'not implemented yet'
         get '/api/v1/reference/locations', headers: headers
         expect(JSON.parse(response.body)).to include_json(data: expected_data)
       end
 
       it 'sets the correct content type header' do
-        pending 'not implemented yet'
         get '/api/v1/reference/locations', headers: headers
         expect(response.headers['Content-Type']).to match(Regexp.escape(ApiController::JSON_API_CONTENT_TYPE))
       end
@@ -46,7 +43,6 @@ RSpec.describe Api::V1::LocationsController do
       let(:headers) { { 'CONTENT_TYPE': 'application/xml' } }
 
       it 'fails if I set the wrong `content-type` header' do
-        pending 'not implemented yet'
         get '/api/v1/reference/locations', headers: headers
         expect(response.code).to eql '415'
       end
@@ -58,7 +54,6 @@ RSpec.describe Api::V1::LocationsController do
 
       context 'with the correct CONTENT_TYPE header' do
         it 'returns a valid 200 JSON response with move data' do
-          pending 'not implemented yet'
           get '/api/v1/reference/locations', headers: headers
           expect(JSON::Validator.validate!(schema, response_json, fragment: '#/200')).to be true
         end
@@ -68,7 +63,6 @@ RSpec.describe Api::V1::LocationsController do
         let(:headers) { { 'CONTENT_TYPE': 'application/xml' } }
 
         it 'returns a valid 415 JSON response' do
-          pending 'not implemented yet'
           get '/api/v1/reference/locations', headers: headers
           expect(JSON::Validator.validate!(schema, response_json, fragment: '#/415')).to be true
         end
