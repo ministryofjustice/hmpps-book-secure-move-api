9f4c26958b76b261655b72c65dc6589151a01796
Steve Hook <steve.hook@gmail.com>
2019-05-24 16:31:11 +0100

    [P4-280] Return moves sorted by destination name (#39)
    * Add sort order to MoveFinder
    
    * Add spec for sorting results of Moves::Finder
    
    Also renamed Moves::MoveFinder to Moves::Finder

--- app/controllers/api/v1/moves_controller.rb
+++ app/controllers/api/v1/moves_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class MovesController < ApiController
       def index
-        moves = Moves::MoveFinder.new(filter_params).call
+        moves = Moves::Finder.new(filter_params).call
 
         paginate moves, include: {
           person: %i[first_names last_name date_of_birth],
