2ad394e4d5ce55786d4664818b40b16c76e33068
Steve Hook <steve.hook@gmail.com>
2019-05-29 13:54:59 +0100

    Override accessor for dates to cast from strings

--- app/models/profile/profile_attribute.rb
+++ app/models/profile/profile_attribute.rb
@@ -1,5 +1,6 @@
 class Profile::ProfileAttribute
-  attr_accessor :description, :comments, :date, :expiry_date, :profile_attribute_type_id
+  attr_accessor :description, :comments, :profile_attribute_type_id
+  attr_reader :date, :expiry_date
 
   def initialize(attributes = {})
     attributes.symbolize_keys!
@@ -9,6 +10,14 @@ class Profile::ProfileAttribute
     self.date = attributes[:date]
     self.expiry_date = attributes[:expiry_date]
     self.profile_attribute_type_id = attributes[:profile_attribute_type_id]
+  end
+
+  def date=(value)
+    @date = value.is_a?(String) ? Date.parse(value) : value
+  end
+
+  def expiry_date=(value)
+    @expiry_date = value.is_a?(String) ? Date.parse(value) : value
   end
 
   def empty?
