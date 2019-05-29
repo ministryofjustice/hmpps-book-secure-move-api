525b5fa138f37396296f23e19bcb43cfeefa4910
Steve Hook <steve.hook@gmail.com>
2019-05-29 15:11:33 +0100

    Refactor specs for Profile#ProfileAttributes

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -11,27 +11,33 @@ RSpec.describe Profile, type: :model do
   it { is_expected.to validate_presence_of(:last_name) }
   it { is_expected.to validate_presence_of(:first_names) }
 
-  it 'serializes profile attributes correctly' do
-    person = create :person
-    profile = person.profiles.first
-    profile.profile_attributes = [
-      {
-        description: 'test',
-        comments: 'just a test',
-        profile_attribute_type_id: 123,
-        date: Date.civil(2019, 5, 30),
-        expiry_date: Date.civil(2019, 6, 30)
-      }
-    ]
-    profile.save
-    profile.profile_attributes
-    profile = Profile.find(profile.id)
-    expect(profile.profile_attributes&.first&.as_json).to eql(
-      description: 'test',
-      comments: 'just a test',
-      profile_attribute_type_id: 123,
-      date: Date.civil(2019, 5, 30),
-      expiry_date: Date.civil(2019, 6, 30)
-    )
+  describe '#profile_attributes' do
+    let!(:person) { create :person }
+    let(:profile) { person.profiles.first }
+    let(:profile_attributes) do
+      [
+        {
+          description: 'test',
+          comments: 'just a test',
+          profile_attribute_type_id: 123,
+          date: Date.civil(2019, 5, 30),
+          expiry_date: Date.civil(2019, 6, 30)
+        }
+      ]
+    end
+
+    it 'serializes profile attributes correctly' do
+      profile.profile_attributes = profile_attributes
+      profile.save
+      reloaded_profile = Profile.find(profile.id)
+      expect(reloaded_profile.profile_attributes&.first&.as_json).to eql(
+        profile_attributes.first
+      )
+    end
+
+    it 'deserializes profile attributes to an array of ProfileAttribute objects' do
+      profile.profile_attributes = profile_attributes
+      expect(profile.profile_attributes.first).to be_a(Profile::ProfileAttribute)
+    end
   end
 end
