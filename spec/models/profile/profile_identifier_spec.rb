a593d6ad3b12163054d6466590b32d3fc0bedebd
Steve Hook <steve.hook@gmail.com>
2019-05-30 13:54:37 +0100

    Failing specs for the serializable classes need for ids

---
+++ spec/models/profile/profile_identifier_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Profile::ProfileIdentifier, type: :model do
+  subject(:profile_identifier) { described_class.new(attribute_values) }
+
+  let(:description) { 'test' }
+  let(:attribute_values) do
+    {
+      value: 'ABC123456',
+      identifier_type: 'pnc_number'
+    }
+  end
+
+  describe '#as_json' do
+    it 'returns a hash of all values' do
+      expect(profile_identifier.as_json).to eql attribute_values
+    end
+  end
+
+  describe '#empty' do
+    context 'when value is missing' do
+      let(:value) { '' }
+
+      it 'returns true' do
+        expect(profile_identifier.empty?).to be true
+      end
+    end
+
+    context 'when value is present' do
+      let(:value) { 'test' }
+
+      it 'returns false' do
+        expect(profile_identifier.empty?).to be false
+      end
+    end
+  end
+end
---
+++ spec/models/profile/profile_identifiers_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Profile::ProfileIdentifiers, type: :model do
+  subject(:profile_identifiers) { described_class.new(data) }
+
+  let(:value) { 'ABC123456' }
+  let(:data) do
+    [
+      {
+        value: 'ABC123456',
+        identifier_type: :pnc_number
+      },
+      {
+        value: 'XYZ123456',
+        identifier_type: :prison_number
+      }
+    ]
+  end
+
+  describe '#to_a' do
+    it 'contains correct number of items' do
+      expect(profile_identifiers.to_a.size).to be 2
+    end
+
+    it 'converts the items to Profile::ProfileIdentifier objects' do
+      expect(profile_identifiers.to_a).to all(be_a Profile::ProfileIdentifier)
+    end
+
+    context 'with an empty item' do
+      subject(:profile_identifiers) { described_class.new(data + [{ value: '' }]) }
+
+      it 'strips out the empty item' do
+        expect(profile_identifiers.to_a.size).to be 2
+      end
+    end
+
+    context 'with serialized input' do
+      subject(:profile_identifiers) { described_class.new(data.to_json) }
+
+      it 'parses JSON and contains correct number of items' do
+        expect(profile_identifiers.to_a.size).to be 2
+      end
+
+      it 'parses JSON and converts the items to Profile::ProfileIdentifier objects' do
+        expect(profile_identifiers.to_a).to all(be_a Profile::ProfileIdentifier)
+      end
+    end
+  end
+end
