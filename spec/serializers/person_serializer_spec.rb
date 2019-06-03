571ebbe1a3312de666739c2c46a18cac1c0a4018
Steve Hook <steve.hook@gmail.com>
2019-06-03 16:54:16 +0100

    Update specs for PersonSerializer

--- spec/serializers/person_serializer_spec.rb
+++ spec/serializers/person_serializer_spec.rb
@@ -27,6 +27,95 @@ RSpec.describe PersonSerializer do
     expect(result[:data][:attributes][:last_name]).to eql 'Roberts'
   end
 
+  describe '#profile attributes' do
+    let(:risk_alert_type) do
+      create :profile_attribute_type, :risk
+    end
+    let(:health_alert_type) do
+      create :profile_attribute_type, :health
+    end
+    let(:court_information_type) do
+      create :profile_attribute_type, :court_information
+    end
+
+    let(:risk_alert) do
+      {
+        description: 'Escape risk',
+        comments: 'Former miner',
+        profile_attribute_type_id: risk_alert_type.id
+      }
+    end
+
+    let(:health_alert) do
+      {
+        description: 'Medication',
+        comments: 'Needs something for a headache',
+        profile_attribute_type_id: health_alert_type.id
+      }
+    end
+
+    let(:court_information) do
+      {
+        description: 'Interpreter required',
+        comments: 'Only speaks Spanish',
+        profile_attribute_type_id: court_information_type.id
+      }
+    end
+
+    before do
+      profile = person.latest_profile
+      profile.profile_attributes = [
+        risk_alert,
+        health_alert,
+        court_information
+      ]
+      profile.save!
+    end
+
+    it 'contains a `risk_alerts` nested collection' do
+      expect(result[:data][:attributes][:risk_alerts].map do |alert|
+        alert[:description]
+      end).to eql ['Escape risk']
+    end
+
+    it 'contains a `health_alerts` nested collection' do
+      expect(result[:data][:attributes][:health_alerts].map do |alert|
+        alert[:description]
+      end).to eql ['Medication']
+    end
+
+    it 'contains a `court_information` nested collection' do
+      expect(result[:data][:attributes][:court_information].map do |alert|
+        alert[:description]
+      end).to eql ['Interpreter required']
+    end
+  end
+
+  describe '#identifiers' do
+    let(:profile_identifiers) do
+      [
+        {
+          value: 'ABC123456',
+          identifier_type: 'pnc_number'
+        },
+        {
+          value: 'XYZ123456',
+          identifier_type: 'prison_number'
+        }
+      ]
+    end
+
+    before do
+      profile = person.latest_profile
+      profile.profile_identifiers = profile_identifiers
+      profile.save!
+    end
+
+    it 'contains two identifiers' do
+      expect(result[:data][:attributes][:identifiers]).to eql profile_identifiers
+    end
+  end
+
   describe 'ethnicity' do
     let(:adapter_options) { { include: { ethnicity: %I[code title description] } } }
     let(:ethnicity) { person.latest_profile&.ethnicity }
