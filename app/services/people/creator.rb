d781b42b46611cecca44ab9f5cf705988706f4fb
Steve Hook <steve.hook@gmail.com>
2019-06-06 10:35:59 +0100

    Get basic spec passing
    Can now create a new Person/Profile

--- app/services/people/creator.rb
+++ app/services/people/creator.rb
@@ -2,14 +2,27 @@
 
 module People
   class Creator
-    attr_accessor :person_params
+    attr_accessor :params
 
-    def initialize(person_params)
-      self.person_params = person_params
+    def initialize(params)
+      self.params = params
     end
 
     def call
-      # TODO: Implement this
+      profile = create_profile
+      profile.person
+    end
+
+    PROFILE_ATTRIBUTES = %i[first_names last_name date_of_birth].freeze
+
+    private
+
+    def create_profile
+      Profile.create!(profile_params.merge(person: Person.create!))
+    end
+
+    def profile_params
+      params[:attributes].slice(*PROFILE_ATTRIBUTES)
     end
   end
 end
