96b3a6225bf50da7a6226548169e5170ca078056
Steve Hook <steve.hook@gmail.com>
2019-06-10 09:16:45 +0100

    Add database column for Move#reference
    Will need to populate this in a later migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_132022) do
+ActiveRecord::Schema.define(version: 2019_06_10_081131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2019_05_30_132022) do
     t.time "time_due", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reference"
+    t.index ["reference"], name: "index_moves_on_reference", unique: true
   end
 
   create_table "nationalities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
