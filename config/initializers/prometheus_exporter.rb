adb7e816bcfb66ade24181e5d96558331df61777
Steve Hook <steve.hook@gmail.com>
2019-06-25 17:37:49 +0100

    Appease Rubocop

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-unless Rails.env == "test"
+unless Rails.env == 'test'
   require 'prometheus_exporter/middleware'
 
   # This reports stats per request like HTTP status and timings
