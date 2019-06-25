3468e3d84249b866baefb7ec28f244c36d2e074c
Steve Hook <steve.hook@gmail.com>
2019-06-25 10:55:30 +0100

    Add initializer for prometheus_exporter

---
+++ config/initializers/prometheus_exporter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+unless Rails.env == "test"
+  require 'prometheus_exporter/middleware'
+
+  # This reports stats per request like HTTP status and timings
+  Rails.application.middleware.unshift PrometheusExporter::Middleware
+end
