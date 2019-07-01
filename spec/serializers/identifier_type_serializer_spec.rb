7e89f688657084591cd616d50a262819f152c401
Steve Hook <steve.hook@gmail.com>
2019-07-01 10:20:32 +0100

    Add IdentifierTypeSerializer

---
+++ spec/serializers/identifier_type_serializer_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe IdentifierTypeSerializer do
+  subject(:serializer) { described_class.new(identifier_type) }
+
+  let(:identifier_type) { create :identifier_type }
+  let(:result) { JSON.parse(ActiveModelSerializers::Adapter.create(serializer).to_json).deep_symbolize_keys }
+
+  it 'contains a type property' do
+    expect(result[:data][:type]).to eql 'identifier_types'
+  end
+
+  it 'contains an id property' do
+    expect(result[:data][:id]).to eql identifier_type.id
+  end
+
+  it 'contains a key attribute' do
+    expect(result[:data][:attributes][:key]).to eql identifier_type.key
+  end
+
+  it 'contains a title attribute' do
+    expect(result[:data][:attributes][:title]).to eql 'PNC ID'
+  end
+end
