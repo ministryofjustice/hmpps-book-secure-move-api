1f2f3fb1edf2bf94be6c4f8df44a144a495df771
Steve Hook <steve.hook@gmail.com>
2019-07-17 10:55:33 +0100

    Refactor to shared context

--- spec/lib/nomis_client/ethnicities_spec.rb
+++ spec/lib/nomis_client/ethnicities_spec.rb
@@ -2,32 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe NomisClient::Ethnicities do
-  let(:oauth2_client) { instance_double('OAuth2::Client', client_credentials: client_credentials) }
-  let(:client_credentials) { instance_double('OAuth2::Strategy::ClientCredentials', get_token: token) }
-  let(:token) do
-    instance_double('OAuth2::AccessToken',
-      get: oauth2_response,
-      expires?: true,
-      refresh!: true,
-      expires_at: token_expires_at)
-  end
-  let(:oauth2_response) do
-    instance_double(
-      'OAuth2::Response',
-      body: response_body,
-      parsed: response_json,
-      status: response_status
-    )
-  end
-
-  before { allow(OAuth2::Client).to receive(:new).and_return(oauth2_client) }
-
-  after do
-    NomisClient.instance_variable_set(:@client, nil)
-    NomisClient.instance_variable_set(:@token, nil)
-  end
-
+RSpec.describe NomisClient::Ethnicities, with_nomis_client_authentication: true do
   describe '.get' do
     let(:response) { described_class.get }
     let(:api_endpoint) { '/reference-domains/domains/ETHNICITY' }
---
+++ spec/support/with_nomis_client_auth_context.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'with NomisClient authentication', shared_context: :metadata do
+  let(:oauth2_client) { instance_double('OAuth2::Client', client_credentials: client_credentials) }
+  let(:client_credentials) { instance_double('OAuth2::Strategy::ClientCredentials', get_token: token) }
+  let(:token) do
+    instance_double('OAuth2::AccessToken',
+      get: oauth2_response,
+      expires?: true,
+      refresh!: true,
+      expires_at: token_expires_at)
+  end
+  let(:oauth2_response) do
+    instance_double(
+      'OAuth2::Response',
+      body: response_body,
+      parsed: response_json,
+      status: response_status
+    )
+  end
+
+  before { allow(OAuth2::Client).to receive(:new).and_return(oauth2_client) }
+
+  after do
+    NomisClient.instance_variable_set(:@client, nil)
+    NomisClient.instance_variable_set(:@token, nil)
+  end
+end
+
+RSpec.configure do |rspec|
+  rspec.include_context 'with NomisClient authentication', with_nomis_client_authentication: true
+end
+
