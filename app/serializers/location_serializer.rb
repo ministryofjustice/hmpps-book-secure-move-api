e2f1155a007ca587b5bf2dcfcd203efd26cd5bbd
Steve Hook <steve.hook@gmail.com>
2019-07-23 15:34:30 +0100

    Typo correction

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,4 +1,4 @@
-# Frozen_string_literal: true
+# frozen_string_literal: true
 
 class LocationSerializer < ActiveModel::Serializer
   attributes :id, :key, :title, :location_type, :nomis_agency_id, :disabled_at
