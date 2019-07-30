5a899d8bc806605af2c14954065ecaf2cf49ae15
Steve Hook <steve.hook@gmail.com>
2019-07-30 14:05:12 +0100

    Change format to 3 letters-4 numbers-1 letter (#92)
    Was just a random alphanumeric string (without certain ambiguous
    characters likes i and 0).

--- app/services/moves/reference_generator.rb
+++ app/services/moves/reference_generator.rb
@@ -4,12 +4,18 @@ module Moves
   class ReferenceGenerator
     def call
       loop do
-        reference = PERMISSIBLE_CHARACTERS.sample(REFERENCE_LENGTH).join
+        reference =
+          PERMISSIBLE_CHARACTERS.sample(REFERENCE_PART1_LENGTH).join +
+          PERMISSIBLE_NUMBERS.sample(REFERENCE_PART2_LENGTH).join +
+          PERMISSIBLE_CHARACTERS.sample(REFERENCE_PART3_LENGTH).join
         break reference unless Move.where(reference: reference).exists?
       end
     end
 
-    REFERENCE_LENGTH = 8
-    PERMISSIBLE_CHARACTERS = %i[A C E F H J K M N P R T U V W X Y 1 2 3 4 5 6 7 8 9].freeze
+    REFERENCE_PART1_LENGTH = 3
+    REFERENCE_PART2_LENGTH = 4
+    REFERENCE_PART3_LENGTH = 1
+    PERMISSIBLE_CHARACTERS = %i[A C E F H J K M N P R T U V W X Y].freeze
+    PERMISSIBLE_NUMBERS = %i[1 2 3 4 5 6 7 8 9].freeze
   end
 end
