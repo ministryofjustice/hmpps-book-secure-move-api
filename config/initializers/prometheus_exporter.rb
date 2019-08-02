32fa090c6bf8de021654f5386967083dc6eca643
Steve Hook <steve.hook@gmail.com>
2019-08-02 14:52:04 +0100

    This change adds per-process memory metrics

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -2,7 +2,9 @@
 
 unless Rails.env == 'test'
   require 'prometheus_exporter/middleware'
+  require 'prometheus_exporter/instrumentation'
 
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
+  PrometheusExporter::Instrumentation::Process.start(type: 'master')
 end
