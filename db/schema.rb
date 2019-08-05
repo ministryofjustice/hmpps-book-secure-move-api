703855f075f89fa1234084ff58363a5cc7d76905
Steve Hook <steve.hook@gmail.com>
2019-08-05 11:14:13 +0100

    Migration to set prompt_for_additional_information for trans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_150644) do
+ActiveRecord::Schema.define(version: 2019_08_05_094511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
