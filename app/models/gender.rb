3e8c94b9a33912d5ec00775d097c5b04def499a3
Steve Hook <steve.hook@gmail.com>
2019-08-05 09:18:48 +0100

    Validate Gender#prompt_for_additional_information
    Should be 'present' and have it's default set to `false`

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -3,4 +3,13 @@
 class Gender < ApplicationRecord
   validates :title, presence: true
   validates :key, presence: true
+  validates :prompt_for_additional_information, inclusion: [true, false]
+
+  before_validation :set_prompt_for_additional_information
+
+  private
+
+  def set_prompt_for_additional_information
+    self.prompt_for_additional_information = false if prompt_for_additional_information.nil?
+  end
 end
