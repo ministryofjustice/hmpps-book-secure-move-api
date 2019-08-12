6c4204caa2dfa06b5209a4bc69203a6dc8c8adeb
Steve Hook <steve.hook@gmail.com>
2019-08-12 22:19:22 +0100

    Specs for NomisClient::People.anonymise

--- app/lib/nomis_client/people.rb
+++ app/lib/nomis_client/people.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+require 'nomis/faker'
+
 class NomisClient
   class People
     class << self
--- spec/lib/nomis_client/people_spec.rb
+++ spec/lib/nomis_client/people_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'nomis_client/moves'
 require 'dotenv/load'
 
 RSpec.describe NomisClient::People do
@@ -10,6 +11,28 @@ RSpec.describe NomisClient::People do
     it 'has the correct number of results' do
       VCR.use_cassette('people', record: :new_episodes) do
         expect(response.count).to be 1
+      end
+    end
+  end
+
+  describe '.anonymise' do
+    before do
+      create :location, title: 'PENTONVILLE (HMP)', nomis_agency_id: 'PVI'
+      create :location, title: 'LEEDS (HMP)', nomis_agency_id: 'LEI'
+      create :location, title: 'MANCHESTER (HMP)', nomis_agency_id: 'MRI'
+    end
+
+    KEYS = %i[
+      offenderNo firstName middleNames lastName dateOfBirth gender sexCode nationalities
+      currentlyInPrison latestBookingId latestLocationId latestLocation internalLocation
+      pncNumber croNumber ethnicity birthCountry religion convictedStatus
+      imprisonmentStatus maritalStatus
+    ].freeze
+    let(:anonymised) { described_class.anonymise(nil) }
+
+    KEYS.each do |key|
+      it "randomly populates the #{key}" do
+        expect(anonymised).to have_key(key)
       end
     end
   end
