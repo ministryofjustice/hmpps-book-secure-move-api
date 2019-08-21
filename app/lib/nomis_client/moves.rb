bf0154932a2f7f2b9876557e463d0c1a786b86cb
Steve Hook <steve.hook@gmail.com>
2019-08-21 08:32:29 +0100

    Drop the test mode code

--- app/lib/nomis_client/moves.rb
+++ app/lib/nomis_client/moves.rb
@@ -4,34 +4,11 @@ module NomisClient
   class Moves < NomisClient::Base
     class << self
       def get(nomis_agency_ids:, date:)
-        return get_test_mode(nomis_agency_id: nomis_agency_ids, date: date) if NomisClient::Base.test_mode?
-
         NomisClient::Base.get(
           '/movements/transfers',
           params: params_for(nomis_agency_ids, date),
           headers: { 'Page-Limit' => '1000' }
         ).parsed
-      end
-
-      def get_test_mode(nomis_agency_id:, date:)
-        date_offset = offset_from(date: date)
-        file_name = "#{NomisClient::Base::FIXTURE_DIRECTORY}/moves-#{date_offset}-#{nomis_agency_id}.json.erb"
-        return empty_response unless File.exist?(file_name)
-
-        JSON.parse(ERB.new(File.read(file_name)).result)
-      end
-
-      def offset_from(date:)
-        (date.in_time_zone.midnight - Time.zone.now.midnight).to_i / 1.days
-      end
-
-      def empty_response
-        {
-          courtEvents: [],
-          transferEvents: [],
-          releaseEvents: [],
-          movements: []
-        }
       end
 
       def params_for(nomis_agency_ids, date)
--- app/lib/nomis_client/people.rb
+++ app/lib/nomis_client/people.rb
@@ -6,18 +6,11 @@ module NomisClient
   class People
     class << self
       def get(nomis_offender_number:)
-        return get_test_mode(nomis_offender_number: nomis_offender_number) if NomisClient::Base.test_mode?
-
         NomisClient::Base.get(
           "/prisoners/#{nomis_offender_number}",
           params: {},
           headers: { 'Page-Limit' => '1000' }
         ).parsed
-      end
-
-      def get_test_mode(nomis_offender_number:)
-        file_name = "#{NomisClient::Base::FIXTURE_DIRECTORY}/people-#{nomis_offender_number}.json.erb"
-        JSON.parse(ERB.new(File.read(file_name)).result)
       end
     end
   end
