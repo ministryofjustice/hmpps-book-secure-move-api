0a68ffa7277a417de514b61bd1967dce40c0d4ab
Steve Hook <steve.hook@gmail.com>
2019-08-22 17:02:53 +0100

    Map alert properties to Ruby naming conventions

--- app/lib/nomis_client/alerts.rb
+++ app/lib/nomis_client/alerts.rb
@@ -4,14 +4,28 @@ class NomisClient
   class Alerts
     class << self
       def get(prison_number)
-        attributes_for(
-          NomisClient.get("/bookings/offenderNo/#{prison_number}/alerts").parsed
-        )
+        NomisClient.get("/bookings/offenderNo/#{prison_number}/alerts").parsed.map do |alert|
+          attributes_for(alert)
+        end
       end
 
-      def attributes_for(alerts)
-        alerts
+      # rubocop:disable Metrics/MethodLength
+      def attributes_for(alert)
+        {
+          alert_id: alert['alertId'],
+          alert_type: alert['alertType'],
+          alert_type_description: alert['alertTypeDescription'],
+          alert_code: alert['alertCode'],
+          alert_code_description: alert['alertCodeDescription'],
+          comment: alert['comment'],
+          created_at: alert['dateCreated'],
+          expires_at: alert['dateExpires'],
+          expired: alert['expired'],
+          active: alert['active'],
+          rnum: alert['rnum']
+        }
       end
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
