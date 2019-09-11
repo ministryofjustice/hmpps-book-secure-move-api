c2012d67c1cb204cfed42d87774432c94f0ab3ce
Steve Hook <steve.hook@gmail.com>
2019-09-11 11:25:34 +0100

    Extend Location with helpers for location type

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Location < ApplicationRecord
+  LOCATION_TYPE_COURT = 'court'
+  LOCATION_TYPE_POLICE = 'police'
+  LOCATION_TYPE_PRISON = 'prison'
+
   NOMIS_AGENCY_TYPES = {
-    'INST' => :prison,
-    'CRT' => :court
+    'INST' => LOCATION_TYPE_PRISON,
+    'CRT' => LOCATION_TYPE_COURT
   }.freeze
 
   has_many :moves_from, class_name: 'Move', foreign_key: :from_location_id
@@ -12,4 +16,16 @@ class Location < ApplicationRecord
   validates :key, presence: true
   validates :title, presence: true
   validates :location_type, presence: true
+
+  def prison?
+    location_type.to_s == LOCATION_TYPE_PRISON
+  end
+
+  def police?
+    location_type.to_s == LOCATION_TYPE_POLICE
+  end
+
+  def court?
+    location_type.to_s == LOCATION_TYPE_COURT
+  end
 end
