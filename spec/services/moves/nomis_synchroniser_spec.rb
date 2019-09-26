59d2d2fab2f5b6a50ddff4623fc320af1c046b85
Steve Hook <steve.hook@gmail.com>
2019-09-26 16:32:59 +0100

    [P4-691] Cancel duplicate moves when importing (#132)
    * Cancel duplicate moves when importing
    
    * Start building a `Moves::Sweeper` service to cancel out-of-date moves
    
    * Add specs for Moves::Sweeper

--- spec/services/moves/nomis_synchroniser_spec.rb
+++ spec/services/moves/nomis_synchroniser_spec.rb
@@ -14,9 +14,11 @@ RSpec.describe Moves::NomisSynchroniser do
 
   describe '.call' do
     let(:importer) { instance_double(Moves::Importer, call: nil) }
+    let(:sweeper) { instance_double(Moves::Sweeper, call: nil) }
 
     before do
       allow(Moves::Importer).to receive(:new).and_return(importer)
+      allow(Moves::Sweeper).to receive(:new).and_return(sweeper)
       allow(NomisClient::Moves).to receive(:get).and_return([])
       synchroniser.call
     end
@@ -26,6 +28,10 @@ RSpec.describe Moves::NomisSynchroniser do
 
       it 'does NOT call importer' do
         expect(Moves::Importer).not_to have_received(:new)
+      end
+
+      it 'does NOT call sweeper' do
+        expect(Moves::Sweeper).not_to have_received(:new)
       end
 
       it 'does NOT call the NOMIS API' do
@@ -38,6 +44,10 @@ RSpec.describe Moves::NomisSynchroniser do
 
       it 'calls importer' do
         expect(Moves::Importer).to have_received(:new).with([])
+      end
+
+      it 'calls sweeper' do
+        expect(Moves::Sweeper).to have_received(:new).with(location, date, [])
       end
 
       it 'calls the NOMIS API' do
