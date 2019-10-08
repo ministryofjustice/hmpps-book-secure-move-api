763fc317439a00ae880bca114f61744c81eb5b92
ngw <ngw@nofeed.org>
2019-10-08 14:36:50 +0100

    locations accept filter supplier_ids

--- app/services/locations/finder.rb
+++ app/services/locations/finder.rb
@@ -9,7 +9,7 @@ module Locations
     end
 
     def call
-      Location.where(filter_params.slice(:location_type, :nomis_agency_id))
+      Location.where(filter_params.slice(:location_type, :nomis_agency_id, :supplier_ids))
     end
   end
 end
