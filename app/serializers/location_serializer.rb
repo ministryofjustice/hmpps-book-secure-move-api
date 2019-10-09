b9b6f4071ad2dd1b4935a64eaa02046888a40f27
ngw <ngw@nofeed.org>
2019-10-09 11:53:42 +0100

    Adding suppliers to locations output

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :key, :title, :location_type, :nomis_agency_id, :disabled_at
+  attributes :id, :key, :title, :location_type, :nomis_agency_id, :disabled_at, :suppliers
+
+  def suppliers
+    object.suppliers.each { |supplier| SupplierSerializer.new(supplier) }
+  end
 end
