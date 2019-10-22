070aaa0007b43f847bcfdc2f14a3488647a62ca4
ngw <ngw@nofeed.org>
2019-10-22 13:47:54 +0100

    police national computer filter

---
+++ spec/services/people/finder_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe People::Finder do
+  subject(:people_finder) { described_class.new(filter_params) }
+
+  let!(:person) { create(:person) }
+  let(:filter_params) { {} }
+
+  describe 'filtering' do
+    context 'when matching police_national_computer filter' do
+      let(:filter_params) { { police_national_computer: 'AB/1234567' } }
+
+      it 'returns people matching the police_national_computer' do
+        expect(people_finder.call.pluck(:id)).to eq [person.id]
+      end
+    end
+  end
+end
