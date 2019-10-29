4cdf09a5687be28e1a1fbf830aab336eb26c77c1
ngw <ngw@nofeed.org>
2019-10-29 14:05:50 +0000

    Working sweeper + documentation

--- app/services/moves/sweeper.rb
+++ app/services/moves/sweeper.rb
@@ -17,17 +17,39 @@ module Moves
     private
 
     def cancel_outdated_moves!
-      outdated_moves = Move.where(
-        date: date,
-        from_location_id: locations.map(&:id)
-      ).where.not(
-        nomis_event_id: current_nomis_event_ids
-      )
-      outdated_moves.update(status: Move::MOVE_STATUS_CANCELLED)
+      update_nomis_event_ids!
+      Move.where(nomis_event_ids: []).update(status: Move::MOVE_STATUS_CANCELLED)
     end
 
     def current_nomis_event_ids
       items.map { |item| item[:nomis_event_id] }
+    end
+
+    def update_nomis_event_ids!
+      return if current_nomis_event_ids.empty?
+
+      scope = Move.where(
+        date: date,
+        from_location_id: locations.map(&:id)
+      )
+      update_nomis_event_ids_containing_currents(scope)
+      update_nomis_event_ids_not_containing_currents(scope)
+    end
+
+    def update_nomis_event_ids_containing_currents(scope)
+      scope.where(
+        'ARRAY[?] && nomis_event_ids', current_nomis_event_ids
+      ).each do |move|
+        move.update(nomis_event_ids: move.nomis_event_ids & current_nomis_event_ids)
+      end
+    end
+
+    def update_nomis_event_ids_not_containing_currents(scope)
+      scope.where.not(
+        'ARRAY[?] && nomis_event_ids', current_nomis_event_ids
+      ).each do |move|
+        move.update(nomis_event_ids: [])
+      end
     end
   end
 end
