af3c6e1faf727bb85ce003ff0b86bdca2eeb7575
ngw <ngw@nofeed.org>
2019-11-29 11:07:57 +0000

    removed render_document and forced content_type

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -3,17 +3,19 @@
 module Api
   module V1
     class DocumentsController < ApiController
+      prepend_before_action :set_force_content_type
+
       CONTENT_TYPE = 'multipart/form-data'
 
       def create
         document = Document.create!(document_attributes)
-        render json: document, status: 201
+        render json: document, status: 201, content_type: ApiController::CONTENT_TYPE
       end
 
       def destroy
         document = Document.find(params[:id])
         document.destroy!
-        render json: document, status: 200
+        render json: document, status: 200, content_type: ApiController::CONTENT_TYPE
       end
 
       private
@@ -30,6 +32,10 @@ module Api
         document_params[:attributes].merge(
           move: Move.find(params.dig(:move_id))
         )
+      end
+
+      def set_force_content_type
+        @force_content_type = true
       end
     end
   end
