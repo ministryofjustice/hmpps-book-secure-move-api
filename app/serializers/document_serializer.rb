2fd6a62ca1c1c49d1e5d51e4eae8dc289664885d
Ryan Brooks <ryan@slatehorse.com>
2019-12-20 11:54:20 +0000

    Use url_for across all environments to retrieve service URL.
    url_for successfully retrieves the local URL for the attachment, which redirects to S3 when in use.

--- app/serializers/document_serializer.rb
+++ app/serializers/document_serializer.rb
@@ -7,9 +7,7 @@ class DocumentSerializer < ActiveModel::Serializer
   attributes :id, :url, :filename, :filesize, :content_type
 
   def url
-    rails_blob_url(object.file)
-  rescue ArgumentError
-    object.file.service_url
+    url_for(object.file)
   end
 
   def filename
