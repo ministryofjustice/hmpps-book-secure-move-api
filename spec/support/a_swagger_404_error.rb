02d228b4e586cfb1f4cfa494100a5eb978b43282
Ryan Brooks <ryan@slatehorse.com>
2020-01-03 11:34:59 +0000

    add shared examples for 401, 404 and 415 error responses for RSwag.
    RSwag carries out a bunch of validation as part of the test run, so we don't need all the shared examples currently in place. Toggling assertion
    when inside an RSwag definition was tricksy, so I've duplicated the content and content-type header assertions, with a view to removing the existing shared examples if/when we've migrated to RSwag entirely

---
+++ spec/support/a_swagger_404_error.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'a swagger 404 error' do
+
+  let(:errors_404) do
+    [
+      {
+        'title' => 'Resource not found',
+        'detail' => detail_404
+      }
+    ]
+  end
+
+  schema '$ref' => '#/definitions/error_responses/404'
+
+  run_test!
+
+  it 'sets the Content-Type header' do
+    expect(response.headers['Content-Type']).to match(Regexp.escape(content_type))
+  end
+
+  it 'returns errors in the body of the response' do
+    expect(JSON.parse(response.body)).to include_json(errors: errors_404)
+  end
+end
