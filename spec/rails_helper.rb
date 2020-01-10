231bb6e15c02c92d9fbbe9d5245678d51b6a9fb5
Stephen Dicks <stephen@starswan.com>
2020-01-10 11:00:18 +0000

    added basic test coverage support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start :rails do
+    minimum_coverage 78.11
+    maximum_coverage_drop 0
+  end
+end
+
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
