4af4b0efb01e4211e2a06c1f530f16626924bc22
ngw <ngw@nofeed.org>
2020-01-17 16:20:19 +0000

    Ability refactoring + test

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,17 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user = nil)
-    if user
-      can :manage, Move, Move.served_by(user) do |move|
-        move.from_location.suppliers.include?(user)
+  def initialize(application = nil)
+    return unless application # no permissions
+
+    if application.owner
+      # owner is a supplier, suppliers can manage only their own locations
+      can :manage, Move, Move.served_by(application.owner) do |move|
+        move.from_location.suppliers.include?(application.owner)
       end
     else
+      # momentarily give all permissions to not break compatibility with
+      # the frontend application
       can :manage, Move
     end
   end
