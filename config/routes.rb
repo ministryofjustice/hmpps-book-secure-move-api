9d7e8be38efb975e5b874548fe7cbf2f8c2ed1df
ngw <ngw@nofeed.org>
2020-02-05 16:34:38 +0000

    POST /documents to upload a document without a move

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :documents, only: %i[create]
       resources :people, only: %i[index create update]
       resources :moves, only: %i[index show create destroy update] do
         resources :documents, only: %i[create destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_150100) do
+ActiveRecord::Schema.define(version: 2020_02_07_111034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2020_01_27_150100) do
   end
 
   create_table "documents", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "move_id", null: false
+    t.uuid "move_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["move_id"], name: "index_documents_on_move_id"
