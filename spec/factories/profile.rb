bbcc5120a2ec86356814a48d23ad57ac3ad83c68
Alexdesi <alexdesi@gmail.com>
2020-02-10 09:09:19 +0000

    Add filter by nomis_offender_no to People::Finder

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -5,7 +5,10 @@ FactoryBot.define do
     first_names { 'Bob' }
     last_name { 'Roberts' }
     date_of_birth { Date.new(1980, 10, 20) }
-    profile_identifiers { [{ identifier_type: 'police_national_computer', value: 'AB/1234567' }] }
+    profile_identifiers {
+      [{ identifier_type: 'police_national_computer', value: 'AB/1234567' },
+       { identifier_type: 'prison_number', value: 'ABCDEFG' }]
+    }
     association(:ethnicity)
     association(:gender)
   end
