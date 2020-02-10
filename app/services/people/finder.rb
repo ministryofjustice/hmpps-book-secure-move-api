47f3e8aa02150fff3b33a955dd4af5b0cddcb57d
Alessandro De Simone <alexdesi@users.noreply.github.com>
2020-02-10 14:17:39 +0000

    Add filter by nomis_offender_no to People::Finder (#246)

--- app/services/people/finder.rb
+++ app/services/people/finder.rb
@@ -19,14 +19,25 @@ module People
     end
 
     def apply_police_national_computer_filters(scope)
-      return unless filter_params.key?(:police_national_computer)
-
       scope = scope.joins(:profiles)
-      scope.where('profiles.profile_identifiers @> ?', police_national_computer)
+
+      if filter_params.key?(:police_national_computer)
+        scope.where('profiles.profile_identifiers @> ?', police_national_computer.to_json)
+      end
+
+      if filter_params.key?(:nomis_offender_no)
+        scope.where('profiles.nomis_offender_no @> ?', nomis_offender_no.to_json)
+      end
+
+      scope
     end
 
     def police_national_computer
-      [{ identifier_type: 'police_national_computer', value: filter_params[:police_national_computer] }].to_json
+      [{ identifier_type: 'police_national_computer', value: filter_params[:police_national_computer] }]
+    end
+
+    def nomis_offender_no
+      [{ identifier_type: 'prison_number', value: filter_params[:nomis_offender_no] }]
     end
   end
 end
--- spec/services/people/finder_spec.rb
+++ spec/services/people/finder_spec.rb
@@ -16,5 +16,13 @@ RSpec.describe People::Finder do
         expect(people_finder.call.pluck(:id)).to eq [person.id]
       end
     end
+
+    context 'when matching nomis_offender_no filter' do
+      let(:filter_params) { { nomis_offender_no: 'ABCDEFG' } }
+
+      it 'returns people matching the police_national_computer' do
+        expect(people_finder.call.pluck(:id)).to eq [person.id]
+      end
+    end
   end
 end
