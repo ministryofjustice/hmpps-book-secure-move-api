3e4b80de633d3ba7ede19afbb3666108213c4b7c
Alexdesi <alexdesi@gmail.com>
2020-02-10 09:09:19 +0000

    Add filter by nomis_offender_no to People::Finder

--- spec/services/people/finder_spec.rb
+++ spec/services/people/finder_spec.rb
@@ -30,5 +30,13 @@ RSpec.describe People::Finder do
         expect(people_finder.call).to eq [person]
       end
     end
+
+    context 'when matching nomis_offender_no filter' do
+      let(:filter_params) { { nomis_offender_no: 'ABCDEFG' } }
+
+      it 'returns people matching the police_national_computer' do
+        expect(people_finder.call.pluck(:id)).to eq [person.id]
+      end
+    end
   end
 end
