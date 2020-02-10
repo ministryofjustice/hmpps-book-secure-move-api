4eb9bfbeb2dac1bc1daa91a689935955ab308a6b
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-10 14:18:03 +0000

    additional tests for discard

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -8,4 +8,40 @@ RSpec.describe Notification, type: :model do
   it { is_expected.to validate_presence_of(:time_stamp) }
   it { is_expected.to validate_presence_of(:event_type) }
   it { is_expected.to validate_presence_of(:topic) }
+
+  describe 'kept?' do
+    subject(:notification) { create(:notification, subscription: subscription, discarded_at: discarded_at) }
+
+    context 'when parent subscription is discarded' do
+      let(:subscription) { build(:subscription, discarded_at: Time.now) }
+
+      context 'when notification is discarded' do
+        let(:discarded_at) { Time.now }
+
+        it { expect(notification.kept?).to be false }
+      end
+
+      context 'when notification is not discarded' do
+        let(:discarded_at) { nil }
+
+        it { expect(notification.kept?).to be false }
+      end
+    end
+
+    context 'when parent subscription is not discarded' do
+      let(:subscription) { build(:subscription, discarded_at: nil) }
+
+      context 'when notification is discarded' do
+        let(:discarded_at) { Time.now }
+
+        it { expect(notification.kept?).to be false }
+      end
+
+      context 'when notification is not discarded' do
+        let(:discarded_at) { nil }
+
+        it { expect(notification.kept?).to be true }
+      end
+    end
+  end
 end
