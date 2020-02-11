7158c74b99ac1515f8ab09a17a0ff0af6126943f
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-11 12:33:35 +0000

    notification serialization
    this was hard work!

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
+# NB: at the moment this serializer only supports Move topics. In the future we should also support Person topics.
 class NotificationSerializer < ActiveModel::Serializer
-  attributes :id, :event_type
+  include Rails.application.routes.url_helpers
 
-  has_one :topic
+  attributes :id, :event_type, :timestamp
 
-  INCLUDED_ATTRIBUTES = { topic: %i[id type] }.freeze
+  # this is a little ugly but it is the only way (?!) to get the serializer to behave exactly as required
+  belongs_to :topic, polymorphic: true, key: 'move', if: :move? do |serializer|
+    link :self, serializer.move_url
+    object.topic
+  end
+
+  def timestamp
+    # NB: "timestamp" is a reserved word in Postgres, hence this is renamed in the json
+    object.time_stamp
+  end
+
+  def move?
+    object.topic.is_a?(Move)
+  end
+
+  def move_url
+    api_v1_move_url(object.topic.id)
+  end
 end
--- spec/factories/notification.rb
+++ spec/factories/notification.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :notification do
     association :subscription
     time_stamp { '2020-01-22 09:07:58' }
-    event_type { 'create_move' }
+    event_type { 'move_created' }
     association :topic, factory: :move
     delivery_attempts { 0 }
     delivery_attempted_at { '2020-01-22 09:07:58' }
