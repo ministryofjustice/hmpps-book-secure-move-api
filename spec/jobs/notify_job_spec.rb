daf4e5d89e6d185b743dc0c7ab584f026d488c68
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-11 16:49:06 +0000

    Update notify_job_spec.rb
    bonus test

--- spec/jobs/notify_job_spec.rb
+++ spec/jobs/notify_job_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe NotifyJob, type: :job do
+  subject(:perform!) { described_class.new.perform(notification_id: notification.id) }
+
   let(:subscription) { create(:subscription) }
   let(:notification) { create(:notification, subscription: subscription, delivered_at: nil, delivery_attempted_at: nil) }
   let(:client) { class_double(Faraday, post: nil) }
@@ -9,29 +11,47 @@ RSpec.describe NotifyJob, type: :job do
   let(:hmac) { Encryptor.hmac(notification.subscription.secret, data) }
   let(:headers) { { 'PECS-SIGNATURE': hmac, 'PECS-NOTIFICATION-ID': notification.id } }
 
-  before do
-    allow(Faraday).to receive(:new).and_return(client)
-    allow(client).to receive(:post).and_return(response)
-    described_class.new.perform(notification_id: notification.id)
+  context 'when notification and subscription are active' do
+    before do
+      allow(Faraday).to receive(:new).and_return(client)
+      allow(client).to receive(:post).and_return(response)
+      perform!
+    end
+
+    context 'when notification is a success' do
+      it 'posts JSON to the callback_url' do
+        expect(client).to have_received(:post).with(notification.subscription.callback_url, data, headers)
+      end
+
+      it 'updates delivered_at' do
+        expect(notification.reload.delivered_at).not_to be_nil
+      end
+    end
+
+    context 'when notification is not a success' do
+      it 'updates delivery_attempts' do
+        expect(notification.reload.delivery_attempts).to eq(1)
+      end
+
+      it 'updates delivery_attempted_at' do
+        expect(notification.reload.delivery_attempted_at).not_to be_nil
+      end
+    end
   end
 
-  context 'when notification is a success' do
-    it 'posts JSON to the callback_url' do
-      expect(client).to have_received(:post).with(notification.subscription.callback_url, data, headers)
-    end
+  context 'when notification is discarded' do
+    before { notification.discard! }
 
-    it 'updates delivered_at' do
-      expect(notification.reload.delivered_at).not_to be_nil
+    it 'raises a Record Not Found error' do
+      expect { perform! }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 
-  context 'when notification is not a success' do
-    it 'updates delivery_attempts' do
-      expect(notification.reload.delivery_attempts).to eq(1)
-    end
+  context 'when subscription is discarded' do
+    before { subscription.discard! }
 
-    it 'updates delivery_attempted_at' do
-      expect(notification.reload.delivery_attempted_at).not_to be_nil
+    it 'raises a Record Not Found error' do
+      expect { perform! }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
