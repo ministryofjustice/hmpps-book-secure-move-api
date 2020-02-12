6a52feac46d5e8b9d24c58160d78b747fb2149a4
Stephen Dicks <stephen@starswan.com>
2020-02-12 13:50:45 +0000

    P4-940 added can_upload_documents to Location model and exposed via API

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :key, :title, :location_type, :nomis_agency_id, :disabled_at, :suppliers
+  attributes :id, :key, :title, :location_type, :nomis_agency_id, :can_upload_documents, :disabled_at, :suppliers
 
   def suppliers
     object.suppliers.each { |supplier| SupplierSerializer.new(supplier) }
