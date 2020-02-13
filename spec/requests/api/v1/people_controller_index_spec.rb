1c888b9bc8e7484d36c161dcf95273e4726bae7d
Alessandro De Simone <alexdesi@users.noreply.github.com>
2020-02-13 14:16:18 +0000

    Add support for nomis offender id to v1 people endpoint (#258)
    * Add filter by nomis_offender_no to People::Finder
    
    * - Add search by nomis_offender_no in the controller
    - Sync people when person_nomis_prison_number is used
    - Small Refactor in Moves::Importer

--- spec/requests/api/v1/people_controller_index_spec.rb
+++ spec/requests/api/v1/people_controller_index_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Api::V1::PeopleController, with_client_authentication: true do
   let(:content_type) { ApiController::CONTENT_TYPE }
   let(:response_json) { JSON.parse(response.body) }
 
+  let(:schema) { load_json_schema('get_people_responses.json') }
+
   describe 'GET /people' do
-    let(:schema) { load_json_schema('get_people_responses.json') }
-
-    let!(:people) { create_list :person, 5 }
-
     describe 'filtering the results' do
+      let!(:people) { create_list :person, 5 }
+
       let(:params) { { filter: { police_national_computer: 'AB/1234567' } } }
 
       context 'when called with police_national_computer filter' do
@@ -30,6 +30,24 @@ RSpec.describe Api::V1::PeopleController, with_client_authentication: true do
         get '/api/v1/people', headers: headers, params: params
 
         expect(People::Finder).to have_received(:new).with(police_national_computer: 'AB/1234567')
+      end
+    end
+
+    context 'when Nomis offender number no is provided' do
+      let(:nomis_offender_no) { 'G5033UT' }
+      let(:params) { { filter: { nomis_offender_no: 'G5033UT' } } }
+      let(:people_finder) { instance_double('People::Finder', call: Person.all) }
+
+      before do
+        allow(People::Finder).to receive(:new).and_return(people_finder)
+        allow(Moves::ImportPeople).to receive(:new).with([person_nomis_prison_number: nomis_offender_no])
+                                                   .and_return(instance_double('Moves::ImportPeople', call: nil))
+      end
+
+      it 'requests data from NOMIS', with_json_schema: true do
+        get '/api/v1/people', headers: headers, params: params
+
+        expect(response).to have_http_status(:ok)
       end
     end
   end
