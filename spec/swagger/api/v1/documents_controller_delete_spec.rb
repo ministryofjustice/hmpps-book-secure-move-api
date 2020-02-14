1767a698a9fd1158032e4268f0b2bfb4f46ebdc3
Stephen Dicks <stephen@starswan.com>
2020-02-14 16:10:00 +0000

    added Rswag test for delete document from move

--- spec/swagger/api/v1/documents_controller_delete_spec.rb
+++ spec/swagger/api/v1/documents_controller_delete_spec.rb
@@ -2,15 +2,17 @@
 
 require 'swagger_helper'
 
-RSpec.describe Api::V1::MovesController, :with_client_authentication, :rswag do
+RSpec.describe Api::V1::DocumentsController, :rswag, :with_client_authentication, type: :request do
   let(:headers) { { 'CONTENT_TYPE': content_type }.merge(auth_headers) }
   let(:content_type) { ApiController::CONTENT_TYPE }
-  let(:move) { create :move }
+  let!(:move) { create(:move, documents: [build(:document)]) }
   let(:move_id) { move.id }
+  let(:document) { move.documents.first }
+  let(:document_id) { document.id }
 
-  path '/moves/{move_id}' do
-    get 'Returns the details of a move' do
-      tags 'Moves'
+  path '/moves/{move_id}/documents/{document_id}' do
+    delete 'deletes a document from a move' do
+      tags 'Documents'
       produces 'application/vnd.api+json'
 
       parameter name: :Authorization,
@@ -44,22 +46,27 @@ RSpec.describe Api::V1::MovesController, :with_client_authentication, :rswag do
                 example: '00525ecb-7316-492a-aae2-f69334b2a155',
                 required: true
 
+      parameter name: :document_id,
+                in: :path,
+                description: 'The ID of the document',
+                schema: {
+                  type: :string,
+                },
+                format: 'uuid',
+                example: '00525ecb-7316-492a-aae2-f69334b2a155',
+                required: true
+
       response '200', 'success' do
         let(:resource_to_json) do
-          JSON.parse(ActionController::Base.render(json: move, include: MoveSerializer::INCLUDED_ATTRIBUTES))
+          JSON.parse(ActionController::Base.render(json: document))
         end
 
-        schema "$ref": '#/definitions/get_move_responses/200'
+        schema "$ref": '#/definitions/delete_document_responses/200'
 
         run_test! do |_example|
           expect(response.headers['Content-Type']).to match(Regexp.escape(content_type))
 
           expect(JSON.parse(response.body)).to eq resource_to_json
-
-          # TODO: this was commented out in the original test, and fails when included
-          # expect(Moves::NomisSynchroniser).to(
-          #     have_received(:new).with(locations: [move.from_location], date: move.date)
-          #   )
         end
       end
 
@@ -67,21 +74,18 @@ RSpec.describe Api::V1::MovesController, :with_client_authentication, :rswag do
         let(:Authorization) { "Basic #{::Base64.strict_encode64('bogus-credentials')}" }
 
         it_behaves_like 'a swagger 401 error'
-        it_behaves_like 'it does not trigger NomisSynchroniser'
       end
 
       response '404', 'not found' do
-        let(:move_id) { SecureRandom.uuid }
-        let(:detail_404) { "Couldn't find Move with 'id'=#{move_id}" }
+        let(:document_id) { SecureRandom.uuid }
+        let(:detail_404) { "Couldn't find Document with 'id'=#{document_id}" }
 
         it_behaves_like 'a swagger 404 error'
-        it_behaves_like 'it does not trigger NomisSynchroniser'
       end
 
       response '415', 'invalid content type' do
         let(:"Content-Type") { 'application/xml' }
         it_behaves_like 'a swagger 415 error'
-        it_behaves_like 'it does not trigger NomisSynchroniser'
       end
     end
   end
