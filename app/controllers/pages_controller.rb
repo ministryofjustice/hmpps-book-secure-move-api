4228ab0e1e1a8dbbfa13706e54195acf4e77f75e
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2020-02-20 12:07:55 +0000

    Revert p4 999 docs (#282)

--- app/controllers/pages_controller.rb
+++
@@ -1,8 +0,0 @@
-# This has to be overridden as our ApplicationController
-# doesn't inherit from ActionController::Base
-
-# rubocop:disable Rails/ApplicationController
-class PagesController < ActionController::Base
-  include HighVoltage::StaticPage
-end
-# rubocop:enable Rails/ApplicationController
