522f09a4f6e320c4f7b25d9e2afa53222da2abea
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-02-24 15:45:19 +0000

    better handling of failed webhook status
    P4-1074

--- app/jobs/notify_job.rb
+++ app/jobs/notify_job.rb
@@ -5,18 +5,29 @@ class NotifyJob < ApplicationJob
 
   def perform(notification_id:)
     notification = Notification.kept.includes(:subscription).find(notification_id)
-
     return unless notification.subscription.enabled?
 
     data = ActiveModelSerializers::Adapter.create(NotificationSerializer.new(notification)).to_json
     hmac = Encryptor.hmac(notification.subscription.secret, data)
+    success = false # assume failure
 
-    response = client.post(notification.subscription.callback_url, data, 'PECS-SIGNATURE': hmac, 'PECS-NOTIFICATION-ID': notification_id)
-
-    notification.delivered_at = DateTime.now if response.success?
+    begin
+      response = client.post(notification.subscription.callback_url, data, 'PECS-SIGNATURE': hmac, 'PECS-NOTIFICATION-ID': notification_id)
+      if response.success?
+        notification.delivered_at = DateTime.now
+        success = true
+      else
+        puts "Error: non-success status received from #{notification.subscription.callback_url} (#{response.status})"
+      end
+    rescue Faraday::ClientError => e
+      puts "Error notifying #{notification.subscription.callback_url}: #{e.inspect}"
+    end
 
     notification.update(delivery_attempts: notification.delivery_attempts.succ,
                         delivery_attempted_at: DateTime.now)
+
+    # It is necessary to raise an error in order for Sidekiq to retry the notification
+    raise 'Notification failed' unless success
   end
 
 private
