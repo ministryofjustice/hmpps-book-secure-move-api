3a3c6cfa98471cba28f9d05f870f5b0d2b71a9c3
Stephen Dicks <stephen.dicks@digital.justice.gov.uk>
2020-03-04 12:12:05 +0000

    add faker data to locations and profiles (#315)

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
   factory :person_without_profiles, class: Person
   factory :profile do
     association(:person, factory: :person_without_profiles)
-    first_names { 'Bob' }
-    last_name { 'Roberts' }
+    first_names { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     date_of_birth { Date.new(1980, 10, 20) }
     profile_identifiers {
       [{ identifier_type: 'police_national_computer', value: 'AB/1234567' },
