ca8462822ab5d4d832b0c97658521e05d9ca8301
Stephen Dicks <stephen@starswan.com>
2020-03-06 07:02:40 +0000

    P4 982 azure app insights

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 cd /usr/src/app
 
 bundle exec rake db:create db:migrate
+# Sadly toml files don't support inline expansion of environment variables
+bundle exec erb config/application_insights.erb >config/application_insights.toml
 bundle exec puma -p $PUMA_PORT
