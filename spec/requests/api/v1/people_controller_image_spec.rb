a96c1c1a0459616a57a879a9a0dc28d7258b53b4
Alexdesi <alexdesi@gmail.com>
2020-03-13 14:12:37 +0000

    Pluralize image endpoint

--- spec/requests/api/v1/people_controller_image_spec.rb
+++ spec/requests/api/v1/people_controller_image_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::PeopleController do
-  describe 'GET /people/:id/image' do
+  describe 'GET /people/:id/images' do
     let(:token) { create(:access_token) }
 
     context 'when there is no image' do
@@ -16,14 +16,14 @@ RSpec.describe Api::V1::PeopleController do
       it 'returns resource not found 404' do
         id = 'non-existent-id'
 
-        get "/api/v1/people/#{id}/image", params: { access_token: token.token }
+        get "/api/v1/people/#{id}/images", params: { access_token: token.token }
 
         expect(response).to have_http_status(:not_found)
       end
     end
 
     context 'with an image' do
-      subject(:get_image) { get "/api/v1/people/#{person.id}/image", params: { access_token: token.token } }
+      subject(:get_image) { get "/api/v1/people/#{person.id}/images", params: { access_token: token.token } }
 
       let!(:person) { create(:profile, :nomis_synced).person }
       let(:image_data) { File.read('spec/fixtures/Arctic_Tern.jpg') }
