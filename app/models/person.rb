ca83b4411d08c8872e22b6430bb187f8acce616a
Alessandro De Simone <alexdesi@users.noreply.github.com>
2020-03-16 16:02:15 +0000

    Save picture in S3 and return S3 URL (#332)
    * Save picture in S3 and return S3 URL
    
    * Add support for json:api in GET /people/id/images
    
    * Pluralize image endpoint
    
    * Remove image_url form /people endpoint
    
    * Update .rubocop.yml
    
    * Refactor image_url

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,7 +4,26 @@ class Person < VersionedModel
   has_many :profiles, dependent: :destroy
   has_many :moves, dependent: :destroy
 
+  has_one_attached :image
+
   def latest_profile
     profiles.last
+  end
+
+  def latest_nomis_booking_id
+    latest_profile.latest_nomis_booking_id
+  end
+
+  def attach_image(image_blob)
+    filename = id + '.jpg'
+
+    tempfile = Tempfile.new('temp.jpg').binmode
+    tempfile.write image_blob
+    tempfile.rewind
+
+    image.attach(io: tempfile, filename: filename, content_type: 'image/jpg')
+    tempfile.close
+
+    filename
   end
 end
