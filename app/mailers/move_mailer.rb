39457f1cf773754b7902d52706dd3cb4a141fc0e
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-03-26 07:56:11 +0000

    P4-1188-tweaks (#347)

--- app/mailers/move_mailer.rb
+++ app/mailers/move_mailer.rb
@@ -11,8 +11,8 @@ class MoveMailer < GovukNotifyRails::Mailer
       set_personalisation(
         'move-reference': move.reference,
         'from-location': move.from_location.title,
-        # to_location isn't set for prison_recall moves
-        'to-location': move.to_location&.title,
+        # NB: to_location isn't set for prison_recall moves, so use N/A instead (GovUK Notify will error if nil is supplied)
+        'to-location': move.to_location&.title || 'N/A',
         'move-date': move.date.strftime(DATE_FORMAT),
         'move-created-at': move.created_at.strftime(TIME_FORMAT),
         'move-updated-at': move.updated_at.strftime(TIME_FORMAT),
