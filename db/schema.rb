422f1091ad98adc582522974cf3f59ffb8850604
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-03-30 13:06:04 +0100

    remove migration (use rake task instead)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_132030) do
+ActiveRecord::Schema.define(version: 2020_03_04_121217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
