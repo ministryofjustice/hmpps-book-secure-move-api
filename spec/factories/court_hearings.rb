1edab511817a3e886d984402f727881b225f9356
William Fish <william.michael.fish@gmail.com>
2020-04-09 09:02:01 +0100

    Adds start_time to court_hearing factory

--- spec/factories/court_hearings.rb
+++ spec/factories/court_hearings.rb
@@ -2,10 +2,14 @@ FactoryBot.define do
   factory :court_hearing do
     move
 
+    start_time { '2018-01-01T18:57Z' }
     nomis_case_number { 'MyString' }
     court_type { 'MyString' }
     comments { 'MyText' }
     nomis_case_id { 1 }
+  end
+
+  trait :saved_to_nomis do
     nomis_hearing_id { 1 }
     saved_to_nomis { false }
   end
