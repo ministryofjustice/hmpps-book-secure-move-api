0e6984dc260bc837ff92a54271525e624edf6024
William Fish <william.michael.fish@gmail.com>
2020-04-10 16:38:39 +0100

    Typo

--- app/controllers/api/v1/court_hearings_controller.rb
+++ app/controllers/api/v1/court_hearings_controller.rb
@@ -25,7 +25,7 @@ module Api
       end
 
       def move
-        return {} if params.require(:data)[:relationships].blank?
+        return if params.require(:data)[:relationships].blank?
 
         relationship_params = params.require(:data).require(:relationships)
 
