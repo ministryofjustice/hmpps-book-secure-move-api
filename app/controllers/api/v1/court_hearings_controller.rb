e44a1854d6199259d26bfbc86a08f1bc0f66944d
William Fish <william.michael.fish@gmail.com>
2020-04-10 16:32:53 +0100

    Adds ability to associate an existing move with a new court hearing

--- app/controllers/api/v1/court_hearings_controller.rb
+++ app/controllers/api/v1/court_hearings_controller.rb
@@ -2,12 +2,16 @@ module Api
   module V1
     class CourtHearingsController < ApiController
       def create
-        court_hearing = CourtHearing.create!(court_hearings_params)
+        court_hearing = CourtHearing.create!(court_hearings_attributes)
 
         render json: court_hearing, status: :created
       end
 
     private
+
+      def court_hearings_attributes
+        court_hearings_params.merge(move: move)
+      end
 
       def court_hearings_params
         params.require(:data).require(:attributes).permit(
@@ -18,6 +22,25 @@ module Api
           :case_type,
           :comments,
         )
+      end
+
+      def move
+        return if relationship_params.blank?
+
+        id = relationship_params.require(:moves).require(:data).permit(:id, :type)[:id]
+
+        Move.find(id)
+      end
+
+      def relationship_params
+        @relationship_params ||=
+          begin
+            if params.require(:data).fetch(:relationships, nil)
+              params.require(:data).require(:relationships)
+            else
+              {}
+            end
+          end
       end
     end
   end
---
+++ spec/requests/api/v1/court_hearings_controller_create_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::MovesController do
+  let(:response_json) { JSON.parse(response.body) }
+
+  describe 'POST /court_hearings' do
+    let(:court_hearing_attributes) do
+      {
+        'start_time': '2018-01-01T18:57Z',
+        'case_start_date': '2018-01-01',
+        'case_number': 'T32423423423',
+        'nomis_case_id': '4232423',
+        'case_type': 'Adult',
+        'comments': 'Witness for Foo Bar',
+      }
+    end
+
+    let(:data) do
+      {
+        type: 'court_hearings',
+        attributes: court_hearing_attributes,
+      }
+    end
+
+    let(:supplier) { create(:supplier) }
+    let(:application) { create(:application, owner_id: supplier.id) }
+    let(:access_token) { create(:access_token, application: application).token }
+    let(:headers) { { 'CONTENT_TYPE': content_type,  }.merge('Authorization' => "Bearer #{access_token}") }
+    let(:content_type) { ApiController::CONTENT_TYPE }
+
+    it "returns 201" do
+      post '/api/v1/court_hearings', params: { data: data }, headers: headers, as: :json
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it "creates a court_hearing" do
+      expect { post '/api/v1/court_hearings', params: { data: data }, headers: headers, as: :json }.
+        to change(CourtHearing, :count).by(1)
+    end
+
+    context "when a move relationship is passed" do
+      let(:move) { create(:move) }
+
+      let(:data) do
+        {
+          type: 'court_hearings',
+          attributes: court_hearing_attributes,
+          relationships: { moves: { data: { type: 'moves', id: move.id } }, }
+        }
+      end
+
+      it 'sets the correct relationship with the move' do
+        post '/api/v1/court_hearings', params: { data: data }, headers: headers, as: :json
+
+        court_hearing = CourtHearing.find(response_json['data']['id'])
+
+        expect(move.court_hearings).to include(court_hearing)
+      end
+    end
+  end
+end
