3cb353dd65ab6389a6972bfd7b00cec995ff99d8
Alessandro De Simone <alexdesi@users.noreply.github.com>
2020-04-10 14:20:40 +0100

    Add service CreateHearingInNomis (#366)

---
+++ app/lib/nomis_client/court_hearing.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module NomisClient
+  class CourtHearing < NomisClient::Base
+    class << self
+      def post(booking_id:, court_case_id:, body_params: {})
+        court_cases_route = "/bookings/#{booking_id}/court-cases/#{court_case_id}/prison-to-court-hearings"
+
+
+        NomisClient::Base.post(court_cases_route, body: body_params.to_json).body
+      end
+    end
+  end
+end
---
+++ spec/services/court_hearings/create_in_nomis_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe CourtHearings::CreateInNomis do
+  context 'when court hearing are valid' do
+    subject(:create_hearing_in_nomis) { described_class.new.call(move, court_hearings) }
+
+    let(:move) {
+      create(:move,
+             from_location: create(:location, nomis_agency_id: from_nomis_agency_id),
+             to_location: create(:location, nomis_agency_id: to_nomis_agency_id))
+    }
+
+    let(:court_hearings) {
+      [
+        create(:court_hearing, nomis_case_id: nomis_case_id, move: move, start_time: hearing_date_time, comments: comments),
+      ]
+    }
+
+    let(:nomis_case_id) { 1111111 }
+    let(:from_nomis_agency_id) { 'LEI' }
+    let(:to_nomis_agency_id) { 'LEEDCC' }
+    let(:hearing_date_time) { Time.new(2020, 12, 1).iso8601 }
+    let(:comments) { 'Restricted access to parking level.' }
+    let(:booking_id) { 123 }
+
+    before do
+      allow(NomisClient::CourtHearing).to receive(:post)
+      move.person.latest_profile.update(latest_nomis_booking_id: booking_id)
+    end
+
+    it 'creates the court hearings in Nomis' do
+      create_hearing_in_nomis
+
+      expect(NomisClient::CourtHearing).to have_received(:post).with(
+        booking_id: booking_id,
+        court_case_id: nomis_case_id,
+        body_params: {
+            'fromPrisonLocation': from_nomis_agency_id, 'toCourtLocation': to_nomis_agency_id,
+            'courtHearingDateTime': hearing_date_time, 'comments': comments
+        },
+      )
+    end
+  end
+end
