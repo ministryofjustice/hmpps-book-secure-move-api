e554de50efa2d47fd3cf91ec13ca6a7847a35c3b
William Fish <william.michael.fish@gmail.com>
2020-04-14 11:37:17 +0100

    Fixes broken specs

--- spec/swagger/api/v1/court_hearings_controller_create_spec.rb
+++ spec/swagger/api/v1/court_hearings_controller_create_spec.rb
@@ -1,58 +1,56 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe Api::V1::CourtHearingsController do
+RSpec.describe Api::V1::CourtHearingsController, :with_client_authentication, :rswag, type: :request do
   let(:response_json) { JSON.parse(response.body) }
 
-  context 'with swagger generation', :with_client_authentication, :rswag do
-    path '/court_hearings' do
-      post 'Creates a court hearing' do
-        tags 'CourtHearings'
-        consumes 'application/vnd.api+json'
-        produces 'application/vnd.api+json'
+  path '/court_hearings' do
+    post 'Creates a court hearing' do
+      tags 'CourtHearings'
+      consumes 'application/vnd.api+json'
+      produces 'application/vnd.api+json'
 
-        parameter name: :Authorization,
-                  in: :header,
-                  schema: {
-                    type: 'string',
-                    default: 'Bearer <your-client-token>',
-                  },
-                  required: true,
-                  description: <<~DESCRIPTION
+      parameter name: :Authorization,
+        in: :header,
+        schema: {
+          type: 'string',
+          default: 'Bearer <your-client-token>',
+        },
+        required: true,
+        description: <<~DESCRIPTION
                     This is "Bearer ", followed by your OAuth 2 Client token.
                     If you're testing interactively in the web UI, you can ignore this field
-                  DESCRIPTION
+        DESCRIPTION
 
         parameter name: :court_hearing,
-                  description: 'The court hearing to create',
-                  in: :body,
-                  attributes: {
-                    schema: '#/definitions/court_hearing/CourtHearing',
-                  }
-
-        let(:court_hearing) do
-          {
-            data: {
-              type: 'court_hearings',
-              attributes: {
-                'start_time': '2018-01-01T18:57Z',
-                'case_start_date': '2018-01-01',
-                'case_number': 'T32423423423',
-                'nomis_case_id': '4232423',
-                'case_type': 'Adult',
-                'comments': 'Witness for Foo Bar',
-              },
-            },
+          description: 'The court hearing to create',
+          in: :body,
+          attributes: {
+            schema: '#/definitions/court_hearing/CourtHearing',
           }
-        end
 
-        response '201', 'created' do
-          schema '$ref': '#/definitions/post_court_hearing_responses/201'
+          let(:court_hearing) do
+            {
+              data: {
+                type: 'court_hearings',
+                attributes: {
+                  'start_time': '2018-01-01T18:57Z',
+                  'case_start_date': '2018-01-01',
+                  'case_number': 'T32423423423',
+                  'nomis_case_id': '4232423',
+                  'case_type': 'Adult',
+                  'comments': 'Witness for Foo Bar',
+                },
+              },
+            }
+          end
 
-          run_test!
-        end
-      end
+          response '201', 'created' do
+            schema '$ref': '#/definitions/post_court_hearing_responses/201'
+
+            run_test!
+          end
     end
   end
 end
