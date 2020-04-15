494fa295f410105819e8e56e1aecf54606f42ede
Alessandro De Simone <alexdesi@users.noreply.github.com>
2020-04-15 18:05:59 +0100

    Save the CourtHearing.post response and Fix the time format (#370)
    *  Fix the time format
    * Save the CourtHearing.post response
    
    Co-authored-by: William Fish <william.michael.fish@gmail.com>

--- spec/services/court_hearings/create_in_nomis_spec.rb
+++ spec/services/court_hearings/create_in_nomis_spec.rb
@@ -19,26 +19,59 @@ RSpec.describe CourtHearings::CreateInNomis do
     let(:nomis_case_id) { 1111111 }
     let(:from_nomis_agency_id) { 'LEI' }
     let(:to_nomis_agency_id) { 'LEEDCC' }
-    let(:hearing_date_time) { Time.new(2020, 12, 1).iso8601 }
+    let(:hearing_date_time) { Time.parse('2020-04-15T17:36:02+01:00') }
     let(:comments) { 'Restricted access to parking level.' }
     let(:booking_id) { 123 }
 
     before do
       allow(NomisClient::CourtHearing).to receive(:post)
+                                              .and_return(instance_double('OAuth2::Response', status: nomis_response_status, body: { 'id' => 123 }.to_json))
       move.person.latest_profile.update(latest_nomis_booking_id: booking_id)
     end
 
-    it 'creates the court hearings in Nomis' do
-      create_hearing_in_nomis
+    context 'when Nomis return 201 success' do
+      let(:nomis_response_status) { 201 }
 
-      expect(NomisClient::CourtHearing).to have_received(:post).with(
-        booking_id: booking_id,
-        court_case_id: nomis_case_id,
-        body_params: {
-            'fromPrisonLocation': from_nomis_agency_id, 'toCourtLocation': to_nomis_agency_id,
-            'courtHearingDateTime': hearing_date_time, 'comments': comments
-        },
-      )
+      it 'creates the court hearings in Nomis' do
+        create_hearing_in_nomis
+
+        expect(NomisClient::CourtHearing).to have_received(:post).with(
+          booking_id: booking_id,
+          court_case_id: nomis_case_id,
+          body_params: {
+              'fromPrisonLocation': from_nomis_agency_id, 'toCourtLocation': to_nomis_agency_id,
+              'courtHearingDateTime': hearing_date_time.utc.iso8601, 'comments': comments
+          },
+        )
+      end
+
+      it 'updates the nomis_hearing_id and saved_to_nomis' do
+        create_hearing_in_nomis
+
+        court_hearing = court_hearings.first
+        expect(court_hearing.saved_to_nomis).to eq true
+        expect(court_hearing.nomis_hearing_id).to eq 123
+      end
+    end
+
+    context 'when Nomis returns an error' do
+      let(:nomis_response_status) { 400 }
+
+      it 'pushes error the warning to Sentry' do
+        allow(Raven).to receive(:capture_message)
+
+        create_hearing_in_nomis
+
+        expect(Raven).to have_received(:capture_message)
+      end
+
+      it 'does NOT set nomis_hearing_id and saved_to_nomis' do
+        create_hearing_in_nomis
+
+        court_hearing = court_hearings.first
+        expect(court_hearing.saved_to_nomis).to eq false
+        expect(court_hearing.nomis_hearing_id).to be_nil
+      end
     end
   end
 end
