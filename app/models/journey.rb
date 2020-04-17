98de00dfcb25336105e915765eeb5ebcabbb5808
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-04-17 16:01:45 +0100

    Update journey.rb
    add comment

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,3 +1,22 @@
+# A Journey is the path (or intended path) between two locations.
+#
+# A Move is composed of one or more Journeys; each individual journey record indicates the path and whether it is billable.
+# Moves can be redirected for various reasons (e.g. supplier is late and the prison is closed; or the PMU call to say the prison is full and
+# the person should be taken to a different location). Sometimes these journeys are billable and sometimes not.
+#
+# Journeys are ordered chronologically by client_timestamp (as opposed to created_at), to allow for queueing and transmission delays in the system sending the journey record.
+# Journeys have an explicit link to a supplier; whereas moves currently do not.
+#
+# Example 1: a move: A--> B is redirected to C by PMU before the supplier arrives at B.
+# Journey1: A --> B (billable, cancelled)
+# Journey2: B --> C (billable, completed)
+# The supplier is paid for two journeys A-->B and B-->C.
+#
+# Example 2: a move: A--> B is redirected to C because the supplier arrived late and was locked out.
+# Journey1: A --> B (billable, completed)
+# Journey2: B --> C (not billable, completed)
+# The supplier is paid for only the first journey A-->B but not B-->C.
+#
 class Journey < ApplicationRecord
   belongs_to :move
   belongs_to :supplier
