c79dd59993a8b88c8ef09ab459366f819c546513
Andrew Retmanski <aretmanski@gmail.com>
2020-04-20 09:32:55 +0100

    Add new endpoint to GET /allocations Also includes a new rake task to setup some fake allocation data - run `bundle exec rake fake_data:create_allocations`
    Request specs will be added in a follow up PR - I want to fix the mess of duplicated swagger definitions first.

---
+++ app/controllers/api/v1/allocations_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class AllocationsController < ApiController
+      def index
+        allocations_params = Allocations::ParamsValidator.new(filter_params)
+        if allocations_params.valid?
+          allocations = Allocations::Finder.new(filter_params).call
+          paginate allocations, include: AllocationSerializer::INCLUDED_ATTRIBUTES
+        else
+          render json: { error: allocations_params.errors }, status: :bad_request
+        end
+      end
+
+      private
+
+      def filter_params
+        params.fetch(:filter, {}).permit(:date_from, :date_to).to_h
+      end
+    end
+  end
+end
---
+++ app/serializers/allocation_serializer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AllocationSerializer < ActiveModel::Serializer
+  attributes :moves_count, :date, :prisoner_category, :sentence_length, :complex_cases, :complete_in_full, :other_criteria
+
+  has_one :from_location
+  has_one :to_location
+
+  INCLUDED_ATTRIBUTES = [:from_location, :to_location]
+end
