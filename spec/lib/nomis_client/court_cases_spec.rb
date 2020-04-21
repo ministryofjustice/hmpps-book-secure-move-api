f4a108b5fec7b9216631f51c97ae0785fbdf34e4
William Fish <william.michael.fish@gmail.com>
2020-04-21 16:12:44 +0100

    [P4-1264] Court case filters (#388)
    * Adds filter for active court cases

--- spec/lib/nomis_client/court_cases_spec.rb
+++ spec/lib/nomis_client/court_cases_spec.rb
@@ -7,13 +7,36 @@ RSpec.describe NomisClient::CourtCases, with_nomis_client_authentication: true d
     let(:nomis_client) { class_double(NomisClient::Base).as_stubbed_const }
 
     let(:booking_id) { '1495077' }
+    let(:filter_params) { nil }
 
     it 'calls the nomis client with the correct booking id' do
       allow(nomis_client).to receive(:get).and_return(instance_double('OAuth2::Response', body: response_body))
 
-      described_class.get(booking_id)
+      described_class.get(booking_id, filter_params)
 
-      expect(nomis_client).to have_received(:get).with("/bookings/#{booking_id}/court-cases?activeOnly=true")
+      expect(nomis_client).to have_received(:get).with("/bookings/#{booking_id}/court-cases")
+    end
+
+    context 'when no filter_params are passed' do
+      it 'returns active court cases' do
+        allow(nomis_client).to receive(:get).and_return(instance_double('OAuth2::Response', body: response_body))
+
+        described_class.get(booking_id)
+
+        expect(nomis_client).to have_received(:get).with("/bookings/#{booking_id}/court-cases?activeOnly=true")
+      end
+    end
+
+    context 'when filter_params are present' do
+      let(:filter_params) { ActionController::Parameters.new(active: 'true') }
+
+      it 'returns active court cases' do
+        allow(nomis_client).to receive(:get).and_return(instance_double('OAuth2::Response', body: response_body))
+
+        described_class.get(booking_id, filter_params)
+
+        expect(nomis_client).to have_received(:get).with("/bookings/#{booking_id}/court-cases?activeOnly=true")
+      end
     end
   end
 end
--- spec/services/people/retrieve_court_cases_spec.rb
+++ spec/services/people/retrieve_court_cases_spec.rb
@@ -5,13 +5,14 @@ require 'rails_helper'
 RSpec.describe People::RetrieveCourtCases do
   let(:person) { instance_double('Person', latest_nomis_booking_id: '12345') }
   let(:response_body) { file_fixture('nomis_get_court_cases_200.json').read }
+  let(:filter_params) { double }
 
   before do
     class_double(NomisClient::CourtCases, get: response_body).as_stubbed_const
   end
 
   it 'returns an array of CourtCase' do
-    court_cases_response = described_class.call(person)
+    court_cases_response = described_class.call(person, filter_params)
 
     expect(court_cases_response).to all(be_a(CourtCase))
   end
