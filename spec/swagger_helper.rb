725ec960f3e81848f792da76e2bb8c0baf413c2e
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-04-22 14:45:56 +0100

    remove duplicate locations, move to examples

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -106,10 +106,10 @@ RSpec.configure do |config|
             "$ref": 'gender.json#/Gender',
           },
           Location: {
-            "$ref": 'location.json#/Location1',
+            "$ref": 'location.json#/Location',
           },
           LocationReference: {
-            "$ref": 'location_reference.json#/LocationReference1',
+            "$ref": 'location_reference.json#/LocationReference',
           },
           Lockout: {
             "$ref": 'lockout.json#/GetLockout',
