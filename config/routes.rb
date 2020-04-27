cc577ef0510ee6ee4514b57289b984cebf9df87c
William Fish <william.michael.fish@gmail.com>
2020-04-27 16:54:31 +0100

    More spec fixes

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,9 @@ Rails.application.routes.draw do
       resources :documents, only: %i[create]
       resources :court_hearings, only: %i[create]
       resources :people, only: %i[index create update] do
-        get 'images', to: 'people#court_cases'
-        get 'court_cases'
-        get 'timetable'
+        get 'images', to: 'people#image'
+        get 'court_cases', to: 'people#court_cases'
+        get 'timetable', to: 'people#timetable'
       end
       resources :moves, only: %i[index show create destroy update] do
         resources :documents, only: %i[create destroy]
