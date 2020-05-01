e25b3f838a97f62e3eb47227a18ecbc6edc1e7f5
Alexdesi <alexdesi@gmail.com>
2020-05-01 13:48:53 +0100

    Add validator for NomisProfileValidator

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class PeopleController < ApiController
       before_action :validate_timetable_filter_params, only: [:timetable]
-      before_action :validate_nomis_booking_id, only: %i[court_cases image timetable]
+      before_action :validate_nomis_profile, only: %i[court_cases image timetable]
 
       def index
         prison_number = filter_params[:prison_number]
@@ -133,7 +133,7 @@ module Api
         end
       end
 
-      def validate_nomis_booking_id
+      def validate_nomis_profile
         profile_validator = People::NomisProfileValidator.new(person.latest_profile)
         profile_validator.validate!
       end
