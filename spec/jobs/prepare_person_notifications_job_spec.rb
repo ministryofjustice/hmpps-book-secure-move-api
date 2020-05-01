9f68dca639750c3e8b8ce8192fa552939c6371a0
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-05-01 10:26:28 +0100

    add tests for PreparePersonNotificationsJob

--- spec/jobs/prepare_person_notifications_job_spec.rb
+++ spec/jobs/prepare_person_notifications_job_spec.rb
@@ -1,5 +1,36 @@
 # frozen_string_literal: true
 
 RSpec.describe PreparePersonNotificationsJob, type: :job do
-  # TODO: add some tests
+  subject(:perform) { described_class.new.perform(topic_id: person.id, action_name: action_name) }
+
+  let(:person) { create :person }
+  let!(:move1) { create :move, person: person }
+  let!(:move2) { create :move,  person: person }
+  let!(:other_move) { create :move }
+
+  before do
+    allow(PrepareMoveNotificationsJob).to receive(:perform_now)
+    perform
+  end
+
+  context 'when updating a person' do
+    let(:action_name) { 'update' }
+
+    it 'calls PrepareMoveNotificationsJob for the related moves but not for the unrelated moves' do
+      expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move1.id, action_name: action_name)
+      expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move2.id, action_name: action_name)
+      expect(PrepareMoveNotificationsJob).not_to have_received(:perform_now).with(topic_id: other_move.id, action_name: action_name)
+    end
+
+  end
+
+  context 'when creating a person' do
+    let(:action_name) { 'create' }
+
+    it 'calls PrepareMoveNotificationsJob for the related moves but not for the unrelated moves' do
+      expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move1.id, action_name: action_name)
+      expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move2.id, action_name: action_name)
+      expect(PrepareMoveNotificationsJob).not_to have_received(:perform_now).with(topic_id: other_move.id, action_name: action_name)
+    end
+  end
 end
