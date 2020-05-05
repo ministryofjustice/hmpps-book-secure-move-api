44bd264f864fe4c2c81693d748b2017916d87e46
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-05-05 16:41:20 +0100

    small test refactor

--- spec/jobs/prepare_person_notifications_job_spec.rb
+++ spec/jobs/prepare_person_notifications_job_spec.rb
@@ -13,23 +13,23 @@ RSpec.describe PreparePersonNotificationsJob, type: :job do
     perform
   end
 
-  context 'when updating a person' do
-    let(:action_name) { 'update' }
-
-    describe 'it calls PrepareMoveNotificationsJob for the related moves' do
-      it { expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move1.id, action_name: action_name) }
-      it { expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move2.id, action_name: action_name) }
-      it { expect(PrepareMoveNotificationsJob).not_to have_received(:perform_now).with(topic_id: other_move.id, action_name: action_name) }
-    end
+  shared_examples 'it calls PrepareMoveNotificationsJob for the related moves' do
+    it { expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move1.id, action_name: action_name) }
+    it { expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move2.id, action_name: action_name) }
+    it { expect(PrepareMoveNotificationsJob).not_to have_received(:perform_now).with(topic_id: other_move.id, action_name: action_name) }
   end
 
+  # NB: we are testing here that creating a person and updating a person behave in the same way from the perspective of PreparePersonNotificationsJob.
+  # NB: note however that the PeopleController however does not call PreparePersonNotificationsJob on creating a person, to reduce duplicate emails.
   context 'when creating a person' do
     let(:action_name) { 'create' }
 
-    describe 'it calls PrepareMoveNotificationsJob for the related moves' do
-      it { expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move1.id, action_name: action_name) }
-      it { expect(PrepareMoveNotificationsJob).to have_received(:perform_now).with(topic_id: move2.id, action_name: action_name) }
-      it { expect(PrepareMoveNotificationsJob).not_to have_received(:perform_now).with(topic_id: other_move.id, action_name: action_name) }
-    end
+    it_behaves_like 'it calls PrepareMoveNotificationsJob for the related moves'
+  end
+
+  context 'when updating a person' do
+    let(:action_name) { 'update' }
+
+    it_behaves_like 'it calls PrepareMoveNotificationsJob for the related moves'
   end
 end
