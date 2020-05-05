1ee0c07da988f3d68204500e807763a61fabd67a
William Fish <william.michael.fish@gmail.com>
2020-05-05 17:18:02 +0100

    Inlcude location in CourtCaseSerializer spec

--- spec/serializers/court_case_serializer_spec.rb
+++ spec/serializers/court_case_serializer_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe FastJsonapi::CourtCaseSerializer do
                                    'agency' => { 'agencyId' => location.nomis_agency_id })
   }
 
-  let(:options) { {} }
+  let(:options) { { include: [:location] } }
 
   let(:location) { create :location, nomis_agency_id: 'SNARCC' }
 
@@ -29,17 +29,33 @@ RSpec.describe FastJsonapi::CourtCaseSerializer do
           nomis_case_id: '111',
           nomis_case_status: 'ACTIVE',
           case_start_date: '2016-11-14',
-          case_number: 'T20167984'
+          case_number: 'T20167984',
         },
         relationships: {
           location: {
             data: {
               id: location.id,
-              type: :location
-            }
-          }
-        }
-      }
+              type: :location,
+            },
+          },
+        },
+      },
+      included: [
+        {
+          type: :locations,
+          attributes: {
+            can_upload_documents: false,
+            disabled_at: nil,
+            id: location.id,
+            key: 'key_1',
+            location_type: 'prison',
+            nomis_agency_id: 'SNARCC',
+            title: location.title,
+          },
+          id: location.id,
+          relationships: { suppliers: { data: [] } },
+        },
+      ],
     }
 
     expect(court_case_deserialized).to eq(expected)
