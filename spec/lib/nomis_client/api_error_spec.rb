5f5a789b7874f0689a2e9041941e4fe85e566231
William Fish <william.michael.fish@gmail.com>
2020-05-05 10:00:53 +0100

    Handle unparseable errors from Azure API Gateway

--- spec/lib/nomis_client/api_error_spec.rb
+++ spec/lib/nomis_client/api_error_spec.rb
@@ -3,13 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe NomisClient::ApiError do
-  it '#json_api_error' do
-    nomis_error = described_class.new(status: 400, error_body: {
-        'userMessage' => 'User message.', 'developerMessage' => 'Developer message.', 'moreInfo' => 'More info.'
-    }.to_json)
+  describe '#json_api_error' do
+    subject(:error) { described_class.new(status: status, error_body: error_body)}
 
-    expect(nomis_error.json_api_error).to eq(code: 'NOMIS-ERROR',
-                                             status: 400, title: 'User message.',
-                                             details: 'Developer message. More info.')
+    let(:status) { 400 }
+
+    let(:error_body) do
+      {
+        'userMessage' => 'User message.',
+        'developerMessage' => 'Developer message.',
+        'moreInfo' => 'More info.'
+      }.to_json
+    end
+
+    it 'returns a properly formatted api error' do
+      expect(error.json_api_error).to eq(
+        code: 'NOMIS-ERROR',
+        status: 400, title: 'User message.',
+        details: 'Developer message. More info.'
+      )
+    end
+
+    context 'when parsing the error_body raises a JSON::ParserError' do
+      let(:error_body) { '<html></html>' }
+
+      it 'returns a properly formatted api error' do
+        expect(error.json_api_error).to eq(
+          code: 'NOMIS-ERROR',
+          status: 400, title: 'Unparseable error from Nomis',
+          details: "Status #{status}. We tried to parse an error from Nomis and failed. Is the Elite2API routeable?"
+        )
+      end
+    end
   end
 end
