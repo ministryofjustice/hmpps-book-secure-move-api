a9b19d2c945ecd6b180e69a80b0b805671de28ea
William Fish <william.michael.fish@gmail.com>
2020-05-05 10:56:03 +0100

    Lint

--- spec/lib/nomis_client/api_error_spec.rb
+++ spec/lib/nomis_client/api_error_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe NomisClient::ApiError do
   describe '#json_api_error' do
-    subject(:error) { described_class.new(status: status, error_body: error_body)}
+    subject(:error) { described_class.new(status: status, error_body: error_body) }
 
     let(:status) { 400 }
 
@@ -12,7 +12,7 @@ RSpec.describe NomisClient::ApiError do
       {
         'userMessage' => 'User message.',
         'developerMessage' => 'Developer message.',
-        'moreInfo' => 'More info.'
+        'moreInfo' => 'More info.',
       }.to_json
     end
 
