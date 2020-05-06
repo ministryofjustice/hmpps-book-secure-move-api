edb60294b3efacba99d11023c89c100ccd32eebf
cwrw <abeer.salameh@gmail.com>
2020-05-06 11:48:48 +0100

    P4-1376 Change migration to add index and foreign key
    jira-ticket: https://dsdmoj.atlassian.net/browse/P4-1376

--- db/migrate/20200504081233_add_allocation_to_moves.rb
+++ db/migrate/20200504081233_add_allocation_to_moves.rb
@@ -1,5 +1,5 @@
 class AddAllocationToMoves < ActiveRecord::Migration[5.2]
   def change
-    add_column :moves, :allocation_id, :uuid, null: true
+    add_reference :moves, :allocation, type: :uuid, index: true, foreign_key: true
   end
 end
