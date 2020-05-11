ff08234d4886c6b78dbd8b4c930c34ac413e6e4c
William Fish <william.michael.fish@gmail.com>
2020-05-11 10:35:25 +0100

    [P4-1185] adds special vehicle to personal care needs (#435)
    * Updates finding an assessment question for personal care needs to just using a static question key for special vehicle needs and defaulting to (not pregnancy) where there is no specification
    * Extends personal care needs nomis client (bizarrely post) call to include extra domains we're interested in
    * Extends AssessmentAnswer builder to include the alert type description for the given domain
    * Moves the AssessmentAnswer builder into the AssessmentAnswer class

--- app/services/personal_care_needs/importer.rb
+++ app/services/personal_care_needs/importer.rb
@@ -2,52 +2,94 @@
 
 module PersonalCareNeeds
   class Importer
-    attr_accessor :profile, :personal_care_needs
-
-    FALLBACK_QUESTION_KEY = :pregnant
     ASSESSMENT_ANSWER_CATEGORY = 'health'
 
+    QUESTION_KEY_PREGNANT = 'pregnant'
+    QUESTION_KEY_SPECIAL_VEHICLE = 'special_vehicle'
+    QUESTION_KEY_FALLBACK = 'pregnant'
+
+    DOMAIN_PHYSICAL = 'PHY'
+    DOMAIN_DISABILITY = 'DISAB'
+    DOMAIN_MATERNITY = 'MATSTAT'
+    DOMAIN_UNKNOWN = nil
+
+    DOMAIN_TO_NOMIS_ALERT_TYPE_DESCRIPTION = {
+      DOMAIN_PHYSICAL   => 'Medical',
+      DOMAIN_DISABILITY => 'Disability',
+      DOMAIN_MATERNITY  => 'Maternity Status',
+    }.freeze
+    DEFAULT_ALERT_TYPE_DESCRIPTION = 'Unknown'
+
+    PROBLEM_CODE_TO_QUESTION_KEY_AND_DOMAIN_MAPPING = {
+      'DI'     => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_FALLBACK        }, # Diabetic,Medical
+      'BRK'    => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Broken Bone/s,Medical
+      'SPR'    => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Sprain,Medical
+      'TOT'    => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_FALLBACK        }, # Dental,Medical
+      'ARTH'   => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Arthritic,Medical
+      'ASTH'   => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_FALLBACK        }, # Asthmatic,Medical
+      'AUTI'   => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_FALLBACK        }, # Autism,Medical
+      'EPI'    => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Epileptic,Medical
+      'FALS'   => { domain: DOMAIN_PHYSICAL,   question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # False Limbs,Medical
+      'DEP'    => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Depression,Psychological
+      'BIP'    => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Bi-Polar,Psychological
+      'HDL'    => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Deaf - Lip Reads,Disability
+      'HDS'    => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Deaf - Uses Sign Language,Disability
+      'HD'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Hearing Impairment - not deaf,Disability
+      'SI'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Speech Impediment,Disability
+      'VI'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Visual Impairment (Inc. Blind),Disability
+      'RM'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Reduced Mobility,Disability
+      'PC'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Progressive Condition,Disability
+      'RC'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Reduced Physical Capacity,Disability
+      'SD'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Severe Disfigurement,Disability
+      'MI'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Mental Illness,Disability
+      'LD'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Learning Difficulties (Inc. Dyslexia),Disability
+      'LDY'    => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Learning Difficulties (Inc. Autism),Disability
+      'OD'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_SPECIAL_VEHICLE }, # Other Disability,Disability
+      'NR'     => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # No Disability Recorded,Disability
+      'PRD'    => { domain: DOMAIN_DISABILITY, question_key: QUESTION_KEY_FALLBACK        }, # Refused to Disclose,Disability
+      'PREG'   => { domain: DOMAIN_MATERNITY,  question_key: QUESTION_KEY_PREGNANT        }, # Pregnant Unaccompanied,Maternity Status
+      'NO9U18' => { domain: DOMAIN_MATERNITY,  question_key: QUESTION_KEY_PREGNANT        }, # "Not Preg, acc over 9mths under 18mths",Maternity Status
+      'NU9'    => { domain: DOMAIN_MATERNITY,  question_key: QUESTION_KEY_PREGNANT        }, # "Not Preg, acc under 9mths",Maternity Status
+      'NU9U18' => { domain: DOMAIN_MATERNITY,  question_key: QUESTION_KEY_PREGNANT        }, # "Not Preg, acc under 9mths under 18mths",Maternity Status
+      'ACCU18' => { domain: DOMAIN_MATERNITY,  question_key: QUESTION_KEY_PREGNANT        }, # "Preg, acc over 9mths under 18mths",Maternity Status
+      'ACCU9'  => { domain: DOMAIN_MATERNITY,  question_key: QUESTION_KEY_PREGNANT        }, # "Preg, acc under 9mths",Maternity Status
+      'AU9U18' => { domain: DOMAIN_MATERNITY,  question_key: QUESTION_KEY_PREGNANT        }, # "Preg, acc under 9mths under 18mths",Maternity Status
+      'NED'    => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Nutrition Eating and Drinking,Social Care
+      'PH'     => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Personal Hygiene,Social Care
+      'TTG'    => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Toileting,Social Care
+      'BAC'    => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Being Appropriately Clothed,Social Care
+      'UPS'    => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Using Prison Safely,Social Care
+      'CHS'    => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Maintaining Cell To Habitable Standard,Social Care
+      'MR'     => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Maintaining Relationships,Social Care
+      'AA'     => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Accessing Activities,Social Care
+      'AS'     => { domain: DOMAIN_UNKNOWN,    question_key: QUESTION_KEY_FALLBACK        }, # Accessing Services,Social Care
+    }.freeze
+    DEFAULT_PROBLEM_CODE_MAPPING = { domain: DOMAIN_UNKNOWN, question_key: QUESTION_KEY_FALLBACK }.freeze
+
     def initialize(profile:, personal_care_needs:)
-      self.profile = profile
-      self.personal_care_needs = personal_care_needs
+      @profile = profile
+      @personal_care_needs = personal_care_needs
     end
 
     def call
-      profile.merge_assessment_answers!(
-        personal_care_needs.map { |personal_care_need| build(personal_care_need) },
-        ASSESSMENT_ANSWER_CATEGORY,
-      )
+      assessment_answers = personal_care_needs.map do |personal_care_need|
+        mapping = PROBLEM_CODE_TO_QUESTION_KEY_AND_DOMAIN_MAPPING.fetch(
+          personal_care_need[:problem_code], DEFAULT_PROBLEM_CODE_MAPPING
+        )
+
+        description = DOMAIN_TO_NOMIS_ALERT_TYPE_DESCRIPTION.fetch(
+          mapping[:domain], DEFAULT_ALERT_TYPE_DESCRIPTION
+        )
+        question = AssessmentQuestion.find_by(key: mapping[:question_key])
+
+        Profile::AssessmentAnswer.from_nomis_personal_care_need(personal_care_need, question, description)
+      end
+
+      profile.merge_assessment_answers!(assessment_answers, ASSESSMENT_ANSWER_CATEGORY)
     end
 
   private
 
-    def build(personal_care_need)
-      assessment_question = find_assessment_question(personal_care_need)
-
-      Profile::AssessmentAnswer.new(
-        title: personal_care_need[:problem_description],
-        assessment_question_id: assessment_question.id,
-        created_at: personal_care_need[:start_date],
-        expires_at: personal_care_need[:end_date],
-        category: assessment_question.category,
-        key: assessment_question.key,
-        nomis_alert_code: personal_care_need.fetch(:problem_code),
-        nomis_alert_type: personal_care_need.fetch(:problem_type),
-        nomis_alert_description: personal_care_need[:problem_description],
-        imported_from_nomis: true,
-      ).tap(&:set_timestamps)
-    end
-
-    def find_assessment_question(personal_care_need)
-      nomis_alert = NomisAlert.includes(:assessment_question).find_by(
-        code: personal_care_need.fetch(:problem_code),
-        type_code: personal_care_need.fetch(:problem_type),
-      )
-      nomis_alert&.assessment_question || fallback_assessment_question
-    end
-
-    def fallback_assessment_question
-      @fallback_assessment_question ||= AssessmentQuestion.find_by!(key: FALLBACK_QUESTION_KEY)
-    end
+    attr_reader :profile, :personal_care_needs
   end
 end
