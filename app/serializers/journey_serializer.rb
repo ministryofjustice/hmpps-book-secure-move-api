e84704b5d7e1f62560bbb13ae35521f64d986f85
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-05-11 09:04:48 +0100

    Update journey_serializer.rb
    simplify

--- app/serializers/journey_serializer.rb
+++ app/serializers/journey_serializer.rb
@@ -3,14 +3,9 @@
 class JourneySerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :billable, :state
+  attributes :id, :state, :billable, :vehicle
   attribute :client_timestamp, key: :timestamp
-  attribute :vehicle
 
   has_one :from_location, serializer: LocationSerializer
   has_one :to_location, serializer: LocationSerializer
-
-  def vehicle
-    object.details&.dig('metadata', 'vehicle')
-  end
 end
