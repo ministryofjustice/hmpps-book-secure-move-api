2c81cac5fbd438c2977f49f944ebf5e5f0a53f56
William Fish <william.michael.fish@gmail.com>
2020-05-22 12:06:03 +0100

    Tidy up matcher and validate type in addition to id

--- spec/support/matchers/have_includes.rb
+++ spec/support/matchers/have_includes.rb
@@ -1,12 +1,31 @@
 RSpec::Matchers.define :have_includes do |*includes|
+  attr_reader :actual, :expected
+
   match do |response_json|
-    ids = response_json.fetch('included', []).map do |included|
-      included.fetch('id', nil)
+    @actual = response_json.fetch('included', []).map do |included|
+      [included['id'], included['type']]
+    end
+    @actual = @actual.sort_by { |(id, _type)| id }
+
+    @expected = includes.flatten
+    @expected = @expected.sort_by(&:id)
+    @expected = @expected.map do |entity|
+      id = entity.id
+      type = entity.class&.to_s&.pluralize&.tableize
+
+      [id, type]
     end
 
-    includes.flatten!
+    values_match?(@expected, @actual)
+  end
 
-    includes.all? { |i| ids.include?(i.id) }
+  def failure_message
+    missing_in_actual = expected - actual
+    missing_in_expected = actual - expected
+
+    message = ''
+    message += "Missing in actual: #{missing_in_actual}" if missing_in_actual.any?
+    message += "Missing in expected: #{missing_in_expected}" if missing_in_expected.any?
   end
 
   diffable
