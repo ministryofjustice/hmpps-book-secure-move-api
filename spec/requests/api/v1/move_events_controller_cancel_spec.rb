16888bddeaba7f6e0c0b6001cb650fba1f0bf531
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-05-28 21:00:44 +0100

    Update move_events_controller_cancel_spec.rb

--- spec/requests/api/v1/move_events_controller_cancel_spec.rb
+++ spec/requests/api/v1/move_events_controller_cancel_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::MoveEventsController do
   let(:response_json) { JSON.parse(response.body) }
 
   describe 'POST /moves/:move_id/cancel' do
-    let(:schema) { load_yaml_schema('post_move_cancel_responses.yaml') }
+    let(:schema) { load_yaml_schema('post_move_events_responses.yaml') }
 
     let(:supplier) { create(:supplier) }
     let(:application) { create(:application, owner_id: supplier.id) }
