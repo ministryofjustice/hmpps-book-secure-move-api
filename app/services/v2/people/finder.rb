77249dc2a06d1520fdd589d9fd1fa452f2a06105
Alexdesi <alexdesi@gmail.com>
2020-06-01 14:38:05 +0100

    Refactor People finder

--- app/services/v2/people/finder.rb
+++ app/services/v2/people/finder.rb
@@ -15,31 +15,16 @@ module V2
 
     private
 
-      def split_params(name)
-        filter_params[name]&.split(',')
-      end
-
       def apply_filters(scope)
-        scope = scope.includes(:profiles, :ethnicity, :gender)
-        scope = apply_police_national_computer_filters(scope)
-        scope = apply_criminal_records_office_filters(scope)
-        scope = apply_prison_number_filters(scope)
-        scope
+        scope.where(filters)
       end
 
-      def apply_police_national_computer_filters(scope)
-        scope = scope.where(police_national_computer: split_params(:police_national_computer)) if filter_params.key?(:police_national_computer)
-        scope
+      def filters
+        allowed_filters.map { |filter_name, value| [filter_name, value&.split(',')] }.to_h
       end
 
-      def apply_criminal_records_office_filters(scope)
-        scope = scope.where(criminal_records_office: split_params(:criminal_records_office)) if filter_params.key?(:criminal_records_office)
-        scope
-      end
-
-      def apply_prison_number_filters(scope)
-        scope = scope.where(prison_number: split_params(:prison_number)) if filter_params.key?(:prison_number)
-        scope
+      def allowed_filters
+        filter_params.select { |filter| %i[police_national_computer criminal_records_office prison_number].include?(filter) }
       end
     end
   end
