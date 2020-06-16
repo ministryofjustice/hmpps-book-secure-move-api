00323b3c1570b0f8e67ceec3e1eb8093ac0cb334
William Fish <william.michael.fish@gmail.com>
2020-06-16 13:33:21 +0100

    Use standard approach for mocking out NomisClient

--- spec/services/profiles/import_alerts_and_personal_care_needs_spec.rb
+++ spec/services/profiles/import_alerts_and_personal_care_needs_spec.rb
@@ -2,38 +2,63 @@
 
 require 'rails_helper'
 
-RSpec.describe Profiles::ImportAlertsAndPersonalCareNeeds do
-  # let(:person) { create :person, :nomis_synced }
-  # let(:prison_number) { person.prison_number }
-  # let(:profile) { create(:profile, person: person) }
-  # let(:alerts_response) { [] }
-  # let(:personal_care_needs_response) { [] }
-  #
-  # before do
-  #   create :assessment_question, :care_needs_fallback
-  #   create :assessment_question, :alerts_fallback
-  #
-  #   allow(NomisClient::Alerts).to receive(:get).and_return(alerts_response)
-  #   allow(NomisClient::PersonalCareNeeds).to receive(:get).and_return(personal_care_needs_response)
-  # end
-  #
-  # context 'when alert is present' do
-  #   let(:alerts_response) { [{ offender_no: person.prison_number, alert_code: 'ACCU9', alert_type: 'MATSTAT' }] }
-  #
-  #   it 'updates the profile' do
-  #     described_class.call(profile, prison_number)
-  #
-  #     expect(profile.assessment_answers[0].attributes).to include({ 'nomis_alert_code' => 'ACCU9', 'nomis_alert_type' => 'MATSTAT' })
-  #   end
-  # end
-  #
-  # context 'when personal_care_needs is present' do
-  #   let(:personal_care_needs_response) { [{ offender_no: person.prison_number, problem_type: 'FOO', problem_code: 'AA' }] }
-  #
-  #   it 'updates the profile' do
-  #     described_class.call(profile, prison_number)
-  #
-  #     expect(profile.assessment_answers[0].attributes).to include({ 'nomis_alert_type': 'FOO', 'nomis_alert_code': 'AA' })
-  #   end
-  # end
+RSpec.describe Profiles::ImportAlertsAndPersonalCareNeeds, with_nomis_client_authentication: true do
+  let(:person) { create :person, :nomis_synced }
+  let(:prison_number) { person.prison_number }
+  let(:profile) { create(:profile, person: person) }
+  let(:alerts_response_body) { [] }
+  let(:personal_care_needs_response_body) { [{ 'offenderNo' => prison_number, 'personalCareNeeds' => [] }] }
+
+  let(:alerts_response) do
+    instance_double(
+      'OAuth2::Response',
+      body: alerts_response_body.to_json,
+      parsed: alerts_response_body,
+      status: 200,
+    )
+  end
+  let(:personal_care_needs_response) do
+    instance_double(
+      'OAuth2::Response',
+      body: personal_care_needs_response_body.to_json,
+      parsed: personal_care_needs_response_body,
+      status: 200,
+    )
+  end
+
+  before do
+    create :assessment_question, :care_needs_fallback
+    create :assessment_question, :alerts_fallback
+
+    allow(token).to receive(:post).and_return(alerts_response, personal_care_needs_response)
+  end
+
+  context 'when alert is present' do
+    let(:alerts_response_body) { [{ 'offenderNo' => person.prison_number, 'alertCode' => 'ACCU9', 'alertType' => 'MATSTAT' }] }
+
+    it 'updates the profile' do
+      described_class.call(profile, prison_number)
+
+      expect(profile.assessment_answers[0].attributes).to include({ 'nomis_alert_code' => 'ACCU9', 'nomis_alert_type' => 'MATSTAT' })
+    end
+  end
+
+  context 'when personal_care_needs is present' do
+    let(:personal_care_needs_response_body) do
+      [
+        {
+          'offenderNo' => prison_number,
+          'personalCareNeeds' => [
+            { 'problemType' => 'FOO', 'problemCode' => 'AA' },
+          ],
+        },
+      ]
+    end
+
+    it 'updates the profile' do
+      described_class.call(profile, prison_number)
+
+      expect(profile.assessment_answers[0].attributes).to include({ 'nomis_alert_type': 'FOO', 'nomis_alert_code': 'AA' })
+    end
+  end
 end
