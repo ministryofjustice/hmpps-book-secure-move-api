2b59adaa109d736a96947a7cf6bdbd578510f029
Andrew Retmanski <aretmanski@gmail.com>
2020-06-16 16:47:49 +0100

    Remove unused allocation events endpoint Previously we implemented `POST /allocations/:id/events` but this has been replaced by a new dedicated endpoint `POST /allocations/:id/cancel`. The original endpoint did not actually get implemented in the front end so can be safely removed.

--- app/services/allocation_events/params_validator.rb
+++ app/services/allocation_events/params_validator.rb
@@ -4,10 +4,6 @@ module AllocationEvents
   class ParamsValidator
     include ActiveModel::Validations
 
-    # TODO: remove event_name when allocation `events` endpoint is no longer in use
-    attr_reader :event_name, :timestamp
-
-    validates :event_name, inclusion: %w[cancel], allow_nil: true
     validates_each :timestamp, presence: true do |record, attr, value|
       Time.iso8601(value)
     rescue ArgumentError
@@ -15,7 +11,6 @@ module AllocationEvents
     end
 
     def initialize(params)
-      @event_name = params.dig(:attributes, :event_name)
       @timestamp = params.dig(:attributes, :timestamp)
     end
   end
