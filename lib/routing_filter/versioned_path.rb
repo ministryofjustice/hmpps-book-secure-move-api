276eee14b420ef08a61204ca434be552916ea842
William Fish <william.michael.fish@gmail.com>
2020-06-19 17:21:29 +0100

    Handle yaml files in addition to swagger ones

--- lib/routing_filter/versioned_path.rb
+++ lib/routing_filter/versioned_path.rb
@@ -2,7 +2,7 @@
 module RoutingFilter
   class VersionedPath < Filter
     def around_recognize(path, _env)
-      remove_version!(path) unless path.include?('swagger')
+      remove_version!(path) unless path.include?('yaml')
 
       yield
     end
