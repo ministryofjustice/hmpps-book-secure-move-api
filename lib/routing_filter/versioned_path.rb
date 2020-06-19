fd4ea7e0acbc80955cccfddcd38fa33a12142311
William Fish <william.michael.fish@gmail.com>
2020-06-19 11:46:20 +0100

    Remove unused methods

--- lib/routing_filter/versioned_path.rb
+++ lib/routing_filter/versioned_path.rb
@@ -1,10 +1,7 @@
 # TODO: Remove this when v1 is dead
 module RoutingFilter
   class VersionedPath < Filter
-    HEADER_VERSION_REGEX = %r{.*version=(?<version>\d+)}.freeze
-    PATH_VERSION_REGEX = %r{\A\/api(\/(?<version>v1))\/.+\Z}.freeze
-
-    def around_recognize(path, env)
+    def around_recognize(path, _env)
       remove_version!(path)
 
       yield
@@ -26,23 +23,6 @@ module RoutingFilter
       path, _options = result
 
       path.gsub!('/api/', '/api/v1/')
-    end
-
-    def raise_not_found_error?(path, env)
-      path_version = path_version(path)
-      header_version = header_version(env.headers['Accept'])
-
-      true if path_version == 'v1' && header_version == '2'
-    end
-
-    def path_version(path)
-      match = path.match(PATH_VERSION_REGEX)
-      match&.[](:version)
-    end
-
-    def header_version(header)
-      match = header.match(HEADER_VERSION_REGEX)
-      match&.[](:version)
     end
   end
 end
