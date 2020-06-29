66157cd4af42401fcef57fb2e815ba9bc4e8c058
cwrw <abeer.salameh@gmail.com>
2020-06-29 15:37:22 +0100

    P4-1749 Add question parser for framework importer
    jira-ticket: https://dsdmoj.atlassian.net/browse/P4-1749
    
    Allow each question to be parsed from a yaml file separately.
    Questions can include dependent questions that do not exist yet.
    If they do update values on existing framework questions, if not
    create it and add it to the questions map.

---
+++ app/services/frameworks/question.rb
@@ -0,0 +1,66 @@
+module Frameworks
+  class Question
+    attr_reader :source, :questions, :filepath
+
+    def initialize(filepath:, questions:)
+      @filepath = filepath
+      @source = YAML.safe_load(File.read(filepath))
+      @questions = questions
+    end
+
+    def key
+      @key ||= File.basename(filepath, '.yml')
+    end
+
+    def call
+      question.question_type = source['type']
+      build_options(source['options'] || [])
+
+      question.required = true if required?(source['validations'])
+      questions
+    end
+
+    def required?(validations)
+      validation_types = validations.flat_map { |validation| validation['type'] }
+      validation_types.any?('required')
+    end
+
+    def build_options(options)
+      options.each do |option|
+        question.options << option['value']
+
+        build_followup_question(
+          followups: option['followup'].presence || [],
+          value: option['value'],
+        )
+
+        build_followup_comments(
+          followup_comment: option['followup_comment'],
+          value: option['value'],
+        )
+      end
+    end
+
+    def build_followup_comments(followup_comment:, value:)
+      if followup_comment
+        question.followup_comment = true
+        question.followup_comment_options << value if required?(followup_comment.fetch('validations', []))
+      end
+    end
+
+    def build_followup_question(followups:, value:)
+      followups.each do |followup|
+        questions[followup] = questions[followup] || FrameworkQuestion.new(key: followup)
+        questions[followup].section = question.section
+        questions[followup].dependent_value = value
+        questions[followup].parent = question
+      end
+    end
+
+    def question
+      @question ||= begin
+        questions[key] = questions[key] || FrameworkQuestion.new(key: key)
+      end
+    end
+  end
+end
---
+++ spec/services/frameworks/question_spec.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Frameworks::Question do
+  describe '#call' do
+    let(:fixture_path) { 'spec/fixtures/files/frameworks/person-escort-record-1/questions' }
+
+    it 'sets a question as required if required validation available' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/medical-details-information.yml",
+      )
+      question = FrameworkQuestion.new(section: 'health', key: 'medical-details-information')
+      described_class.new(filepath: filepath, questions: { 'medical-details-information' => question }).call
+
+      expect(question.required).to eq(true)
+    end
+
+    it 'sets the type on a question' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/medical-details-information.yml",
+      )
+      question = FrameworkQuestion.new(section: 'health', key: 'medical-details-information')
+      described_class.new(filepath: filepath, questions: { 'medical-details-information' => question }).call
+
+      expect(question.question_type).to eq('textarea')
+    end
+
+    it 'sets the options on a question' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/regular-medication.yml",
+      )
+      question = FrameworkQuestion.new(section: 'health', key: 'regular-medication')
+      described_class.new(filepath: filepath, questions: { 'regular-medication' => question }).call
+
+      expect(question.options).to contain_exactly('Yes', 'No')
+    end
+
+    it 'does not set the options on a question if type does not permit it' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/medical-details-information.yml",
+      )
+      question = FrameworkQuestion.new(section: 'health', key: 'medical-details-information')
+      described_class.new(filepath: filepath, questions: { 'medical-details-information' => question }).call
+
+      expect(question.options).to be_empty
+    end
+
+    it 'allows folllowup comments to be set on a question' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/medical-professional-referral.yml",
+      )
+      question = FrameworkQuestion.new(section: 'health', key: 'medical-professional-referral')
+      described_class.new(filepath: filepath, questions: { 'medical-professional-referral' => question }).call
+
+      expect(question.followup_comment).to be_truthy
+    end
+
+    it 'allows folllowup comments to be required on an option' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/medical-professional-referral.yml",
+      )
+      question = FrameworkQuestion.new(section: 'health', key: 'medical-professional-referral')
+      described_class.new(filepath: filepath, questions: { 'medical-professional-referral' => question }).call
+
+      expect(question.followup_comment_options).to contain_exactly('Yes')
+    end
+
+    it 'sets dependent questions values' do
+      filepath = Rails.root.join("#{fixture_path}/sensitive-medication.yml")
+      question = FrameworkQuestion.new(section: 'health', key: 'sensitive-medication')
+      dependent_question = FrameworkQuestion.new(section: 'health', key: 'medication-while-moving')
+      questions = {
+        'sensitive-medication' => question,
+        'medication-while-moving' => dependent_question,
+      }
+
+      described_class.new(filepath: filepath, questions: questions).call
+
+      expect(dependent_question.dependent_value).to eq('Yes')
+    end
+
+    it 'sets dependent questions parents' do
+      filepath = Rails.root.join("#{fixture_path}/sensitive-medication.yml")
+      question = FrameworkQuestion.new(section: 'health', key: 'sensitive-medication')
+      dependent_question = FrameworkQuestion.new(section: 'health', key: 'medication-while-moving')
+      questions = {
+        'sensitive-medication' => question,
+        'medication-while-moving' => dependent_question,
+      }
+
+      described_class.new(filepath: filepath, questions: questions).call
+
+      expect(dependent_question.parent).to eq(question)
+    end
+
+    it 'adds question to set of questions if it did not exist before' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/sensitive-medication.yml",
+      )
+      question = FrameworkQuestion.new(section: 'health', key: 'sensitive-medication')
+      questions = { 'sensitive-medication' => question }
+
+      updated_questions = described_class.new(filepath: filepath, questions: questions).call
+      expect(updated_questions['medication-while-moving']).to be_a(FrameworkQuestion)
+    end
+
+    it 'adds dependent question to set of questions if it did not exist before' do
+      filepath = Rails.root.join(
+        "#{fixture_path}/medical-details-information.yml",
+      )
+
+      questions = described_class.new(filepath: filepath, questions: {}).call
+      expect(questions['medical-details-information']).to be_a(FrameworkQuestion)
+    end
+  end
+end
