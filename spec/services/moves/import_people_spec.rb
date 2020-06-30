6f6d4c116d03bf93b6dabd11d0df66a8fefd94ec
William Fish <william.michael.fish@gmail.com>
2020-06-30 10:24:30 +0100

    [P4-1823] Handle profiles with no assessment answers in importer (#656)
    * Handle profiles without assessment answers in importer
    
    Jira link: https://dsdmoj.atlassian.net/browse/P4-1823
    
    We noticed a bug during importing of people and profiles in v1 in production. Profiles (and therefore people) weren't being saved because the `changed?` method returns true for new profiles without any assessment_answers.
    
    This commit fixes this issue by checking to see whether person
    attributes have changed in addition to profile ones.

--- spec/services/moves/import_people_spec.rb
+++ spec/services/moves/import_people_spec.rb
@@ -5,31 +5,35 @@ require 'rails_helper'
 RSpec.describe Moves::ImportPeople do
   subject(:importer) { described_class.new(input_data) }
 
-  let(:input_data) do
-    [prisoner_one.nomis_prison_number]
-  end
-
-  let!(:brixton_prison) { create(:location, nomis_agency_id: 'BXI', location_type: 'prison') }
-  let!(:wood_green_court) { create(:location, nomis_agency_id: 'WDGRCC', location_type: 'court') }
-  let!(:prisoner_one) { create(:person, :nomis_synced) }
-  let!(:prisoner_two) { create(:person, :nomis_synced) }
-  let!(:profile_one) { create(:profile, person: prisoner_one) }
-  let!(:profile_two) { create(:profile, person: prisoner_two) }
+  let(:input_data) { [nomis_prison_number] }
+  let(:nomis_prison_number) { 'G3239GV' }
 
   let(:alerts_response) do
-    [{ offender_no: prisoner_one.nomis_prison_number, alert_code: 'ACCU9', alert_type: 'MATSTAT' },
-     { offender_no: prisoner_two.nomis_prison_number, alert_code: 'ACCU9', alert_type: 'MATSTAT' },
-     { offender_no: prisoner_two.nomis_prison_number, alert_code: 'ACCU4', alert_type: 'MATSTAT' }]
+    [
+      {
+        offender_no: nomis_prison_number,
+        alert_code: 'ACCU9',
+        alert_type: 'MATSTAT',
+      },
+    ]
   end
-  let(:offender_numbers_response) { [{ offender_no: 'G3239GV' }, { offender_no: 'G7157AB' }] }
   let(:prison_numbers_response) do
-    [{ prison_number: prisoner_one.nomis_prison_number, first_name: 'Bob' },
-     { prison_number: prisoner_two.nomis_prison_number, first_name: 'Bob' }]
+    [
+      {
+        prison_number: nomis_prison_number,
+        first_name: 'Bob',
+        last_name: 'Beep',
+      },
+    ]
   end
   let(:personal_care_needs_response) do
-    [{ offender_no: prisoner_one.nomis_prison_number, problem_type: 'FOO', problem_code: 'AA' },
-     { offender_no: prisoner_two.nomis_prison_number, problem_type: 'FOO', problem_code: 'BAC' },
-     { offender_no: prisoner_two.nomis_prison_number, problem_type: 'FOO', problem_code: 'TTG' }]
+    [
+      {
+        offender_no: nomis_prison_number,
+        problem_type: 'FOO',
+        problem_code: 'AA',
+      },
+    ]
   end
 
   before do
@@ -41,13 +45,56 @@ RSpec.describe Moves::ImportPeople do
     create(:assessment_question, :alerts_fallback)
   end
 
-  context 'with one existing record' do
+  context 'with an existing record' do
+    before do
+      create(:person, nomis_prison_number: nomis_prison_number, prison_number: nomis_prison_number)
+    end
+
     it 'keeps people the same' do
       expect { importer.call }.not_to change(Person, :count)
     end
 
     it 'keeps profiles the same' do
       expect { importer.call }.not_to change(Profile, :count)
+    end
+  end
+
+  context 'with no existing records' do
+    it 'creates new `Person` records' do
+      expect { importer.call }.to change(Person, :count).by(1)
+    end
+
+    it 'creates new `Profile` records' do
+      expect { importer.call }.to change(Profile, :count).by(1)
+    end
+
+    it 'populates assessment answers' do
+      importer.call
+
+      assessment_answers = Person.last.profiles.first.assessment_answers
+
+      expect(assessment_answers).to be_present
+    end
+
+    context 'with no alerts or personal care needs' do
+      let(:alerts_response) { [] }
+      let(:personal_care_needs_response) { [] }
+
+      it 'creates new `Person` records' do
+        expect { importer.call }.to change(Person, :count).by(1)
+      end
+
+      it 'creates new `Profile` records' do
+        expect { importer.call }.to change(Profile, :count).by(1)
+      end
+
+      it 'does not populate assessment answers' do
+        importer.call
+
+        assessment_answers = Person.last.profiles.first.assessment_answers
+
+        expect(assessment_answers).to be_empty
+      end
     end
   end
 end
