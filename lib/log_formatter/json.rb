cfba744dca1d1359100b4c48b3f6d8a3897bfe7d
William Fish <william.michael.fish@gmail.com>
2020-07-03 16:01:33 +0100

    Adds log_formatter

---
+++ lib/log_formatter/json.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+module LogFormatter
+  class Json
+    def call(severity, timestamp, progname, msg)
+      data = {}
+      data[:severity] = severity if severity
+      data[:timestamp] = timestamp if timestamp
+      data[:progname] = progname if progname
+
+      # Handle messages coming from lograge and other json preformatted log messages
+      begin
+        msg = JSON.parse(msg)
+        data.merge!(msg)
+      rescue JSON::ParserError
+        data[:msg] = msg
+      end
+
+      ::JSON.dump(data) + "\n"
+    end
+  end
+end
