d66e1a5c72b807e1bceb59264b3e8fc65b3c13b2
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-07-08 14:54:45 +0100

    wip

--- spec/integrations/move_workflows/court_to_prison/person_with_nomis_prison_number_spec.rb
+++ spec/integrations/move_workflows/court_to_prison/person_with_nomis_prison_number_spec.rb
@@ -10,21 +10,21 @@ RSpec.describe 'dashboard outgoing moves' do
   # THEN the supplier should call the api as outlined in the example below
 
   let(:app) { Rack::Builder.parse_file("config.ru").first }
-  let(:headers) do
-    {
-        'CONTENT_TYPE': 'application/vnd.api+json',
-        'Accept': 'application/vnd.api+json; version=2',
-        'Authorization': "Bearer spoofed-token",
-    }
-  end
 
-  let(:prison_number) { 'F008AR' }
-  let(:get_people_index) { get "/api/people?filter[prison_number]=#{prison_number}", headers: headers }
+  let(:prison_number) { 'G8133UA' }
 
   it 'creates a move' do
-    get_people_index
+    header 'CONTENT_TYPE', 'application/vnd.api+json'
+    header 'Accept', 'application/vnd.api+json; version=2'
+    header 'Authorization', 'Bearer spoofed-token'
+
+    get "/api/people?filter[prison_number]=#{prison_number}"
+
+
+    puts "\n\n----"
     puts last_response.body
-    byebug
+    puts "----\n\n"
+    # byebug
   end
 
 end
