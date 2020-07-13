ed772a7e8d92ad2f1373a7377b000bc45180ab5d
cwrw <abeer.salameh@gmail.com>
2020-07-13 10:50:46 +0100

    P4-1761 Build framework responses for the PER
    jira-ticket: https://dsdmoj.atlassian.net/browse/P4-1761
    
    * Add activerecord import gem to do bulk imports
    * Add scope to order by latest framework version for
      returning latest default frameworks
    * Build responses recursively for dependent questions
      with correct response type
    * We need to define the per record before persisting
      responses because of dependent records being
      associated correctly

--- app/models/framework_question.rb
+++ app/models/framework_question.rb
@@ -17,7 +17,36 @@ class FrameworkQuestion < VersionedModel
                         foreign_key: 'parent_id'
   belongs_to :parent, class_name: 'FrameworkQuestion', optional: true
 
-
   has_many :flags
   has_many :framework_responses
+
+  def build_responses(question: self, person_escort_record:)
+    response = build_response(question, person_escort_record)
+    return response unless question.dependents.any?
+
+    question.dependents.find_each do |dependent_question|
+      dependent_response = build_responses(question: dependent_question, person_escort_record: person_escort_record)
+      dependent_response_values = dependent_response.slice(:type, :framework_question_id, :dependents, :person_escort_record)
+
+      response.dependents.build(dependent_response_values)
+    end
+
+    response
+  end
+
+  private
+
+  def build_response(question, person_escort_record)
+    klass =
+      case question.question_type
+      when 'radio'
+        question.followup_comment ? FrameworkResponse::Object : FrameworkResponse::String
+      when 'checkbox'
+        question.followup_comment ? FrameworkResponse::Collection : FrameworkResponse::Array
+      else
+        FrameworkResponse::String
+      end
+
+    klass.new(framework_question_id: question.id, person_escort_record: person_escort_record)
+  end
 end
