518e62e4c1e46f9ddf58b78b4d03ad1c43049f01
William Fish <william.michael.fish@gmail.com>
2020-07-27 16:18:00 +0100

    Fix automatically generated schema spacing

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_07_27_143634) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
