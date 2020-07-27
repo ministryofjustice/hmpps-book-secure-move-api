86aabe3a839701aa820345a4f5e7f69bee83c084
William Fish <william.michael.fish@gmail.com>
2020-07-27 16:06:01 +0100

    Remove conditional check for extension

--- db/migrate/20200727143320_enable_ci_text.rb
+++ db/migrate/20200727143320_enable_ci_text.rb
@@ -1,9 +1,9 @@
 class EnableCiText < ActiveRecord::Migration[6.0]
   def up
-    enable_extension('citext') unless extensions.include?('citext')
+    enable_extension('citext')
   end
 
   def down
-    disable_extension('citext') if extensions.include?('citext')
+    disable_extension('citext')
   end
 end
