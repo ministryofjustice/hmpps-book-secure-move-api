717d503e36825693f14aab5030144b64b234f7ec
William Fish <william.michael.fish@gmail.com>
2020-07-28 09:17:36 +0100

    [P4-1967] Fixes case sensitivity for all person identifiers (#743)

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Person do
-  let(:person) { create(:person) }
+  subject(:person) { create(:person) }
 
   it { is_expected.to have_many(:profiles) }
   it { is_expected.to have_many(:moves) }
@@ -19,5 +19,38 @@ RSpec.describe Person do
 
     expect(person.image.attached?).to be true
     expect(person.image.filename).to eq "#{person.id}.jpg"
+  end
+
+  describe '#police_national_computer' do
+    subject!(:person) { create(:person, police_national_computer: 'FLIBBLE') }
+
+    it 'is case insensitive' do
+      expect(described_class.where(police_national_computer: 'flibble')).to include(person)
+    end
+  end
+
+  describe '#criminal_records_office' do
+    subject!(:person) { create(:person, criminal_records_office: 'FLIBBLE') }
+
+    it 'is case insensitive' do
+      expect(described_class.where(criminal_records_office: 'flibble')).to include(person)
+    end
+  end
+
+  describe '#prison_number' do
+    subject!(:person) { create(:person, prison_number: 'FLIBBLE') }
+
+    it 'is case insensitive' do
+      expect(described_class.where(prison_number: 'flibble')).to include(person)
+    end
+  end
+
+  # TODO: Remove nomis_prison_number once we remove v1 from our system
+  describe '#nomis_prison_number' do
+    subject!(:person) { create(:person, nomis_prison_number: 'FLIBBLE') }
+
+    it 'is case insensitive' do
+      expect(described_class.where(nomis_prison_number: 'flibble')).to include(person)
+    end
   end
 end
