b0796e82f4e2e9b0a4f409fdcb795bdfe3dd0a3e
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-07-30 10:25:09 +0100

    linting

--- spec/lib/nomis_client/base_spec.rb
+++ spec/lib/nomis_client/base_spec.rb
@@ -19,8 +19,8 @@ RSpec.describe NomisClient::Base do
   before do
     # NB: the NomisClient uses class methods which persist for lifetime of the test suite and can cause problems; clearing
     # these class instance variables before and after tests helps prevent cross-contamination
-    NomisClient::Base.instance_variable_set(:@client, nil)
-    NomisClient::Base.instance_variable_set(:@token, nil)
+    described_class.instance_variable_set(:@client, nil)
+    described_class.instance_variable_set(:@token, nil)
 
     allow(OAuth2::Client).to receive(:new).and_return(oauth2_client)
   end
@@ -28,8 +28,8 @@ RSpec.describe NomisClient::Base do
   after do
     # NB: the NomisClient uses class methods which persist for lifetime of the test suite and can cause problems; clearing
     # these class instance variables before and after tests helps prevent cross-contamination
-    NomisClient::Base.instance_variable_set(:@client, nil)
-    NomisClient::Base.instance_variable_set(:@token, nil)
+    described_class.instance_variable_set(:@client, nil)
+    described_class.instance_variable_set(:@token, nil)
   end
 
   describe '.get' do
