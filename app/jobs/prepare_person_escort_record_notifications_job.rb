e9882f0caab0057657131267c830709267c82476
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-07-31 09:18:37 +0100

    small tweaks

--- app/jobs/prepare_person_escort_record_notifications_job.rb
+++ app/jobs/prepare_person_escort_record_notifications_job.rb
@@ -4,7 +4,7 @@ class PreparePersonEscortRecordNotificationsJob < ApplicationJob
   queue_as :notifications
 
   def perform(topic_id:, action_name:)
-    PersonEscortRecord.find(topic_id).profile.moves.each do |move|
+    PersonEscortRecord.find(topic_id).profile.moves.find_each do |move|
       PrepareMoveNotificationsJob.perform_now(topic_id: move.id, action_name: action_name)
     end
   end
