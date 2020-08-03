925e7d2db1d72f171f6efbd5fae08c2f48e631f3
Martyn Whitwell <martyn.whitwell@gmail.com>
2020-08-03 16:04:59 +0100

    small tidying up, add 1.1 story spec

--- spec/requests/api_stories/move_workflows/1_police_to_police/1_1_person_with_police_national_computer_spec.rb
+++ spec/requests/api_stories/move_workflows/1_police_to_police/1_1_person_with_police_national_computer_spec.rb
@@ -2,46 +2,50 @@
 
 require 'rack/test'
 
-RSpec.describe 'singleton', type: :request do
+# 1.1 Police to Police police_transfer move for a person with a PNC identifier
+# https://github.com/ministryofjustice/hmpps-book-secure-move-api/wiki/API-Walkthroughs
+
+# rubocop:disable Rails/HttpPositionalArguments
+RSpec.describe 'police-to-police transfer', type: :request do
   include Rack::Test::Methods
   include_context 'with mock prison-api'
   include_context 'with Nomis alerts reference data'
 
-  let(:prison_number) { 'G8133UA' }
-  let(:prison1) { create(:location, :prison) }
-  let(:prison2) { create(:location, :prison) }
-  let(:prison_transfer_reason) { create(:prison_transfer_reason) }
+  let(:police_national_computer) { '05/886838E' }
+  let(:person) { create(:person_without_profiles, police_national_computer: police_national_computer, prison_number: nil, criminal_records_office: nil) }
+  let(:police1) { create(:location, :police) }
+  let(:police2) { create(:location, :police) }
+  let(:assessment_special_diet) { AssessmentQuestion.where(key: 'special_diet_or_allergy').first }
 
-  # rubocop:disable Rails/HttpPositionalArguments
   # FRONTEND REQUESTS --------------------------
-  let(:frontend_get_people_by_prison_number) do
-    get "/api/people?filter[prison_number]=#{prison_number}"
+  let(:frontend_get_people_by_police_national_computer) do
+    get "/api/people?filter[police_national_computer]=#{police_national_computer}"
     validate_response(last_response, schema: 'get_people_responses.yaml', strict: false, version: 'v2', status: 200)
   end
 
-  let(:frontend_post_synchronised_profile) do
+  let(:frontend_post_new_profile) do
     header 'Idempotency-Key', SecureRandom.uuid
     post "/api/people/#{frontend_person_id}/profiles", {
-      "data": {
-        "type": 'profiles',
+      data: {
+        type: 'profiles',
+        attributes: {
+          assessment_answers: [{ title: assessment_special_diet.title, assessment_question_id: assessment_special_diet.id, comments: 'Extra Marmite' }],
+        },
       },
     }.to_json
     validate_response(last_response, schema: 'post_profiles_responses.yaml', strict: false, version: 'v1', status: 201)
   end
 
-  let(:frontend_post_prison_to_prison_move) do
+  let(:frontend_post_police_to_police_move) do
     header 'Idempotency-Key', SecureRandom.uuid
     post '/api/moves/', {
       "data": {
         "type": 'moves',
         "attributes": {
-          "date_from": '2020-07-21',
-          "date_to": '2020-07-23',
-          "move_agreed": true,
-          "move_agreed_by": 'Fred Bloggs',
-          "status": 'proposed',
-          "additional_information": 'example IPT singleton prison to prison transfer',
-          "move_type": 'prison_transfer',
+          "date": '2020-07-21',
+          "status": 'requested',
+          "additional_information": 'example Police to Police transfer',
+          "move_type": 'police_transfer',
         },
         "relationships": {
           "profile": {
@@ -53,19 +57,13 @@ RSpec.describe 'singleton', type: :request do
           "from_location": {
             "data": {
               "type": 'locations',
-              "id": prison1.id,
+              "id": police1.id,
             },
           },
           "to_location": {
             "data": {
               "type": 'locations',
-              "id": prison2.id,
-            },
-          },
-          "prison_transfer_reason": {
-            "data": {
-              "type": 'prison_transfer_reasons',
-              "id": prison_transfer_reason.id,
+              "id": police2.id,
             },
           },
         },
@@ -74,36 +72,22 @@ RSpec.describe 'singleton', type: :request do
     validate_response(last_response, schema: 'post_moves_responses.yaml', strict: false, version: 'v2', status: 201)
   end
 
-  let(:frontend_post_approve_move) do
-    header 'Idempotency-Key', SecureRandom.uuid
-    post "/api/moves/#{frontend_created_move_id}/approve", {
-      "data": {
-        "type": 'approve',
-        "attributes": {
-          "timestamp": '2020-07-21T09:09:09.123Z',
-          "date": '2020-07-22',
-        },
-      },
-    }.to_json
-    validate_response(last_response, status: 204)
-  end
-
-  let(:frontend_created_move_id) do
-    frontend_post_prison_to_prison_move['data']['id']
+  let(:frontend_move_id) do
+    frontend_post_police_to_police_move['data']['id']
   end
 
   let(:frontend_person_id) do
-    frontend_get_people_by_prison_number['data'].first['id']
+    frontend_get_people_by_police_national_computer['data'].first['id']
   end
 
   let(:frontend_profile_id) do
-    frontend_post_synchronised_profile['data']['id']
+    frontend_post_new_profile['data']['id']
   end
   # FRONTEND REQUESTS --------------------------
 
   # SUPPLIER REQUESTS --------------------------
   let(:get_requested_moves) do
-    get '/api/moves/?filter%5Bstatus%5D=requested&filter%5Bdate_from%5D=2020-07-21&filter%5Bdate_to%5D=2020-07-23'
+    get '/api/moves/?filter%5Bstatus%5D=requested&filter%5Bdate_from%5D=2020-07-21&filter%5Bdate_to%5D=2020-07-21'
     validate_response(last_response, schema: 'get_moves_responses.yaml', strict: false, version: 'v2', status: 200)
   end
 
@@ -151,7 +135,6 @@ RSpec.describe 'singleton', type: :request do
     validate_response(last_response, schema: 'get_move_responses.yaml', strict: false, version: 'v2', status: 200)
   end
   # SUPPLIER REQUESTS --------------------------
-  # rubocop:enable Rails/HttpPositionalArguments
 
   let(:move_id) do
     get_requested_moves['data'].first['id']
@@ -162,23 +145,25 @@ RSpec.describe 'singleton', type: :request do
     header 'Accept', 'application/vnd.api+json; version=2'
     header 'Authorization', 'Bearer spoofed-token'
 
-    # These steps simulate the frontend creating a move request
+    # create person and police stations
+    person
+    police1
+    police2
+
+    # These steps simulate the frontend creating a move request before the supplier processes it
 
     # get person record(s)
-    frontend_get_people_by_prison_number
+    frontend_get_people_by_police_national_computer
 
-    # create a new profile synchronised with Nomis alerts
-    frontend_post_synchronised_profile
+    # create a new profile with manually set alerts
+    frontend_post_new_profile
 
-    # create a new move from prison to prison
-    frontend_post_prison_to_prison_move
-
-    # approve the move, changing status from proposed to requested
-    frontend_post_approve_move
+    # create a new move from police station to police station
+    frontend_post_police_to_police_move
   end
 
   # rubocop:disable RSpec/MultipleExpectations
-  it 'singleton prison to prison move' do
+  it 'police to police transfer move' do
     # get requested moves awaiting processing
     get_requested_moves
 
@@ -193,14 +178,15 @@ RSpec.describe 'singleton', type: :request do
 
     # finally, retrieve the move and verify it is completed
     get_move.tap do |json|
-      expect(json.dig('data', 'id')).to eql frontend_created_move_id
-      expect(json.dig('data', 'attributes', 'move_type')).to eql 'prison_transfer'
+      expect(json.dig('data', 'id')).to eql frontend_move_id
+      expect(json.dig('data', 'attributes', 'move_type')).to eql 'police_transfer'
       expect(json.dig('data', 'attributes', 'status')).to eql 'completed'
-      expect(json.dig('data', 'attributes', 'date')).to eql '2020-07-22'
+      expect(json.dig('data', 'attributes', 'date')).to eql '2020-07-21'
       expect(json.dig('data', 'relationships', 'profile', 'data', 'id')).to eql frontend_profile_id
-      expect(json.dig('data', 'relationships', 'from_location', 'data', 'id')).to eql prison1.id
-      expect(json.dig('data', 'relationships', 'to_location', 'data', 'id')).to eql prison2.id
+      expect(json.dig('data', 'relationships', 'from_location', 'data', 'id')).to eql police1.id
+      expect(json.dig('data', 'relationships', 'to_location', 'data', 'id')).to eql police2.id
     end
   end
   # rubocop:enable RSpec/MultipleExpectations
 end
+# rubocop:enable Rails/HttpPositionalArguments
