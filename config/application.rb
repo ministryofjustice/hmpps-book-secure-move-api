e3af591fb64a357ab45f9967cb486244b8dcd871
Alessandro De Simone <alexdesi@users.noreply.github.com>
2020-08-11 16:45:14 +0100

    Add CloudDataFeed to make simple to update report on S3 (#803)
    Add CloudDataFeed to update report on S3

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
 require 'rails/test_unit/railtie'
+require 'aws-sdk-s3'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
