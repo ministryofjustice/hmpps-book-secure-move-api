59a07e38e682b0ad0ddadb883b6a34db4034db83
William Fish <william.michael.fish@gmail.com>
2020-08-11 16:38:46 +0100

    Apply suggestions from code review
    Co-authored-by: Alessandro De Simone <alexdesi@users.noreply.github.com>

--- app/services/feeds/move.rb
+++ app/services/feeds/move.rb
@@ -1,8 +1,8 @@
 module Feeds
   class Move
     def initialize(updated_at_from = nil, updated_at_to = nil)
-      @updated_at_from = updated_at_from || Time.zone.now.beginning_of_day - 1.day
-      @updated_at_to = updated_at_to || Time.zone.now.end_of_day - 1.day
+      @updated_at_from = updated_at_from || Time.zone.yesterday.beginning_of_day
+      @updated_at_to = updated_at_to || Time.zone.yesterday.end_of_day
 
       @feed = []
     end
