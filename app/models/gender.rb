f98621f6af154de068675355310aebaa16bbe5be
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-08-12 12:02:25 +0100

    Add report feed for Person

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -3,4 +3,12 @@
 class Gender < ApplicationRecord
   validates :title, presence: true
   validates :key, presence: true
+
+
+  def for_feed()
+    {
+        "gender" => key,
+    }
+  end
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
-
 class Person < VersionedModel
+  FEED_ATTRIBUTES = %w[
+    id
+    created_at
+    updated_at
+    first_names
+    last_name
+    date_of_birth
+    gender_additional_information
+    criminal_records_office
+    nomis_prison_number
+    police_national_computer
+    prison_number
+    last_synced_with_nomis
+    latest_nomis_booking_id
+  ].freeze
+
   IDENTIFIER_TYPES = %i[
     police_national_computer criminal_records_office prison_number
   ].freeze
@@ -14,6 +29,9 @@ class Person < VersionedModel
 
   scope :ordered_by_name, ->(direction) { order('last_name' => direction, 'first_names' => direction) }
   scope :search_by_last_name, ->(search) { select(:id).where('last_name ILIKE :search', search: "%#{search}%") }
+  scope :updated_at_range, lambda { |from, to|
+    where(updated_at: from..to)
+  }
 
   validates :last_name, presence: true
   validates :first_names, presence: true
@@ -32,4 +50,14 @@ class Person < VersionedModel
       end
     end
   end
+
+  def for_feed
+    feed_attributes = attributes.slice(*FEED_ATTRIBUTES)
+
+    feed_attributes.merge!(gender.for_feed) if gender
+    feed_attributes.merge!(ethnicity.for_feed) if ethnicity
+
+    feed_attributes
+  end
+
 end
