97b0ae9dd40f6787d0c7136c03bceb9c9d83cf76
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-08-20 13:23:59 +0100

    Match fluentbit time key

--- lib/log_formatter/json.rb
+++ lib/log_formatter/json.rb
@@ -5,7 +5,7 @@ module LogFormatter
     def call(severity, timestamp, progname, msg)
       data = {}
       data[:severity] = severity if severity
-      data[:timestamp] = timestamp if timestamp
+      data[:time] = timestamp if timestamp
       data[:progname] = progname if progname
 
       # Handle messages coming from lograge and other json preformatted log messages
