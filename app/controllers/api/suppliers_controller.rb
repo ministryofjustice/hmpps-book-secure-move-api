8ca327f70248cdc4287b38c984a5b2f5b39c60b6
Alessandro De Simone <alexdesi@users.noreply.github.com>
2020-08-20 15:04:07 +0100

    Add new endpoint /suppliers/:supplier_id/locations  (#842)
    * Add new endpoint /suppliers/:supplier_id/locations

---
+++ app/controllers/api/suppliers_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api
+  class SuppliersController < ApiController
+    def locations
+      paginate locations_from_supplier_moves, include: included_relationships
+    end
+
+  private
+
+    def locations_from_supplier_moves
+      supplier = Supplier.find(params[:supplier_id])
+
+      Location.joins(:moves_from)
+              .includes(:suppliers)
+              .where(moves: { supplier_id: supplier.id })
+              .order(key: :asc)
+              .distinct
+    end
+
+    def supported_relationships
+      LocationSerializer::SUPPORTED_RELATIONSHIPS
+    end
+  end
+end
