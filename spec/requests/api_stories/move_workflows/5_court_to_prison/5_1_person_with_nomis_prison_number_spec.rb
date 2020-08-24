0ec68a3b6f9f6a775ab0a18b0e452ca80fa03dc4
Martyn Whitwell <martyn.whitwell@digital.justice.gov.uk>
2020-08-24 10:14:50 +0100

    fix api_story tags (#867)

--- spec/requests/api_stories/move_workflows/5_court_to_prison/5_1_person_with_nomis_prison_number_spec.rb
+++ spec/requests/api_stories/move_workflows/5_court_to_prison/5_1_person_with_nomis_prison_number_spec.rb
@@ -6,7 +6,7 @@ require 'rack/test'
 # https://github.com/ministryofjustice/hmpps-book-secure-move-api/wiki/API-Walkthroughs
 
 # rubocop:disable Rails/HttpPositionalArguments
-RSpec.describe 'court to prison move', type: :request do
+RSpec.describe 'court to prison move', type: :request, api_story: true do
   include Rack::Test::Methods
   include_context 'with mock prison-api'
   include_context 'with Nomis alerts reference data'
