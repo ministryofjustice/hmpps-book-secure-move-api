59e127aca04948188b95d2fcf4a8bec20034adb9
William Fish <william.michael.fish@gmail.com>
2020-08-25 11:04:33 +0100

    Adds generic_event model and specs

---
+++ spec/factories/generic_event.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :generic_event do
+    eventable { association(:move) }
+    occurred_at { Time.zone.now }
+    notes { 'Flibble' }
+    created_by { GenericEvent::CREATED_BY_OPTIONS.sample }
+  end
+
+  factory :event_move_cancel, parent: :generic_event, class: 'Events::MoveCancel' do
+    details do
+      {
+        cancellation_reason: 'made_in_error',
+        cancellation_reason_comment: 'It was a mistake',
+      }
+    end
+  end
+end
---
+++ spec/models/events/move_cancel_spec.rb
@@ -0,0 +1,52 @@
+RSpec.describe Events::MoveCancel do
+  subject(:generic_event) { build(:event_move_cancel) }
+
+  it 'validates cancellation_reason' do
+    expect(generic_event).to validate_inclusion_of(:cancellation_reason).in_array(Move::CANCELLATION_REASONS)
+  end
+
+  it 'validates eventable_type' do
+    expect(generic_event).to validate_inclusion_of(:eventable_type).in_array(%w[Move])
+  end
+
+  describe '#cancellation_reason' do
+    it 'returns the cancellation_reason from the details' do
+      expect(generic_event.cancellation_reason).to eq(generic_event.details['cancellation_reason'])
+    end
+  end
+
+  describe '#cancellation_reason_comment' do
+    it 'returns the cancellation_reason_comment from the details' do
+      expect(generic_event.cancellation_reason_comment).to eq(generic_event.details['cancellation_reason_comment'])
+    end
+  end
+
+  describe '#trigger' do
+    before do
+      allow(Allocations::RemoveFromNomis).to receive(:call)
+    end
+
+    it 'does not persist changes to the eventable' do
+      generic_event.trigger
+      expect(generic_event.eventable).not_to be_persisted
+    end
+
+    it 'sets the eventable `status` to cancelled' do
+      expect { generic_event.trigger }.to change { generic_event.eventable.status }.from('requested').to('cancelled')
+    end
+
+    it 'sets the eventable `cancellation_reason`' do
+      expect { generic_event.trigger }.to change { generic_event.eventable.cancellation_reason }.from(nil).to('made_in_error')
+    end
+
+    it 'sets the eventable `cancellation_reason_comment`' do
+      expect { generic_event.trigger }.to change { generic_event.eventable.cancellation_reason_comment }.from(nil).to('It was a mistake')
+    end
+
+    it 'calls the Allocations::RemoveFromNomis service with the eventable' do
+      generic_event.trigger
+
+      expect(Allocations::RemoveFromNomis).to have_received(:call).with(generic_event.eventable)
+    end
+  end
+end
