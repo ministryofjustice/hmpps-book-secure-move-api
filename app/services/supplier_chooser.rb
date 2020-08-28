bb83e9e9f2d5af84fa3f296f7cc8ac5d02b22554
Andrew Retmanski <aretmanski@gmail.com>
2020-08-28 08:42:14 +0100

    Quick fix for go live with Serco Moves created by suppliers include locations that are not mapped in the supplier_location.yml file. We need to come up with a nice solution for this, potentially reverting to the previous approach based on the doorkeeper_application_owner. As a short term fix we are going live with just one supplier, Serco so can make the assumption that any moves created without an appropriate SupplierLocation mapping will have been created by Serco.

--- app/services/supplier_chooser.rb
+++ app/services/supplier_chooser.rb
@@ -11,6 +11,12 @@ class SupplierChooser
 
     # NB: business rules _should_ preclude multiple suppliers for visibility
     supplier_location = SupplierLocation.location(location.id).effective_on(effective_date).first
-    supplier_location&.supplier
+
+    # FIXME: This is a very temporary solution to the issue of suppliers not being able to create
+    # moves for locations that have not been mapped in the supplier_locatations.yml files.
+    # Only one supplier (Serco) is going live initially so we can assume that any moves created that
+    # don't have a mapped supplier location are ones created by Serco. This can be replaced by a
+    # smarter solution in the very near future.
+    supplier_location&.supplier || Supplier.find_by(key: 'serco')
   end
 end
