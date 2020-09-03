d0d92a5fe8ba9ee064fcee5db4bf1c5e607f4d2e
William Fish <william.michael.fish@gmail.com>
2020-09-03 10:30:19 +0100

    Update spec references to now-removed elite2api prefix

--- spec/lib/nomis_client/court_cases_spec.rb
+++ spec/lib/nomis_client/court_cases_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe NomisClient::CourtCases, with_nomis_client_authentication: true d
     it 'calls the nomis client with the correct booking id' do
       described_class.get(booking_id, filter_params)
 
-      expect(token).to have_received(:get).with('/elite2api/api/bookings/1495077/court-cases', {})
+      expect(token).to have_received(:get).with('/api/bookings/1495077/court-cases', {})
     end
 
     context 'when no filter_params are passed' do
       it 'returns active court cases' do
         described_class.get(booking_id)
 
-        expect(token).to have_received(:get).with("/elite2api/api/bookings/#{booking_id}/court-cases?activeOnly=true", {})
+        expect(token).to have_received(:get).with("/api/bookings/#{booking_id}/court-cases?activeOnly=true", {})
       end
     end
 
@@ -27,7 +27,7 @@ RSpec.describe NomisClient::CourtCases, with_nomis_client_authentication: true d
       it 'returns active court cases' do
         described_class.get(booking_id, filter_params)
 
-        expect(token).to have_received(:get).with("/elite2api/api/bookings/#{booking_id}/court-cases?activeOnly=true", {})
+        expect(token).to have_received(:get).with("/api/bookings/#{booking_id}/court-cases?activeOnly=true", {})
       end
     end
   end
