00b6f48fbebec7d3f36fb6ddfe76882031e61054
William Fish <william.michael.fish@gmail.com>
2020-09-07 17:44:13 +0100

    Adds factories for journey generic_events

--- spec/factories/generic_event.rb
+++ spec/factories/generic_event.rb
@@ -15,4 +15,46 @@ FactoryBot.define do
       }
     end
   end
+
+  factory :event_journey_cancel, parent: :generic_event, class: 'GenericEvent::JourneyCancel' do
+    eventable { association(:journey) }
+  end
+
+  factory :event_journey_complete, parent: :generic_event, class: 'GenericEvent::JourneyComplete' do
+    eventable { association(:journey) }
+  end
+
+  factory :event_journey_lockout, parent: :generic_event, class: 'GenericEvent::JourneyLockout' do
+    eventable { association(:journey) }
+    details do
+      {
+        from_location_id: create(:location).id,
+      }
+    end
+  end
+
+  factory :event_journey_lodging, parent: :generic_event, class: 'GenericEvent::JourneyLodging' do
+    eventable { association(:journey) }
+    details do
+      {
+        to_location_id: create(:location).id,
+      }
+    end
+  end
+
+  factory :event_journey_reject, parent: :generic_event, class: 'GenericEvent::JourneyReject' do
+    eventable { association(:journey) }
+  end
+
+  factory :event_journey_start, parent: :generic_event, class: 'GenericEvent::JourneyStart' do
+    eventable { association(:journey) }
+  end
+
+  factory :event_journey_uncancel, parent: :generic_event, class: 'GenericEvent::JourneyUncancel' do
+    eventable { association(:journey) }
+  end
+
+  factory :event_journey_uncomplete, parent: :generic_event, class: 'GenericEvent::JourneyUncomplete' do
+    eventable { association(:journey) }
+  end
 end
