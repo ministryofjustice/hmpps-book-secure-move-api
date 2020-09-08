e0728a2968d4c515032f95923d5155f9031e8410
William Fish <william.michael.fish@gmail.com>
2020-09-08 12:14:01 +0100

    Adds factories for move events

---
+++ spec/models/generic_event/move_accept_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe GenericEvent::MoveAccept do
+  subject(:generic_event) { build(:event_move_accept) }
+
+  it_behaves_like 'a move event' do
+    subject(:generic_event) { build(:event_move_accept) }
+  end
+
+  describe '#trigger' do
+    it 'does not persist changes to the eventable' do
+      generic_event.trigger
+      expect(generic_event.eventable).not_to be_persisted
+    end
+
+    it 'sets the eventable `status` to booked' do
+      expect { generic_event.trigger }.to change { generic_event.eventable.status }.from('requested').to('booked')
+    end
+  end
+end
