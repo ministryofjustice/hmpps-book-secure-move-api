432b585ba5d4d7714f7a2ae70ef7582a5f53c319
William Fish <william.michael.fish@gmail.com>
2020-09-21 16:42:15 +0100

    Adds url tag to sentry context

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,10 @@
-return unless ENV['SENTRY_DSN'].present?
+return if ENV['SENTRY_DSN'].blank?
 
 Raven.configure do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
+
+Raven.tags_context(
+  'url' => ENV['SERVER_FQDN'],
+)
