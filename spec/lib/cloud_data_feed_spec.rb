eb68b2580ff29c874e8c3f16c2f712c3eff4bcc5
Alexdesi <alexdesi@gmail.com>
2020-09-24 15:40:38 +0100

    Add new tasks to backfill generic event starting from a certain date

--- spec/lib/cloud_data_feed_spec.rb
+++ spec/lib/cloud_data_feed_spec.rb
@@ -12,8 +12,19 @@ RSpec.describe CloudDataFeed do
 
   it 'writes a file on S3 and return the full_name' do
     full_name = described_class.new('bucket_name')
-                               .write('some content', 'report.json')
+                    .write('some content', 'report.json')
 
     expect(full_name).to eq('2020/01/30/2020-01-30-report.json')
+  end
+
+  context 'when a report date is specified' do
+    let(:report_date) { Date.new(2020, 1, 15) }
+
+    it 'creates a file and names it base on the report date ' do
+      full_name = described_class.new('bucket_name')
+                                 .write('some content', 'report.json', report_date)
+
+      expect(full_name).to eq('2020/01/15/2020-01-15-report.json')
+    end
   end
 end
