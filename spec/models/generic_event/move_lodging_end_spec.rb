47541697f8221cb5797a27d88ad96a6d10178a0e
William Fish <william.michael.fish@gmail.com>
2020-09-24 16:23:35 +0100

    Lint

--- spec/models/generic_event/move_lodging_end_spec.rb
+++ spec/models/generic_event/move_lodging_end_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe GenericEvent::MoveLodgingStart do
+RSpec.describe GenericEvent::MoveLodgingEnd do
   subject(:generic_event) { build(:event_move_lodging_end) }
 
   it { is_expected.to validate_inclusion_of(:eventable_type).in_array(%w[Move]) }
