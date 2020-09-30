d6dbb182aa030fac81a05af3e919583f0b2b44b9
William Fish <william.michael.fish@gmail.com>
2020-09-30 13:30:06 +0100

    Reinstate removed relationship for specs. Removal coming soon

--- app/models/move.rb
+++ app/models/move.rb
@@ -85,6 +85,7 @@ class Move < VersionedModel
   has_many :journeys, -> { default_order }, dependent: :restrict_with_exception, inverse_of: :move
   has_many :court_hearings, dependent: :restrict_with_exception
 
+  has_many :move_events, as: :eventable, dependent: :destroy # NB: polymorphic association
   has_many :events, as: :eventable, dependent: :destroy # NB: polymorphic association
   has_many :generic_events, as: :eventable, dependent: :destroy # NB: polymorphic association
 
