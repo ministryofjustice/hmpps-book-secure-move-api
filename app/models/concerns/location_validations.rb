0c17cc8f186e23ca895248732d4a088652993b6e
William Fish <william.michael.fish@gmail.com>
2020-10-09 07:34:22 +0100

    Convention over configuration with ActiveRecord::RecordNotFound handling
    This change precludes the possibility of presence validations since we
    skip the mechanism for handling and rendering validation errors when we
    trigger the check for the record existing.

--- app/models/concerns/location_validations.rb
+++ app/models/concerns/location_validations.rb
@@ -14,11 +14,8 @@ module LocationValidations
       details[location_key] = location_id
     end
 
-    validates location_key, presence: true
-    validates_each location_key do |record, attr, value|
+    validates_each location_key do |_record, _attr, value|
       Location.find(value)
-    rescue ActiveRecord::RecordNotFound
-      record.errors.add(attr, "The location relationship you passed has an id that does not exist in our system. Please use an existing #{location_key.to_s.sub('_id', '')}")
     end
   end
 end
--- spec/support/event_requiring_a_location.rb
+++ spec/support/event_requiring_a_location.rb
@@ -1,28 +1,7 @@
 RSpec.shared_examples 'an event requiring a location' do |location_key|
-  it "validates #{location_key} presence" do
-    expect(generic_event).to validate_presence_of(location_key)
-  end
-
-  context "when the #{location_key} does not exist" do
-    let(:named_location) { location_key.to_s.sub('_id', '') }
-
-    before do
-      generic_event.public_send("#{location_key}=", 'flibble')
-    end
-
-    it { is_expected.not_to be_valid }
-
-    it 'attaches a custom message to the generic event' do
-      expect { generic_event.valid? }
-        .to change { generic_event.errors[location_key] }
-        .from([])
-        .to(["The location relationship you passed has an id that does not exist in our system. Please use an existing #{named_location}"])
-    end
-  end
-
   it "validates #{location_key} existing in the database" do
     generic_event.public_send("#{location_key}=", 'flibble')
 
-    expect(generic_event).not_to be_valid
+    expect { generic_event.valid? }.to raise_error(ActiveRecord::RecordNotFound, "Couldn't find Location with 'id'=flibble")
   end
 end
