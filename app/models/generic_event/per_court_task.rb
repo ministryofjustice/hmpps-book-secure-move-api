b5e0be9c5d083b46899eb2576361eb6d785b98c0
William Fish <william.michael.fish@gmail.com>
2020-10-09 18:20:28 +0100

    Adds missing DETAILS_ATTRIBUTES

--- app/models/generic_event/per_court_task.rb
+++ app/models/generic_event/per_court_task.rb
@@ -1,6 +1,9 @@
 class GenericEvent
   class PerCourtTask < GenericEvent
     LOCATION_ATTRIBUTE_KEY = :location_id
+    DETAILS_ATTRIBUTES = %w[
+      supplier_personnel_number
+    ].freeze
 
     include PersonEscortRecordEventValidations
     include LocationValidations
