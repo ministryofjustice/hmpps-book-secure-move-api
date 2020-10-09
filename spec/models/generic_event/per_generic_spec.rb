0ba1212b4a2d29c3f68c4cb2703a89072da502a5
William Fish <william.michael.fish@gmail.com>
2020-10-09 18:24:44 +0100

    Lint

--- spec/models/generic_event/per_generic_spec.rb
+++ spec/models/generic_event/per_generic_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe GenericEvent::PerCourtTask do
+RSpec.describe GenericEvent::PerGeneric do
   subject(:generic_event) { build(:event_per_generic) }
 
   it { is_expected.to validate_inclusion_of(:eventable_type).in_array(%w[PersonEscortRecord]) }
