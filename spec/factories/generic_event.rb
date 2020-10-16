fdb3aec49ca614a23ada1cf18bd4edef8de44a17
William Fish <william.michael.fish@gmail.com>
2020-10-16 16:54:49 +0100

    Adds person_move_used_force event

--- spec/factories/generic_event.rb
+++ spec/factories/generic_event.rb
@@ -568,4 +568,16 @@ FactoryBot.define do
       }
     end
   end
+
+  factory :event_person_move_used_force, parent: :generic_event, class: 'GenericEvent::PersonMoveUsedForce' do
+    eventable { association(:move) }
+    details do
+      {
+        location_id: create(:location).id,
+        supplier_personnel_number: SecureRandom.uuid,
+        vehicle_reg: Faker::Vehicle.license_plate,
+        reported_at: Time.zone.now.iso8601,
+      }
+    end
+  end
 end
