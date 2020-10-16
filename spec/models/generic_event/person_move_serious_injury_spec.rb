e6ff6ff957f0a27c1d020f536c9cda40fc2e662e
William Fish <william.michael.fish@gmail.com>
2020-10-16 16:51:26 +0100

    Adds person_move_serious_injury event

---
+++ spec/models/generic_event/person_move_serious_injury_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe GenericEvent::PersonMoveSeriousInjury do
+  subject(:generic_event) { build(:event_person_move_serious_injury) }
+
+  it_behaves_like 'an event with details', :supplier_personnel_numbers, :vehicle_reg, :reported_at
+  it_behaves_like 'an event with relationships', :location_id
+  it_behaves_like 'an event with eventable types', 'Person', 'Move'
+  it_behaves_like 'an event requiring a location', :location_id
+  it_behaves_like 'an event that specifies a vehicle registration'
+
+  it { is_expected.to validate_presence_of(:supplier_personnel_numbers) }
+
+  context 'when reported_at is not a valid iso8601 date' do
+    before do
+      generic_event.reported_at = '2019/01/01'
+    end
+
+    it { is_expected.not_to be_valid }
+  end
+end
