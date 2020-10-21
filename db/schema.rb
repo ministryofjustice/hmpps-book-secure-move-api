156d41df2795740b3358a8d47352cd56a2bcc347
cwrw <abeer.salameh@gmail.com>
2020-10-21 08:01:06 +0100

    P4-2356 Add prefilled to framework responses, and prefill source to PER
    jira-ticket: https://dsdmoj.atlassian.net/browse/P4-2356
    
    Add `prefilled` column to `framework_responses`, which defaults to
    false. This value will determine if a response was prefilled on creation.
    
    Add a `prefill_source_id` to `person_escort_records` to persist the
    person escort record the current person escort record was prefilled
    from on creation if applicable.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_055517) do
+ActiveRecord::Schema.define(version: 2020_10_21_065138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -211,6 +211,7 @@ ActiveRecord::Schema.define(version: 2020_10_21_055517) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "responded", default: false, null: false
+    t.boolean "prefilled", default: false, null: false
     t.index ["framework_question_id"], name: "index_framework_responses_on_framework_question_id"
     t.index ["parent_id"], name: "index_framework_responses_on_parent_id"
     t.index ["person_escort_record_id"], name: "index_framework_responses_on_person_escort_record_id"
@@ -464,8 +465,10 @@ ActiveRecord::Schema.define(version: 2020_10_21_055517) do
     t.datetime "confirmed_at"
     t.uuid "move_id"
     t.jsonb "nomis_sync_status", default: [], null: false
+    t.uuid "prefill_source_id"
     t.index ["framework_id"], name: "index_person_escort_records_on_framework_id"
     t.index ["move_id"], name: "index_person_escort_records_on_move_id"
+    t.index ["prefill_source_id"], name: "index_person_escort_records_on_prefill_source_id"
     t.index ["profile_id"], name: "index_person_escort_records_on_profile_id", unique: true
   end
 
