a7ee66b59786b19e76ca9c1f5d23a4d12d682540
William Fish <william.michael.fish@gmail.com>
2020-10-22 09:21:23 +0100

    Adds incident fault classification enum to events
    jira link: https://dsdmoj.atlassian.net/browse/P4-2394
    
    Fault classification is needed to on every request to create an incident
    event.
    
    - [x] Adds fault classification to the parent incident event model
    - [x] Adds fault classification to the tests of the individual incident
      models via shared examples

--- app/models/generic_event/incident.rb
+++ app/models/generic_event/incident.rb
@@ -3,14 +3,21 @@ class GenericEvent
     LOCATION_ATTRIBUTE_KEY = :location_id
 
     def self.inherited(child)
-      child.details_attributes :supplier_personnel_numbers, :vehicle_reg, :reported_at
+      child.details_attributes :supplier_personnel_numbers, :vehicle_reg, :reported_at, :fault_classification
       child.relationship_attributes :location_id
       child.eventable_types 'Move', 'Person'
+      child.enum fault_classification: {
+        not_supplier: 'not_supplier',
+        supplier: 'supplier',
+        investigation: 'investigation',
+      }
 
       child.include LocationValidations
       child.include SupplierPersonnelNumberValidations
 
       child.validates :reported_at, iso_date_time: true
+      child.validates :fault_classification, inclusion: { in: child.fault_classifications }
+
       super
     end
   end
--- spec/support/an_event_about_an_incident.rb
+++ spec/support/an_event_about_an_incident.rb
@@ -1,10 +1,19 @@
 RSpec.shared_examples 'an event about an incident' do
-  it_behaves_like 'an event with details', :supplier_personnel_numbers, :vehicle_reg, :reported_at
+  it_behaves_like 'an event with details', :supplier_personnel_numbers, :vehicle_reg, :reported_at, :fault_classification
   it_behaves_like 'an event with relationships', :location_id
   it_behaves_like 'an event with eventable types', 'Person', 'Move'
   it_behaves_like 'an event requiring a location', :location_id
 
+  let(:fault_classifications) do
+    %w[
+      not_supplier
+      supplier
+      investigation
+    ]
+  end
+
   it { is_expected.to validate_presence_of(:supplier_personnel_numbers) }
+  it { is_expected.to validate_inclusion_of(:fault_classification).in_array(fault_classifications) }
 
   context 'when reported_at is not a valid iso8601 date' do
     before do
