1575e1e0d6fb5f84fec1dc1616fd445b1eaa36e8
cwrw <abeer.salameh@gmail.com>
2020-10-28 09:17:12 +0000

    Enforce the application to run under HTTPS in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # set to :info so that logging for synchroniser shows up in staging/preprod/production
   #
