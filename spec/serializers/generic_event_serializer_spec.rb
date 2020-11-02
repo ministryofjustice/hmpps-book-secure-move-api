df6e6d56aa45b408239dc48a6ff76a7cadfd09ad
William Fish <william.michael.fish@gmail.com>
2020-11-02 13:28:20 +0000

    jira link: https://dsdmoj.atlassian.net/browse/P4-2423
    Support eventable as an include in the move and generic event serializer
    
    The frontend needs this for contextual information in the event
    timeline.

--- spec/serializers/generic_event_serializer_spec.rb
+++ spec/serializers/generic_event_serializer_spec.rb
@@ -8,32 +8,41 @@ RSpec.describe GenericEventSerializer do
   let(:event) { create :event_move_cancel }
   let(:result) { JSON.parse(serializer.serializable_hash.to_json).deep_symbolize_keys }
 
-  context 'with no options' do
-    let(:adapter_options) { {} }
-    let(:expected_json) do
-      {
-        data: {
-          id: event.id,
-          type: 'events',
-          attributes: {
-            occurred_at: event.occurred_at.iso8601,
-            recorded_at: event.recorded_at.iso8601,
-            notes: 'Flibble',
-            event_type: 'MoveCancel',
-            details: {
-              cancellation_reason: 'made_in_error',
-              cancellation_reason_comment: 'It was a mistake',
-            },
-          },
-          relationships: {
-            eventable: { data: { type: 'moves', id: move.id } },
+  let(:expected_json) do
+    {
+      data: {
+        id: event.id,
+        type: 'events',
+        attributes: {
+          occurred_at: event.occurred_at.iso8601,
+          recorded_at: event.recorded_at.iso8601,
+          notes: 'Flibble',
+          event_type: 'MoveCancel',
+          details: {
+            cancellation_reason: 'made_in_error',
+            cancellation_reason_comment: 'It was a mistake',
           },
         },
-      }
-    end
+        relationships: {
+          eventable: { data: { type: 'moves', id: move.id } },
+        },
+      },
+    }
+  end
+
+  context 'with no options' do
+    let(:adapter_options) { {} }
     let(:move) { event.eventable }
 
     it { expect(result).to include_json(expected_json) }
     it { expect(result[:included]).to be_nil }
+  end
+
+  context 'with include eventable' do
+    let(:adapter_options) { { include: [:eventable] } }
+    let(:move) { event.eventable }
+
+    it { expect(result).to include_json(expected_json) }
+    it { expect(result[:included].map { |include| include[:type] }).to eq(%w[moves]) }
   end
 end
