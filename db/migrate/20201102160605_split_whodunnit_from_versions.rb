65938cb0001fdb65d30a8e2c6576150459059039
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2020-11-03 09:46:22 +0000

    Change migration to use raw SQL

--- db/migrate/20201102160605_split_whodunnit_from_versions.rb
+++ db/migrate/20201102160605_split_whodunnit_from_versions.rb
@@ -1,31 +1,9 @@
 class SplitWhodunnitFromVersions < ActiveRecord::Migration[6.0]
   def up
-    count = 0
-
-    PaperTrail::Version.all.each do |version|
-      next if version.whodunnit.blank?
-
-      begin
-        Supplier.find(version.whodunnit)
-      rescue ActiveRecord::RecordNotFound
-        next
-      end
-
-      version.update_attributes(supplier_id: version.whodunnit, whodunnit: nil)
-      count += 1
-    end
-
-    p "Changed #{count} PaperTrail::Versions from whodunnit to supplier_id"
+    execute('UPDATE versions SET supplier_id = whodunnit::uuid, whodunnit = null WHERE whodunnit IN (SELECT id::text FROM suppliers)')
   end
 
   def down
-    count = 0
-
-    PaperTrail::Version.where.not(supplier_id: nil).each do |version|
-      version.update_attributes(supplier_id: nil, whodunnit: version.supplier_id)
-      count += 1
-    end
-
-    p "Changed #{count} PaperTrail::Versions from supplier_id to whodunnit"
+    execute('UPDATE versions SET whodunnit = supplier_id, supplier_id = null WHERE supplier_id IS NOT NULL')
   end
 end
