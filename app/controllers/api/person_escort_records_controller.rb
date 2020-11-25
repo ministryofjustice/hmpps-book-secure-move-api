c2c12d8a5324ed66fb03a8fcf48b0667dfec564e
cwrw <abeer.salameh@gmail.com>
2020-11-25 05:23:33 +0000

    P4-2449 Move assessmentable logic to abstract controller for reuse
    jira-ticket: https://dsdmoj.atlassian.net/browse/P4-2449
    
    To allow support for multiple assessments, move shared logic to an
    abstract controller for "assessmentables". Define the specific class
    in the specific controller for the assessment.
    
    Also move param validator to assessmentable service.

--- app/controllers/api/person_escort_records_controller.rb
+++ app/controllers/api/person_escort_records_controller.rb
@@ -1,68 +1,15 @@
 # frozen_string_literal: true
 
 module Api
-  class PersonEscortRecordsController < ApiController
-    after_action :send_notification, only: :update
-
-    NEW_PERMITTED_PER_PARAMS = [
-      :type,
-      attributes: [:version],
-      relationships: [move: {}],
-    ].freeze
-
-    UPDATE_PERMITTED_PER_PARAMS = [
-      :type,
-      attributes: [:status],
-    ].freeze
-
-    def create
-      person_escort_record = PersonEscortRecord.save_with_responses!(
-        version: new_person_escort_record_params.dig(:attributes, :version),
-        move_id: new_person_escort_record_params.dig(:relationships, :move, :data, :id),
-      )
-
-      render_person_escort_record(person_escort_record, :created)
-    end
-
-    def update
-      PersonEscortRecords::ParamsValidator.new(update_person_escort_record_status).validate!
-      person_escort_record.confirm!(update_person_escort_record_status)
-
-      render_person_escort_record(person_escort_record, :ok)
-    end
-
-    def show
-      render_person_escort_record(person_escort_record, :ok)
-    end
-
+  class PersonEscortRecordsController < FrameworkAssessmentsController
   private
 
-    def new_person_escort_record_params
-      params.require(:data).permit(NEW_PERMITTED_PER_PARAMS).to_h
+    def assessment_class
+      @assessment_class ||= PersonEscortRecord
     end
 
-    def update_person_escort_record_params
-      params.require(:data).permit(UPDATE_PERMITTED_PER_PARAMS)
-    end
-
-    def update_person_escort_record_status
-      update_person_escort_record_params.to_h.dig(:attributes, :status)
-    end
-
-    def supported_relationships
-      PersonEscortRecordSerializer::SUPPORTED_RELATIONSHIPS
-    end
-
-    def person_escort_record
-      @person_escort_record ||= PersonEscortRecord.find(params[:id])
-    end
-
-    def render_person_escort_record(person_escort_record, status)
-      render_json person_escort_record, serializer: PersonEscortRecordSerializer, include: included_relationships, status: status
-    end
-
-    def send_notification
-      Notifier.prepare_notifications(topic: person_escort_record, action_name: 'update')
+    def assessment_serializer
+      @assessment_serializer ||= PersonEscortRecordSerializer
     end
   end
 end
