500c03adcaf52ecdeaf33068949a1bcae1520461
cwrw <abeer.salameh@gmail.com>
2021-01-07 17:02:08 +0200

    P4-2589 Use value_type column value in response serializer
    jira-ticket: https://dsdmoj.atlassian.net/browse/P4-2589
    
    Switch to using persisted value type on responses,
    to avoid having to reach into framework questions

--- app/serializers/framework_response_serializer.rb
+++ app/serializers/framework_response_serializer.rb
@@ -10,11 +10,7 @@ class FrameworkResponseSerializer
   has_many :flags, serializer: FrameworkFlagSerializer, &:framework_flags
   has_many :nomis_mappings, serializer: FrameworkNomisMappingSerializer, &:framework_nomis_mappings
 
-  attributes :value, :responded, :prefilled
-
-  attribute :value_type do |object|
-    object.framework_question.response_type
-  end
+  attributes :value, :responded, :prefilled, :value_type
 
   SUPPORTED_RELATIONSHIPS = %w[
     assessment
