8c2d8848ca713958fc2090f2750e016429158261
Martyn Whitwell <martyn.whitwell@gmail.com>
2021-02-08 18:08:30 +0000

    P4-2655 relaxed postcodes

--- spec/models/generic_event/person_move_vehicle_broke_down_spec.rb
+++ spec/models/generic_event/person_move_vehicle_broke_down_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe GenericEvent::PersonMoveVehicleBrokeDown do
   subject(:generic_event) { build(:event_person_move_vehicle_broke_down) }
 
-  it_behaves_like 'an event with details', :supplier_personnel_numbers, :vehicle_reg, :reported_at, :postcode
+  it_behaves_like 'an event with details', :supplier_personnel_numbers, :vehicle_reg, :reported_at, :postcode, :location_description
   it_behaves_like 'an event with relationships', location_id: :locations
   it_behaves_like 'an event with eventable types', 'Person', 'Move'
   it_behaves_like 'an event requiring a location', :location_id
@@ -21,6 +21,15 @@ RSpec.describe GenericEvent::PersonMoveVehicleBrokeDown do
   context 'when reported_at is not a valid iso8601 date' do
     before do
       generic_event.reported_at = '2019/01/01'
+    end
+
+    it { is_expected.not_to be_valid }
+  end
+
+  context 'when the postcode and the location_description are missing' do
+    before do
+      generic_event.postcode = nil
+      generic_event.location_description = nil
     end
 
     it { is_expected.not_to be_valid }
