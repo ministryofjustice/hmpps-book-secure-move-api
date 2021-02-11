6e5d6cfd83c330c14613617c78b5d9902b879036
Andrew Retmanski <aretmanski@gmail.com>
2021-02-11 14:15:49 +0000

    Small cleanup of legacy ENV vars for Govuk notify service The `GOVUK_NOTIFY_TEMPLATE_ID` variable has now been retired and replaced with `GOVUK_NOTIFY_MOVE_TEMPLATE_ID` and `GOVUK_NOTIFY_PER_TEMPLATE_ID`.

--- config/initializers/gov_uk_notify.rb
+++ config/initializers/gov_uk_notify.rb
@@ -1,15 +1,11 @@
 # We only send emails if GOVUK_NOTIFY_ENABLED is true - and should generally only be enabled on the Sidekiq pod
 # Note that the app pod and metrics containers should not send GovUk Notify emails
 if ENV['GOVUK_NOTIFY_ENABLED'] =~ /true/i
-  if ENV['GOVUK_NOTIFY_API_KEY'].blank?
-    Rails.logger.warn('GOVUK_NOTIFY_API_KEY env var is not set; emails cannot be sent')
-    Raven.capture_message('GOVUK_NOTIFY_API_KEY env var is not set; emails cannot be sent', { level: 'warning' })
-  end
-
-  # TODO: Remove use of GOVUK_NOTIFY_TEMPLATE_ID - change to GOVUK_NOTIFY_MOVE_TEMPLATE_ID & GOVUK_NOTIFY_PER_TEMPLATE_ID
-  if ENV['GOVUK_NOTIFY_TEMPLATE_ID'].blank?
-    Rails.logger.warn('GOVUK_NOTIFY_TEMPLATE_ID env var is not set; emails cannot be sent')
-    Raven.capture_message('GOVUK_NOTIFY_TEMPLATE_ID env var is not set; emails cannot be sent', { level: 'warning' })
+  %w[GOVUK_NOTIFY_API_KEY GOVUK_NOTIFY_MOVE_TEMPLATE_ID GOVUK_NOTIFY_PER_TEMPLATE_ID].each do |name|
+    if ENV[name].blank?
+      Rails.logger.warn("#{name} env var is not set; emails cannot be sent")
+      Raven.capture_message("#{name} env var is not set; emails cannot be sent", { level: 'warning' })
+    end
   end
 
   ActionMailer::Base.add_delivery_method :govuk_notify, GovukNotifyRails::Delivery, api_key: ENV.fetch('GOVUK_NOTIFY_API_KEY', nil)
