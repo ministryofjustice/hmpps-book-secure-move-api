b0d66368d8123c409d07f93a417773092a993a24
Martyn Whitwell <martyn.whitwell@gmail.com>
2021-03-04 16:26:55 +0000

    wip

--- app/services/generic_events/runner.rb
+++ app/services/generic_events/runner.rb
@@ -4,9 +4,25 @@ module GenericEvents
       @eventable = eventable
     end
 
-    def call(dry_run: false)
-      @eventable.generic_events.applied_order.each { |e| e.trigger({ dry_run: dry_run }) }
-      @eventable.handle_event_run unless dry_run
+    def call(dry_run: false, &block)
+
+      if dry_run
+        @eventable.transaction do
+
+          @eventable.generic_events.applied_order.each do |event|
+            event.trigger({ dry_run: dry_run })
+            yield event if block_given?
+          end
+
+          raise ActiveRecord::Rollback
+        end
+      else
+        @eventable.generic_events.applied_order.each do |event|
+          event.trigger({ dry_run: dry_run })
+          yield event if block_given?
+        end
+        @eventable.handle_event_run
+      end
     end
   end
 end
