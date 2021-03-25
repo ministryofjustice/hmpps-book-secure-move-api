29194f0198051c7eab5c4567875775833e95be0d
Martyn Whitwell <martyn.whitwell@gmail.com>
2021-03-25 12:42:00 +0000

    Cleanup defunct move and PER prometheus counters
    (replaced by writing metrics to buckets)

--- lib/prometheus/collectors.rb
+++ lib/prometheus/collectors.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-# NB: this file is executed by the "book-secure-move-metrics" container in each pod
+# NB: Keep this file here as it is executed by the "book-secure-move-metrics" container in each pod
 require File.expand_path('../../config/environment', __dir__) unless defined? Rails
-require './lib/prometheus/move_collector'
-require './lib/prometheus/person_escort_record_collector'
