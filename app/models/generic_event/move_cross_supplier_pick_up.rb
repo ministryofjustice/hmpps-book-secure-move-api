1ca30793206f0412fdd443a895e5338963555455
Martyn Whitwell <martyn.whitwell@gmail.com>
2021-04-29 08:54:54 +0100

    wip

--- app/models/generic_event/move_cross_supplier_pick_up.rb
+++ app/models/generic_event/move_cross_supplier_pick_up.rb
@@ -1,10 +1,26 @@
 class GenericEvent
   class MoveCrossSupplierPickUp < GenericEvent
-    relationship_attributes previous_move_id: :moves
+    details_attributes :previous_move_reference
+
+    # relationship_attributes previous_move_id: :moves
+    # relationship_attributes previous_move_reference: :moves
+
     eventable_types 'Move'
 
-    validates_each :previous_move_id do |_record, _attr, value|
-      Move.find(value)
+    # validates_each :previous_move_id do |_record, _attr, value|
+    #   Move.find(value)
+    # end
+
+    def previous_move
+
+    end
+
+    def previous_move_reference
+
+    end
+
+    def previous_move_reference=
+
     end
   end
 end
