54b9881971973a714bc31d52ce3e301f9775e6a5
Thomas Leese <thomas@leese.io>
2021-07-16 12:38:40 +0100

    Add FrameworkNomisMappings::Assessments
    This adds a new importer for assessments to fetch them from the API and
    turn them into `FrameworkNomisMapping` instances so we can start to
    display them on the frontend as approriate.

---
+++ app/services/framework_nomis_mappings/assessments.rb
@@ -0,0 +1,56 @@
+module FrameworkNomisMappings
+  class Assessments
+    attr_reader :booking_id, :nomis_sync_status
+
+    def initialize(booking_id:, nomis_sync_status: FrameworkNomisMappings::NomisSyncStatus.new(resource_type: 'assessments'))
+      @booking_id = booking_id
+      @nomis_sync_status = nomis_sync_status
+    end
+
+    def call
+      return [] if booking_id.blank?
+
+      build_mappings.compact
+    end
+
+  private
+
+    def imported_assessments
+      @imported_assessments ||= NomisClient::Assessments.get(booking_id: booking_id).tap do
+        nomis_sync_status.set_success
+      end
+    rescue Faraday::ConnectionFailed, Faraday::TimeoutError, OAuth2::Error => e
+      Rails.logger.warn "Importing framework assessments mappings error: #{e.message}"
+      nomis_sync_status.set_failure(message: e.message)
+
+      []
+    end
+
+    def build_mappings
+      imported_assessments.map do |assessment|
+        next unless valid_assessment?(assessment)
+
+        FrameworkNomisMapping.new(
+          raw_nomis_mapping: assessment,
+          code_type: 'assessment',
+          code: assessment[:assessment_code],
+          code_description: assessment[:assessment_description],
+          comments: assessment_comments(assessment),
+          approval_date: assessment[:approval_date],
+          next_review_date: assessment[:next_review_date],
+        )
+      end
+    end
+
+    def assessment_comments(assessment)
+      comments = assessment[:classification]
+      comments += " — #{assessment[:assessment_comment]}" if assessment[:assessment_comment].present?
+      comments
+    end
+
+    def valid_assessment?(assessment)
+      next_review_date = assessment[:next_review_date].present? ? Date.parse(assessment[:next_review_date]) : nil
+      next_review_date.nil? || next_review_date >= Time.zone.today
+    end
+  end
+end
