4e780c97a1785cd85cd592bd11e01fc80fecd23b
Thomas Leese <thomas@leese.io>
2021-10-04 11:38:02 +0100

    Modify reports:person_escort_record_quality to send email
    Rather than print out a CSV we will now send an email to the recipients
    who are recorded as wanting to know about the results of the report.

--- spec/lib/tasks/reports/person_escort_record_quality_spec.rb
+++ spec/lib/tasks/reports/person_escort_record_quality_spec.rb
@@ -5,39 +5,25 @@ require 'rails_helper'
 RSpec.describe Rake::Task['reports:person_escort_record_quality'] do
   before do
     allow(Reports::PersonEscortRecordQuality).to receive(:call).and_return('csv')
-
+    travel_to Time.zone.local(2020, 2, 1)
     described_class.reenable
   end
 
-  context 'without a start date' do
-    it 'raises an error' do
-      expect { described_class.invoke }.to raise_error(KeyError)
+  after { travel_back }
+
+  around do |example|
+    ClimateControl.modify(PER_QUALITY_REPORT_RECIPIENTS: 'test1@example.com,test2@example.com') do
+      example.run
     end
   end
 
-  context 'with a start date' do
-    it 'writes the CSV to stdout' do
-      expect { described_class.invoke('2020-01-01') }.to output("csv\n").to_stdout
-    end
-
-    it 'parses the date and passes it to the report' do
-      described_class.invoke('2020-01-01')
-
-      expect(Reports::PersonEscortRecordQuality)
-        .to have_received(:call).with(start_date: Date.new(2020, 1, 1), end_date: nil)
-    end
-
-    context 'with an end date' do
-      it 'writes the CSV to stdout' do
-        expect { described_class.invoke('2020-01-01', '2021-01-01') }.to output("csv\n").to_stdout
-      end
-
-      it 'parses the date and passes it to the report' do
-        described_class.invoke('2020-01-01', '2021-01-01')
-
-        expect(Reports::PersonEscortRecordQuality)
-          .to have_received(:call).with(start_date: Date.new(2020, 1, 1), end_date: Date.new(2021, 1, 1))
-      end
-    end
+  it 'calls the mailer with the right parameters' do
+    expect { described_class.invoke }
+      .to have_enqueued_mail(ReportMailer, :person_escort_record_quality)
+            .with(a_hash_including(params: {
+              recipients: ['test1@example.com', 'test2@example.com'],
+              start_date: Date.new(2020, 1, 1),
+              end_date: Date.new(2020, 3, 31),
+            }))
   end
 end
