92f9bb7e5b9229d2eccdc922cb050ecd34cc9922
Adam Sharp <adam.sharp@digital.justice.gov.uk>
2021-10-07 16:59:22 +0100

    Add vehicle_reg to JourneyStart/Complete

--- app/models/generic_event/journey_complete.rb
+++ app/models/generic_event/journey_complete.rb
@@ -1,8 +1,15 @@
 class GenericEvent
   class JourneyComplete < GenericEvent
+    details_attributes :vehicle_reg
     eventable_types 'Journey'
 
     def trigger(*)
+      if vehicle_reg.present?
+        eventable.vehicle_registration = vehicle_reg
+      else
+        Sentry.capture_message("#{self.class} created without vehicle_reg", level: 'warning', extra: { supplier: supplier&.key })
+      end
+
       eventable.complete
     end
   end
--- spec/models/generic_event/journey_start_spec.rb
+++ spec/models/generic_event/journey_start_spec.rb
@@ -3,7 +3,32 @@ require 'rails_helper'
 RSpec.describe GenericEvent::JourneyStart do
   subject(:generic_event) { build(:event_journey_start) }
 
-  it_behaves_like 'a journey event', :start do
-    subject(:generic_event) { build(:event_journey_start) }
+  it_behaves_like 'a journey event', :start
+
+  context 'when supplied a vehicle_reg' do
+    before do
+      generic_event.vehicle_reg = 'a vehicle reg'
+    end
+
+    it 'sets the vehicle_registration on the eventable' do
+      generic_event.trigger
+      expect(generic_event.eventable.vehicle_registration).to eq(generic_event.vehicle_reg)
+    end
+  end
+
+  context 'when not supplied a vehicle_reg' do
+    let(:supplier) { create(:supplier, :serco) }
+
+    before do
+      generic_event.vehicle_reg = nil
+      generic_event.supplier = supplier
+      allow(Sentry).to receive(:capture_message)
+    end
+
+    it 'does not set the vehicle_registration on the eventable and logs to sentry' do
+      expect(Sentry).to receive(:capture_message).with('GenericEvent::JourneyStart created without vehicle_reg', level: 'warning', extra: { supplier: supplier&.key })
+      generic_event.trigger
+      expect(generic_event.eventable.vehicle_registration).to eq('AB12 CDE')
+    end
   end
 end
