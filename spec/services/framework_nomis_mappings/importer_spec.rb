271e808792ff53d1bbf4c63e42de4923e279abdd
Thomas Leese <thomas@leese.io>
2021-10-13 08:43:53 +0100

    Import contacts from NOMIS
    This includes contacts in the `FrameworkNomisMappings::Importer` to make
    sure they are imported as well as the existing types.

--- spec/services/framework_nomis_mappings/importer_spec.rb
+++ spec/services/framework_nomis_mappings/importer_spec.rb
@@ -30,6 +30,31 @@ RSpec.describe FrameworkNomisMappings::Importer do
     ]
   end
 
+  let(:nomis_assessments) do
+    [
+      {
+        assessment_code: 'CSR',
+        assessment_description: 'Cell Share Risk Assessment',
+        classification: 'Standard',
+        approval_date: '2020-01-01',
+        next_review_date: '2100-04-23',
+        offender_no: '321',
+      },
+    ]
+  end
+
+  let(:nomis_contacts) do
+    [
+      {
+        active_flag: true,
+        next_of_kin: true,
+        first_name: 'First',
+        middle_name: 'Middle',
+        last_name: 'Last',
+      },
+    ]
+  end
+
   let(:nomis_reasonable_adjustments) do
     [
       {
@@ -72,19 +97,6 @@ RSpec.describe FrameworkNomisMappings::Importer do
     ]
   end
 
-  let(:nomis_assessments) do
-    [
-      {
-        assessment_code: 'CSR',
-        assessment_description: 'Cell Share Risk Assessment',
-        classification: 'Standard',
-        approval_date: '2020-01-01',
-        next_review_date: '2100-04-23',
-        offender_no: '321',
-      },
-    ]
-  end
-
   let(:framework_response1) do
     alert_code = create(:framework_nomis_code, code: 'VI', code_type: 'alert')
     question = create(:framework_question, framework_nomis_codes: [alert_code])
@@ -96,7 +108,8 @@ RSpec.describe FrameworkNomisMappings::Importer do
     reasonable_adjustment_fallback = create(:framework_nomis_code, code: nil, code_type: 'reasonable_adjustment', fallback: true)
     personal_care_need_fallback = create(:framework_nomis_code, code: nil, code_type: 'personal_care_need', fallback: true)
     assessment_code = create(:framework_nomis_code, code: 'CSR', code_type: 'assessment')
-    question = create(:framework_question, framework_nomis_codes: [personal_care_need_code, reasonable_adjustment_fallback, personal_care_need_fallback, assessment_code])
+    contact_code = create(:framework_nomis_code, code: 'NEXTOFKIN', code_type: 'contact')
+    question = create(:framework_question, framework_nomis_codes: [assessment_code, contact_code, personal_care_need_code, reasonable_adjustment_fallback, personal_care_need_fallback])
     create(:string_response, framework_question: question)
   end
 
@@ -106,15 +119,16 @@ RSpec.describe FrameworkNomisMappings::Importer do
 
   before do
     allow(NomisClient::Alerts).to receive(:get).and_return(nomis_alerts)
+    allow(NomisClient::Assessments).to receive(:get).and_return(nomis_assessments)
+    allow(NomisClient::Contacts).to receive(:get).and_return(nomis_contacts)
     allow(NomisClient::PersonalCareNeeds).to receive(:get).and_return(nomis_personal_care_needs)
     allow(NomisClient::ReasonableAdjustments).to receive(:get).and_return(nomis_reasonable_adjustments)
-    allow(NomisClient::Assessments).to receive(:get).and_return(nomis_assessments)
   end
 
-  it 'persists all alerts, reasonable adjustments, personal care needs and assessments from NOMIS clients as NOMIS mappings' do
+  it 'persists all alerts, assessments, contacts, reasonable adjustments and personal care needs from NOMIS clients as NOMIS mappings' do
     person_escort_record = create(:person_escort_record, framework_responses: [framework_response1, framework_response2], profile: person.profiles.first)
 
-    expect { described_class.new(assessmentable: person_escort_record).call }.to change(FrameworkNomisMapping, :count).by(7)
+    expect { described_class.new(assessmentable: person_escort_record).call }.to change(FrameworkNomisMapping, :count).by(8)
   end
 
   it 'associates NOMIS mappings correctly to framework responses' do
@@ -139,7 +153,7 @@ RSpec.describe FrameworkNomisMappings::Importer do
     person_escort_record = create(:person_escort_record, framework_responses: [framework_response1, framework_response2], profile: person.profiles.first)
     described_class.new(assessmentable: person_escort_record).call
 
-    expect(framework_response2.framework_nomis_mappings.pluck(:code)).to contain_exactly('VI', 'DA', 'PEEP', 'BA', 'CSR')
+    expect(framework_response2.framework_nomis_mappings.pluck(:code)).to contain_exactly('VI', 'DA', 'PEEP', 'BA', 'CSR', 'NEXTOFKIN')
   end
 
   it 'associates NOMIS mapping codes to responses scoped to NOMIS mapping type' do
@@ -147,7 +161,7 @@ RSpec.describe FrameworkNomisMappings::Importer do
     described_class.new(assessmentable: person_escort_record).call
 
     expect(framework_response1.framework_nomis_mappings.pluck(:code_type).uniq).to contain_exactly('alert')
-    expect(framework_response2.framework_nomis_mappings.pluck(:code_type).uniq).to contain_exactly('reasonable_adjustment', 'personal_care_need', 'assessment')
+    expect(framework_response2.framework_nomis_mappings.pluck(:code_type).uniq).to contain_exactly('assessment', 'contact', 'reasonable_adjustment', 'personal_care_need')
   end
 
   it 'does not associate NOMIS mappings mapped to a fallback if none exist' do
@@ -162,14 +176,15 @@ RSpec.describe FrameworkNomisMappings::Importer do
     allow(NomisClient::Alerts).to receive(:get).and_raise(OAuth2::Error, oauth2_response)
     person_escort_record = create(:person_escort_record, framework_responses: [framework_response1, framework_response2], profile: person.profiles.first)
 
-    expect { described_class.new(assessmentable: person_escort_record).call }.to change(FrameworkNomisMapping, :count).by(5)
+    expect { described_class.new(assessmentable: person_escort_record).call }.to change(FrameworkNomisMapping, :count).by(6)
   end
 
   it 'does nothing if no NOMIS mappings present for a person' do
     allow(NomisClient::Alerts).to receive(:get).and_return([])
+    allow(NomisClient::Assessments).to receive(:get).and_return([])
+    allow(NomisClient::Contacts).to receive(:get).and_return([])
     allow(NomisClient::PersonalCareNeeds).to receive(:get).and_return([])
     allow(NomisClient::ReasonableAdjustments).to receive(:get).and_return([])
-    allow(NomisClient::Assessments).to receive(:get).and_return([])
     person_escort_record = create(:person_escort_record, framework_responses: [framework_response1, framework_response2], profile: person.profiles.first)
 
     expect { described_class.new(assessmentable: person_escort_record).call }.not_to change(FrameworkNomisMapping, :count)
@@ -199,9 +214,10 @@ RSpec.describe FrameworkNomisMappings::Importer do
     expect(person_escort_record.nomis_sync_status).to include_json(
       [
         { 'resource_type' => 'alerts', 'status' => 'success' },
+        { 'resource_type' => 'assessments', 'status' => 'success' },
+        { 'resource_type' => 'contacts', 'status' => 'success' },
         { 'resource_type' => 'personal_care_needs', 'status' => 'success' },
         { 'resource_type' => 'reasonable_adjustments', 'status' => 'success' },
-        { 'resource_type' => 'assessments', 'status' => 'success' },
       ],
     )
   end
@@ -216,9 +232,10 @@ RSpec.describe FrameworkNomisMappings::Importer do
     expect(person_escort_record.nomis_sync_status).to include_json(
       [
         { 'resource_type' => 'alerts', 'status' => 'failed' },
+        { 'resource_type' => 'assessments', 'status' => 'success' },
+        { 'resource_type' => 'contacts', 'status' => 'success' },
         { 'resource_type' => 'personal_care_needs', 'status' => 'success' },
         { 'resource_type' => 'reasonable_adjustments', 'status' => 'success' },
-        { 'resource_type' => 'assessments', 'status' => 'success' },
       ],
     )
   end
