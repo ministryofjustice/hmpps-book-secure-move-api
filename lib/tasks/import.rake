459d770753f51e35a482ed15ec7569cd87643181
Thomas Leese <thomas@leese.io>
2021-10-21 09:39:58 +0100

    Add import:journeys_missing_vehicle:serco task
    This runs the importer task for missing vehicles following the
    conventions of the spreadsheet provided by Serco.

--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 namespace :import do
+  namespace :journeys_missing_vehicle do
+    desc "Import journeys which are missing a vehicle using Serco's spreadsheets."
+    task :serco, [:csv_path] => :environment do |_, args|
+      csv_path = args.fetch(:csv_path)
+      columns = {
+        journey_id: :BASMMOJJourneyID,
+        move_id: :BASMMOJMoveID,
+        vehicle_registration: :SERs_vehicleReg,
+      }
+
+      print Imports::JourneysMissingVehicle.call(csv_path: csv_path, columns: columns).summary
+    end
+  end
 end
---
+++ spec/lib/tasks/imports/journeys_missing_vehicle_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Rake::Task['import:journeys_missing_vehicle:serco'] do
+  let(:results) { instance_double('Imports::Results') }
+
+  before do
+    allow(results).to receive(:summary).and_return('Summary')
+    allow(Imports::JourneysMissingVehicle).to receive(:call).and_return(results)
+    described_class.reenable
+  end
+
+  it 'calls the importer with the right parameters' do
+    expect(Imports::JourneysMissingVehicle).to receive(:call).with(
+      csv_path: 'data.csv',
+      columns: {
+        journey_id: :BASMMOJJourneyID,
+        move_id: :BASMMOJMoveID,
+        vehicle_registration: :SERs_vehicleReg,
+      },
+    )
+
+    expect { described_class.invoke('data.csv') }
+      .to output('Summary').to_stdout
+  end
+end
