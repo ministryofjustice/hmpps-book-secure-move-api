787f3c03b74d6e21a56e918cab5b9b3d805f03b5
Thomas Leese <thomas@leese.io>
2021-10-27 08:33:51 +0100

    Add Imports::MissingMoveStartEvents
    This creates move start events for moves which are missing those events
    from the contents of a spreadsheet.

---
+++ app/lib/imports/missing_move_start_events.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+class Imports::MissingMoveStartEvents
+  include Eventable
+
+  def self.call(...)
+    new(...).call
+  end
+
+  def initialize(csv_path:, columns:)
+    @csv_path = csv_path
+    @columns = columns
+    @current_move = nil
+  end
+
+  private_class_method :new
+
+  def call
+    results
+  end
+
+private
+
+  attr_reader :csv_path, :columns
+
+  def results
+    @results ||= records.each_with_object(Imports::Results.new) do |record, results|
+      move = Move.find_by(id: record[:move_id])
+      if move.nil?
+        results.record_failure(record)
+        next
+      end
+
+      @current_move = move
+
+      process_event(move, GenericEvent::MoveStart, {
+        attributes: {
+          timestamp: record[:event_timestamp],
+        },
+      })
+
+      @current_move = nil
+
+      results.save(move, record)
+    end
+  end
+
+  def records
+    @records ||= CSV.table(csv_path).map do |record|
+      {
+        move_id: record[columns.fetch(:move_id)],
+        event_timestamp: record[columns.fetch(:event_timestamp)],
+      }
+    end
+  end
+
+  def doorkeeper_application_owner
+    @current_move&.supplier
+  end
+
+  def created_by
+    doorkeeper_application_owner&.name
+  end
+end
