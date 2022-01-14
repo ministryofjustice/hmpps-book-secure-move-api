099179c723cd340c71e1559e25cdc11e48031618
Thomas Leese <thomas@leese.io>
2022-01-14 10:59:37 +0000

    Revert "Expand scope of supplier locations endpoint"

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -11,12 +11,9 @@ module Api
     def locations_from_supplier_moves
       supplier = Supplier.find(params[:supplier_id])
 
-      ids_from_moves = supplier.moves.select(:from_location_id)
-      ids_from_supplier_locations = supplier.supplier_locations.effective_on(Time.zone.today).select(:location_id)
-
-      Location.includes(:suppliers)
-              .where(id: ids_from_moves)
-              .or(Location.where(id: ids_from_supplier_locations))
+      Location.joins(:moves_from)
+              .includes(:suppliers)
+              .where(moves: { supplier_id: supplier.id })
               .order(key: :asc)
               .distinct
     end
