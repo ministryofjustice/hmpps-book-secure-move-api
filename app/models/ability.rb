37318ae43f6ab63d50c3fb9f58564c5c87798738
Thomas Leese <thomas@leese.io>
2022-01-28 09:36:58 +0000

    Allow suppliers to manage moves of other suppliers
    This is only allows if the move is between locations owned by different
    suppliers. For example, if location A is managed by supplier A and
    location B is managed by supplier B, and there is a move booked which
    goes between location A and B, both suppliers should have permission to
    manage this move.
    
    This functionality allows us to support "supplier to supplier" moves
    where the person is handed over to a different supplier during the
    course of the move.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,15 +7,14 @@ class Ability
     return unless application # no permissions
 
     if application.owner
-      # owner is a supplier, suppliers can manage only their own locations
-      # Note: the permission :manage represents any action on object - See https://github.com/CanCanCommunity/cancancan/wiki/defining-abilities
-      can :manage, Move, Move.where(supplier: application.owner) do |move|
-        move.supplier == application.owner
+      can :manage, Move, Move.for_supplier(application.owner) do |move|
+        move.supplier == application.owner ||
+          move.from_location&.suppliers&.include?(application.owner) ||
+          move.to_location&.suppliers&.include?(application.owner)
       end
+
       can :manage, Journey, supplier_id: application.owner_id
     else
-      # temporarily give all permissions to non-supplier accounts so as not to break compatibility with
-      # the frontend application
       can :manage, Move
       can :manage, Journey
     end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -20,6 +20,38 @@ RSpec.describe Ability, type: :model do
     it { is_expected.not_to be_able_to(:manage, Move.new(supplier: another_supplier)) }
   end
 
+  context 'when application is owned by cross-supplier move' do
+    let(:supplier_a) { create :supplier }
+    let(:supplier_b) { create :supplier }
+    let(:move) do
+      location_a = create(:location, suppliers: [supplier_a])
+      location_b = create(:location, suppliers: [supplier_b])
+      create(:move, from_location: location_a, to_location: location_b)
+    end
+
+    shared_examples 'move is accessible' do
+      let(:application) { Doorkeeper::Application.create(name: 'test', owner: supplier) }
+
+      it { is_expected.to be_able_to(:manage, move) }
+
+      it 'finds the move when queried' do
+        expect(Move.accessible_by(ability)).to eq([move])
+      end
+    end
+
+    context 'and as supplier A' do
+      let(:supplier) { supplier_a }
+
+      include_examples 'move is accessible'
+    end
+
+    context 'and as supplier B' do
+      let(:supplier) { supplier_b }
+
+      include_examples 'move is accessible'
+    end
+  end
+
   context 'when application is owned by frontend application' do
     let(:application) { Doorkeeper::Application.create(name: 'test') }
 
