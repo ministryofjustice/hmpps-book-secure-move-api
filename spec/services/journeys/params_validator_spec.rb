d04c61471705d1a9941d9b0469b2d5a63fffd21e
Thomas Leese <thomas@leese.io>
2022-02-01 08:28:16 +0000

    Validate journey data parameter
    We will allow users to pass in the date of a journey when creating or
    updating a journey.

--- spec/services/journeys/params_validator_spec.rb
+++ spec/services/journeys/params_validator_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe Journeys::ParamsValidator do
 
   let(:billable) { false }
   let(:timestamp) { '2020-04-29T22:45:59.000Z' }
+  let(:date) { '2020-04-29' }
+  let(:params) { { attributes: { billable: billable, timestamp: timestamp, date: date } } }
 
   shared_examples('it validates required billable') do
     context 'when invalid' do
@@ -68,23 +70,53 @@ RSpec.describe Journeys::ParamsValidator do
     end
   end
 
+  shared_examples('it validates optional date') do
+    context 'when blank' do
+      let(:date) { '' }
+
+      it { is_expected.to be_valid(validation_context) }
+    end
+
+    context 'when nil' do
+      let(:date) { nil }
+
+      it { is_expected.to be_valid(validation_context) }
+    end
+
+    context 'when missing' do
+      before { params[:attributes].delete(:date) }
+
+      it { is_expected.to be_valid(validation_context) }
+    end
+  end
+
+  shared_examples('it validates date format') do
+    context 'when invalid' do
+      let(:date) { 'foo' }
+
+      it { is_expected.not_to be_valid(validation_context) }
+    end
+  end
+
   context 'when creating' do
     let(:validation_context) { :create }
-    let(:params) { { attributes: { billable: billable, timestamp: timestamp } } }
 
     it { is_expected.to be_valid(validation_context) }
 
     it_behaves_like 'it validates required billable'
+    it_behaves_like 'it validates optional date'
     it_behaves_like 'it validates timestamp'
+    it_behaves_like 'it validates date format'
   end
 
   context 'when updating' do
     let(:validation_context) { :update }
-    let(:params) { { attributes: { billable: billable, timestamp: timestamp } } }
 
     it { is_expected.to be_valid(validation_context) }
 
     it_behaves_like 'it validates optional billable'
+    it_behaves_like 'it validates optional date'
     it_behaves_like 'it validates timestamp'
+    it_behaves_like 'it validates date format'
   end
 end
