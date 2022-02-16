fcd219f69b0844b8cbbfdb620eb438235094e791
Thomas Leese <thomas@leese.io>
2022-02-16 10:17:10 +0000

    Refactor GenericEvent::Incident spec
    This makes use of the one-liner syntax and subjects.

--- spec/models/generic_event/incident_spec.rb
+++ spec/models/generic_event/incident_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe GenericEvent::Incident, type: :model do
-  describe '#event_type' do
-    it 'removes GenericEvent namespace when type is present' do
-      event = described_class.new
+  let(:event) { described_class.new }
 
-      expect(event.event_type).to eq 'Incident'
-    end
+  describe '#event_type' do
+    subject(:event_type) { event.event_type }
+
+    it { is_expected.to eq('Incident') }
   end
 
   describe '#event_classification' do
-    it 'returns :incident' do
-      event = described_class.new
+    subject(:event_classification) { event.event_classification }
 
-      expect(event.event_classification).to eq :incident
-    end
+    it { is_expected.to eq(:incident) }
 
-    it 'is automatically assigned on creation' do
-      event = create(:event_person_move_assault, classification: nil)
+    context 'when event has nil classification' do
+      let(:event) { create(:event_person_move_assault, classification: nil) }
 
-      expect(event.classification).to eq 'incident'
+      it { is_expected.to eq(:incident) }
     end
   end
 end
