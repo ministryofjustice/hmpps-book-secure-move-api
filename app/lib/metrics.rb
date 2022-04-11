ce3df359090407afb9a553e842d28820ebebd4f4
Thomas Leese <thomas@leese.io>
2022-04-11 09:41:34 +0100

    Record move count after initialize
    This is to prevent a database issue where the metrics are recorded
    before the database is ready.
    
    Also, I've refactored the metrics class to be lazy.

--- app/lib/metrics.rb
+++ app/lib/metrics.rb
@@ -5,20 +5,10 @@ require 'prometheus/client/data_stores/direct_file_store'
 class Metrics
   include Singleton
 
-  def initialize
+  def configure
     Prometheus::Client.config.data_store = Prometheus::Client::DataStores::DirectFileStore.new(
       dir: Rails.root.join('tmp/prometheus'),
     )
-
-    registry = Prometheus::Client.registry
-
-    @move_count_gauge = registry.gauge(
-      :app_move_count_total,
-      docstring: 'The total count of the number of moves.',
-      store_settings: { aggregation: :max },
-    )
-
-    record_move_count
   end
 
   def record_move_count
@@ -27,5 +17,15 @@ class Metrics
 
 private
 
-  attr_reader :move_count_gauge
+  def registry
+    @registry ||= Prometheus::Client.registry
+  end
+
+  def move_count_gauge
+    @move_count_gauge ||= registry.gauge(
+      :app_move_count_total,
+      docstring: 'The total count of the number of moves.',
+      store_settings: { aggregation: :max },
+    )
+  end
 end
