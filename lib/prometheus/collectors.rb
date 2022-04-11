d6dba189437f040a19668784b491fc513aeac735
Thomas Leese <thomas@leese.io>
2022-04-11 08:55:24 +0100

    Remove unused Prometheus collectors directory

--- lib/prometheus/collectors.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-# NB: Keep this file here as it is executed by the "book-secure-move-metrics" container in each pod
-require File.expand_path('../../config/environment', __dir__) unless defined? Rails
