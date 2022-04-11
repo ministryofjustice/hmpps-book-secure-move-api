5c8f6a8f4f2079a60b86d8ddc484de60a70a9333
Thomas Leese <thomas@leese.io>
2022-04-11 09:16:02 +0100

    Allow recording move count metrics
    This stores the move count in a gauge configured to aggregate to the
    maximum value.

--- app/lib/metrics.rb
+++ app/lib/metrics.rb
@@ -9,5 +9,23 @@ class Metrics
     Prometheus::Client.config.data_store = Prometheus::Client::DataStores::DirectFileStore.new(
       dir: Rails.root.join("tmp/prometheus")
     )
+
+    registry = Prometheus::Client.registry
+
+    @move_count_gauge = registry.gauge(
+      :app_move_count_total,
+      docstring: 'The total count of the number of moves.',
+      store_settings: { aggregation: :max }
+    )
+
+    record_move_count
   end
+
+  def record_move_count
+    move_count_gauge.set(Move.unscoped.count)
+  end
+
+private
+
+  attr_reader :move_count_gauge
 end
--- spec/lib/metrics_spec.rb
+++ spec/lib/metrics_spec.rb
@@ -3,9 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe Metrics do
-  subject(:instance) { Metrics.instance }
+  subject(:instance) { Metrics.send(:new) }
+
+  let(:config) { instance_double(Prometheus::Client::Config) }
+  let(:registry) { instance_double(Prometheus::Client::Registry) }
+  let(:move_count_gauge) { instance_double(Prometheus::Client::Gauge) }
+
+  before do
+    allow(config).to receive(:data_store=)
+    allow(Prometheus::Client).to receive(:config).and_return(config)
+
+    allow(move_count_gauge).to receive(:set)
+    allow(registry).to receive(:gauge).with(:app_move_count_total, anything).and_return(move_count_gauge)
+    allow(Prometheus::Client).to receive(:registry).and_return(registry)
+  end
 
   it 'sets the data store' do
-    expect(Prometheus::Client.config.data_store).to be_instance_of(Prometheus::Client::DataStores::DirectFileStore)
+    expect(config).to receive(:data_store=).with(instance_of(Prometheus::Client::DataStores::DirectFileStore))
+    instance
+  end
+
+  it 'sets the move count' do
+    expect(move_count_gauge).to receive(:set)
+    instance
+  end
+
+  describe '#record_move_count' do
+    it 'sets the move count' do
+      expect(move_count_gauge).to receive(:set)
+      instance.record_move_count
+    end
   end
 end
