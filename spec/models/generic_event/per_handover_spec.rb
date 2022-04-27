47f6585fab134ed5ad27e860faa77eb7a4c7fe7d
Thomas Leese <thomas@leese.io>
2022-04-27 10:56:38 +0100

    Require PerHandover event to have a location
    As we anticipate there will be a need to have multiple PER handover
    events during a lockout, we should store the location of where the
    handover occurred.

--- spec/models/generic_event/per_handover_spec.rb
+++ spec/models/generic_event/per_handover_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe GenericEvent::PerHandover do
   subject(:generic_event) { build(:event_per_handover) }
 
   it { is_expected.to validate_inclusion_of(:eventable_type).in_array(%w[PersonEscortRecord]) }
+
+  it_behaves_like 'an event with relationships', location_id: :locations
+  it_behaves_like 'an event requiring a location', :location_id
+  it_behaves_like 'an event with a location in the feed', :location_id
 end
