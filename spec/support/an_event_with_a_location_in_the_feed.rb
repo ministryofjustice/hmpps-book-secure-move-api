b78ffa9c053198ddc90f423e8a33796f617f4aae
Thomas Leese <thomas@leese.io>
2022-04-28 08:56:01 +0100

    Handle optional locations in event feed
    Some events, notable the PerHandover event, will have an optional
    location. This is currently not supported when it tries to export the
    feed for the events, and so we get an exception raised.

--- spec/support/an_event_with_a_location_in_the_feed.rb
+++ spec/support/an_event_with_a_location_in_the_feed.rb
@@ -2,31 +2,48 @@
 
 RSpec.shared_examples 'an event with a location in the feed' do |location_id_key|
   describe '#for_feed' do
-    before do
-      generic_event.details[location_id_key] = location.id
-    end
+    subject(:for_feed) { generic_event.for_feed }
 
-    let(:location) { create(:location) }
+    before { generic_event.details[location_id_key] = location&.id }
 
-    let(:expected_json) do
-      location_key = location_id_key.to_s.sub('_id', '')
-      location_type_key = "#{location_key}_type"
+    let(:location_key) { location_id_key.to_s.sub('_id', '') }
+    let(:location_type_key) { "#{location_key}_type" }
 
+    let(:common_expected_json) do
       {
         'id' => generic_event.id,
         'type' => generic_event.type.sub('GenericEvent::', ''),
         'notes' => 'Flibble',
         'eventable_id' => generic_event.eventable_id,
         'eventable_type' => generic_event.eventable_type,
-        'details' => {
-          location_key => location.nomis_agency_id,
-          location_type_key => location.location_type,
-        },
       }
     end
 
-    it 'generates a feed document' do
-      expect(generic_event.for_feed).to include_json(expected_json)
+    context 'when location is present' do
+      let(:location) { create(:location) }
+
+      let(:expected_json) do
+        common_expected_json.merge({
+          'details' => {
+            location_key => location.nomis_agency_id,
+            location_type_key => location.location_type,
+          },
+        })
+      end
+
+      it { is_expected.to include_json(expected_json) }
+    end
+
+    context 'when location is nil' do
+      let(:location) { nil }
+
+      let(:expected_json) do
+        common_expected_json.merge({
+          'details' => { location_key => nil },
+        })
+      end
+
+      it { is_expected.to include_json(expected_json) }
     end
   end
 end
