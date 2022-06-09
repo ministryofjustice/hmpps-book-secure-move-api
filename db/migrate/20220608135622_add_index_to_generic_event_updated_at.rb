2da3a336cd524c4e51e975e660741bf9b1d67c94
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2022-06-09 13:07:57 +0100

    P4-3715 changing migration for updated_at on generic_events to only create if it doesn't exist.

--- db/migrate/20220608135622_add_index_to_generic_event_updated_at.rb
+++ db/migrate/20220608135622_add_index_to_generic_event_updated_at.rb
@@ -2,6 +2,6 @@ class AddIndexToGenericEventUpdatedAt < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def change
-    add_index :generic_events, :updated_at, algorithm: :concurrently
+    add_index :generic_events, :updated_at, algorithm: :concurrently, if_not_exists: true
   end
 end
