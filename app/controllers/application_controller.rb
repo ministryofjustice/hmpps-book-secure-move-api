56c81229cbb5c4ffe2505a6ed16eb6188f15b094
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-03-09 16:26:54 +0000

    P4-4100 add request id to payload in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def append_info_to_payload(payload)
+    super
+    payload[:request_id] = request.request_id
+  end
 end
