6185c42daea656fb203dbcc6292edb43e51b66cb
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2023-03-23 17:35:48 +0000

    P4-4102 Delete null byte

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -324,7 +324,7 @@ private
       controller_name: controller_name,
       path: request.path,
       params: request.query_parameters,
-      body: request.body.string,
+      body: request.body.string.delete('\u0000'),
     )
   end
 end
