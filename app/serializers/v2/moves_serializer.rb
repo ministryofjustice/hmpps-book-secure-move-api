59b7c460e657b82875d05ff14b92ec51e517398d
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2023-05-09 15:22:40 +0100

    P4-3894 Include timeline_events on move list endpoint

--- app/serializers/v2/moves_serializer.rb
+++ app/serializers/v2/moves_serializer.rb
@@ -51,6 +51,7 @@ module V2
       prison_transfer_reason
       supplier
       important_events
+      timeline_events
     ].freeze
 
     belongs_to :from_location, serializer: ::LocationSerializer
