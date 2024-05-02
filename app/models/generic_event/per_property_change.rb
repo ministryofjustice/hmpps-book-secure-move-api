2fc80256fa560af2fd5e77b129f2bfaeab68db81
Josh Jones <josh.jones4@digital.justice.gov.uk>
2024-05-02 12:37:24 +0100

    Made changes to the per_property_change event to fix issue reported by Geoamey where location_id was misplaced.

--- app/models/generic_event/per_property_change.rb
+++ app/models/generic_event/per_property_change.rb
@@ -1,5 +1,12 @@
 class GenericEvent
   class PerPropertyChange < GenericEvent
+    LOCATION_ATTRIBUTE_KEY = :location_id
+
+    details_attributes :vehicle_reg, :supplier_personnel_number, :police_personnel_number
+    relationship_attributes location_id: :locations
     eventable_types 'PersonEscortRecord'
+
+    include LocationValidations
+    include PersonnelNumberValidations
+    include LocationFeed
   end
-end
