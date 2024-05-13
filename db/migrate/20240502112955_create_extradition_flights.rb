98ffd6086b7420d0428f09e654ceb85a9da50286
Daniel Benton <daniel.benton@digital.justice.gov.uk>
2024-05-13 11:54:28 +0100

    MAP-1038 rename FlightDetails -> ExtradtionFlight

--- db/migrate/20240502112955_create_extradition_flights.rb
+++ db/migrate/20240502112955_create_extradition_flights.rb
@@ -2,7 +2,7 @@ class CreateExtraditionFlights < ActiveRecord::Migration[7.0]
   def change
     create_table :extradition_flights, id: :uuid do |t|
       t.string :flight_number, null: false
-      t.string :flight_time, null: false
+      t.datetime :flight_time, null: false
       t.references :move, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
