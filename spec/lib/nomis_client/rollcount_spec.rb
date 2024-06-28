58288958567c8ac11e8a0ead1a7ed781875d0d9d
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-06-28 09:16:31 +0100

    Map 1363 move basm api to use new roll count (#2280)

--- spec/lib/nomis_client/rollcount_spec.rb
+++ spec/lib/nomis_client/rollcount_spec.rb
@@ -4,28 +4,31 @@ require 'rails_helper'
 
 RSpec.describe NomisClient::Rollcount, with_nomis_client_authentication: true do
   describe '.get' do
-    let(:response) { described_class.get(agency_id:, unassigned:) }
+    let(:response) { described_class.get(agency_id:) }
     let(:agency_id) { 'PRI' }
-    let(:unassigned) { true }
 
     context 'when a resource is found' do
       let(:response_status) { 200 }
       let(:response_body) { file_fixture('nomis/get_rollcount_200.json').read }
 
-      it 'has the correct number of results' do
-        expect(response.count).to be 5
-      end
-
       it 'returns the correct data' do
-        expect(response.first.symbolize_keys).to eq({
-          availablePhysical: 367,
-          bedsInUse: 33,
-          currentlyInCell: 2,
-          currentlyOut: 31,
-          livingUnitDesc: 'COURT',
-          livingUnitId: 8839,
-          maximumCapacity: 400,
-          outOfOrder: 0,
+        expect(response.symbolize_keys).to eq({
+          prisonId: 'PRI',
+          numUnlockRollToday: 0,
+          numCurrentPopulation: 97,
+          numArrivedToday: 0,
+          numInReception: 0,
+          numStillToArrive: 0,
+          numOutToday: 0,
+          numNoCellAllocated: 0,
+          totals: {
+            'bedsInUse' => 53,
+            'currentlyInCell' => 7,
+            'currentlyOut' => 44,
+            'workingCapacity' => 0,
+            'netVacancies' => 0,
+            'outOfOrder' => 0,
+          },
         })
       end
     end
