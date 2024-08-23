1e86bd4ce4441760ef6749c9b531c453b1b788e1
danbenton-mojdt <113102670+danbenton-mojdt@users.noreply.github.com>
2024-08-23 15:37:38 +0100

    Script release commands (#2308)
    * Script release commands
    
    * Make release script check we're on main and in line with origin

---
+++ release.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+git fetch
+currentBranch=$(git branch --show-current)
+
+if [ "${currentBranch}" != 'main' ]
+   then
+   echo "Not on main, exiting"
+   exit 1
+fi
+
+ diff=$(git diff origin/main --name-only)
+ if [ "${diff}" != '' ]
+    then
+    echo "Non-zero diff with origin, exiting"
+    exit 2
+fi
+
+
+# Get the latest tag, so we know what the next one will be
+currentVersion=$(git describe --abbrev=0)
+
+DATE=$(date "+%Y-%m-%d")
+# Replace vx.x.x with the next version
+read -p "Enter next version number (current: ${currentVersion}): " NEXT_VERSION
+
+if [ "${NEXT_VERSION:0:1}" != 'v' ]
+  then
+  NEXT_VERSION="v${NEXT_VERSION}"
+fi
+
+echo
+echo
+echo "=========="
+echo
+echo "Tagging version ${NEXT_VERSION} with message \"Deploying on ${DATE}\""
+echo
+
+read -p "Press enter to continue... (Ctrl+C to cancel)" go
+
+git tag -a ${NEXT_VERSION} -m "Deploying on ${DATE}"
+git push origin $NEXT_VERSION
+
+read -p Generate changelog? Enter to contine or Ctrl+C to quit
+
+git checkout -b changelog-$NEXT_VERSION
+bundle
+rake changelog
+git add CHANGELOG.md
+git commit -m "Generated changelog for $NEXT_VERSION"
+git push --set-upstream origin changelog-$NEXT_VERSION
