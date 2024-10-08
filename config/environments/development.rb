0c6ebc5613f5f9058eb329c03cf6a5c9184f7958
Toby Privett <toby@snaplab.co.uk>
2024-10-08 11:30:56 +0100

    Add `bullet` gem to identify n+1 queries
    - Configured to log to:
    `logs/bullet.log`
    
    https://github.com/flyerhzm/bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,12 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.add_footer    = true
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
