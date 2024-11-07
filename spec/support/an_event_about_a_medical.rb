3bff5f1d4ea6ca5a3e486325e98d0c5c66eed313
Toby Privett <toby@snaplab.co.uk>
2024-11-07 10:13:30 +0000

    PerMedical attributes can be optional
    Applies to:
    - details#advised_at
    - details#advised_by

--- spec/support/an_event_about_a_medical.rb
+++ spec/support/an_event_about_a_medical.rb
@@ -5,9 +5,6 @@ RSpec.shared_examples 'an event about a medical' do
   it_behaves_like 'an event with a supplier personnel number'
   it_behaves_like 'an event with a location in the feed', :location_id
 
-  it { is_expected.to validate_presence_of(:advised_at) }
-  it { is_expected.to validate_presence_of(:advised_by) }
-
   it { is_expected.to validate_inclusion_of(:eventable_type).in_array(%w[PersonEscortRecord]) }
 
   it { is_expected.to validate_presence_of(:supplier_personnel_number) }
@@ -28,10 +25,12 @@ RSpec.shared_examples 'an event about a medical' do
     it { is_expected.not_to be_valid }
   end
 
-  context 'when the treated_at and treated_by are nil' do
+  context 'when the advised_at, advised_by, treated_at and treated_by are nil' do
     before do
-      generic_event.treated_by = nil
+      generic_event.advised_at = nil
+      generic_event.advised_by = nil
       generic_event.treated_at = nil
+      generic_event.treated_by = nil
     end
 
     it { is_expected.to be_valid }
