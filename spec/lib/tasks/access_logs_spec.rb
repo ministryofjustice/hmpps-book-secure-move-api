f4b8d39c498c5c817aae3f91ae446b96a46dcf92
Toby Privett <toby.privett@digital.justice.gov.uk>
2024-11-15 12:19:02 +0000

    MAP-1781 Cleanup Access Logs more than 3 months old (#2408)
    * Cleanup Access Logs more than 3 months old
    
    * Fix rubocop issues
    
    * Prefer a cutoff date of 6 months
    
    * Add a spec to test the access_logs:cleanup task
    
    * Prefer cutoff date of 1 year
    
    * `reenable` lets us run a rake task multiple times in a spec

---
+++ spec/lib/tasks/access_logs_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Rake::Task['access_logs:cleanup'] do
+  before do
+    # 2 access_logs to retain
+    create(:access_log, timestamp: 1.month.ago)
+    create(:access_log, timestamp: 7.months.ago)
+
+    # 3 access_logs to delete
+    create(:access_log, timestamp: 13.months.ago)
+    create(:access_log, timestamp: 14.months.ago)
+    create(:access_log, timestamp: 15.months.ago)
+
+    allow($stdout).to receive(:puts)
+
+    described_class.reenable
+    described_class.invoke
+  end
+
+  it 'cleans up the access_logs' do
+    expect(AccessLog.count).to eq(2)
+  end
+
+  it 'writes to stdout' do
+    expect($stdout)
+      .to have_received(:puts)
+      .with('Cleaning up 3 access_logs in 1 iterations')
+  end
+end
