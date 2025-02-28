da52afbd88ca8f3513eff57e0739e26e2f54687a
Toby Privett <toby@snaplab.co.uk>
2025-02-28 09:56:28 +0000

    Add Notify template name to the gov_uk initializer
    https://dsdmoj.atlassian.net/browse/MAP-2128

--- config/initializers/gov_uk_notify.rb
+++ config/initializers/gov_uk_notify.rb
@@ -1,7 +1,7 @@
 # We only send emails if GOVUK_NOTIFY_ENABLED is true - and should generally only be enabled on the Sidekiq pod
 # Note that the app pod and metrics containers should not send GovUk Notify emails
 if ENV['GOVUK_NOTIFY_ENABLED'] =~ /true/i
-  %w[GOVUK_NOTIFY_API_KEY GOVUK_NOTIFY_MOVE_TEMPLATE_ID GOVUK_NOTIFY_PER_TEMPLATE_ID].each do |name|
+  %w[GOVUK_NOTIFY_API_KEY GOVUK_NOTIFY_MOVE_TEMPLATE_ID GOVUK_NOTIFY_MOVE_REJECT_TEMPLATE_ID GOVUK_NOTIFY_PER_TEMPLATE_ID].each do |name|
     if ENV[name].blank?
       Rails.logger.warn("#{name} env var is not set; emails cannot be sent")
       Sentry.capture_message("#{name} env var is not set; emails cannot be sent", level: 'warning')
