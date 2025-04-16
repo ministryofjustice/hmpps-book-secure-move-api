c887510d055b92b594dac75b61f8a63ad57f6b58
Toby Privett <toby@snaplab.co.uk>
2025-04-16 15:28:56 +0100

    Add date_changed_reason field for moves
    - Add date_changed_reason column to moves table
    - Include date_changed_reason in MoveDateChanged events
    - Update controllers and tests to handle the new field
    
    https://dsdmoj.atlassian.net/browse/MAP-2301

--- app/models/generic_event/move_date_changed.rb
+++ app/models/generic_event/move_date_changed.rb
@@ -1,6 +1,7 @@
 class GenericEvent
   class MoveDateChanged < GenericEvent
-    details_attributes :date
+    details_attributes :date, :date_changed_reason
+
     eventable_types 'Move'
 
     validates :date, presence: true, iso_date_time: true
