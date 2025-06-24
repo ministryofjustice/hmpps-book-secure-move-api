e73ed80612d3418dee483fafc90ea30eb961fae0
Toby Privett <toby@snaplab.co.uk>
2025-06-24 10:27:00 +0100

    Fix ImportPeople to use Alerts API v1 per-person calls

--- app/services/moves/import_people.rb
+++ app/services/moves/import_people.rb
@@ -16,21 +16,21 @@ module Moves
 
     def import_people
       people = NomisClient::People.get(prison_numbers).index_by { |p| p.fetch(:prison_number) }
-      alerts = AlertsApiClient::Alerts.get(prison_numbers).group_by { |p| p.fetch(:offender_no) }
       personal_care_needs = NomisClient::PersonalCareNeeds
                             .get(nomis_offender_numbers: prison_numbers)
                             .group_by { |p| p.fetch(:offender_no) }
 
       new_person_count = 0
       changed_profile_count = 0
-      people.each do |offender_no, person_data|
-        profile = People::BuildPersonAndProfileV1.new(person_data).call
 
+      people.each do |prison_number, person_data|
+        profile = People::BuildPersonAndProfileV1.new(person_data).call
         new_person_count += 1 if profile.new_record?
 
-        Alerts::Importer.new(profile:, alerts: alerts.fetch(offender_no, [])).call
+        alerts = AlertsApiClient::Alerts.get(prison_number)
+        Alerts::Importer.new(profile:, alerts:).call
 
-        PersonalCareNeeds::Importer.new(profile:, personal_care_needs: personal_care_needs.fetch(offender_no, [])).call
+        PersonalCareNeeds::Importer.new(profile:, personal_care_needs: personal_care_needs.fetch(prison_number, [])).call
 
         next unless profile.changed? || profile.person.changed?
 
