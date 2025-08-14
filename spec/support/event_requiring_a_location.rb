98379c1a8d14dd586518fefbc09bce33a4367bfa
Toby Privett <toby@snaplab.co.uk>
2025-08-14 10:16:33 +0100

    Fix spec for Rails ActiveRecord error message format change
    Rails now uses inspect for ID values in RecordNotFound messages.
    
    Update matcher to use regex.

--- spec/support/event_requiring_a_location.rb
+++ spec/support/event_requiring_a_location.rb
@@ -1,7 +1,6 @@
 RSpec.shared_examples 'an event requiring a location' do |location_key|
   it "validates #{location_key} existing in the database" do
     generic_event.public_send("#{location_key}=", 'flibble')
-
-    expect { generic_event.valid? }.to raise_error(ActiveRecord::RecordNotFound, "Couldn't find Location with 'id'=flibble")
+    expect { generic_event.valid? }.to raise_error(ActiveRecord::RecordNotFound, /Couldn't find Location with 'id'=.*flibble/)
   end
 end
