dc39fe1b07fe602cd162b9b32a4569ce4cce66d6
Dan Benton <daniel.benton@justice.gov.uk>
2026-01-20 14:50:44 +0000

    MAPB-89 Add section 46 to moves

--- db/migrate/20260120143730_add_section_46_to_moves.rb
+++ db/migrate/20260120143730_add_section_46_to_moves.rb
@@ -1,4 +1,4 @@
-class AddSection46PaceToMoves < ActiveRecord::Migration[8.0]
+class AddSectionFortySixPaceToMoves < ActiveRecord::Migration[8.0]
   def change
     add_column :moves, :section_46, :boolean
   end
