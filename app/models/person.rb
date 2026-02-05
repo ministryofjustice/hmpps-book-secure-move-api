70e5f76e43ce6feac71bb9cb3b8852bb76e3ac53
Dan Benton <daniel.benton@justice.gov.uk>
2026-02-05 17:38:02 +0000

    MAPB-219 Lint

--- app/models/person.rb
+++ app/models/person.rb
@@ -33,7 +33,7 @@ class Person < VersionedModel
   scope :ordered_by_name, ->(direction) { order('last_name' => direction, 'first_names' => direction) }
   scope :search_by_last_name, ->(search) { select(:id).where('last_name ILIKE :search', search: "%#{search}%") }
 
-  scope :filter_by_pnc_canonical, ->(raw_input) do
+  scope :filter_by_pnc_canonical, ->(raw_input) {
     variants = PncNormalizer.variants(raw_input)
     return none if variants.empty?
 
@@ -45,7 +45,7 @@ class Person < VersionedModel
         regexp_replace(UPPER(people.police_national_computer), '[^0-9A-Z]', '', 'g')
       ) IN (?)
     SQL
-  end
+  }
 
   validates :last_name, presence: true
   validates :first_names, presence: true
