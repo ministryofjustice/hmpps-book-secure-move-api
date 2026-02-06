c1a6d0f8e63f5a43ddb1d4b00c65f8fef2381df7
Dan Benton <daniel.benton@justice.gov.uk>
2026-02-06 09:25:19 +0000

    MAPB-219 Add PNC normalizer spec

---
+++ spec/services/people/pnc_normalizer_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PncNormalizer do
+  it "handles 2-digit year and unpadded number" do
+    variants = described_class.variants("14/12018R")
+    expect(variants).to include("14/012018R")
+  end
+
+  it "handles 4-digit year and unpadded" do
+    variants = described_class.variants("2014-12018r")
+    expect(variants).to include("2014/012018R")
+  end
+
+  it "matches 2-digit search against 4-digit DB" do
+    db = create(:person, police_national_computer: "2014/120018R")
+    expect(Person.filter_by_pnc_canonical("14/120018R")).to include(db)
+  end
+
+  it "matches unpadded search against padded DB" do
+    db = create(:person, police_national_computer: "2014/0120018R")
+    expect(Person.filter_by_pnc_canonical("2014/120018R")).to include(db)
+  end
+end
