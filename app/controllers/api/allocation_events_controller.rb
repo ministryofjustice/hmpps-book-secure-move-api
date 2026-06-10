a424f14a2d084c3d3deca725d03dffbdf09822fe
Dan Benton <daniel.benton@justice.gov.uk>
2026-06-10 12:05:00 +0100

    Keep cancelling allocation moves with OTHER for now

--- app/controllers/api/allocation_events_controller.rb
+++ app/controllers/api/allocation_events_controller.rb
@@ -31,7 +31,7 @@ module Api
     end
 
     def cancel_move_params
-      cancel_params.deep_merge(attributes: { cancellation_reason: Move::CANCELLATION_REASON_ALLOCATION_CANCELLED })
+      cancel_params.deep_merge(attributes: { cancellation_reason: Move::CANCELLATION_REASON_OTHER })
     end
 
     def cancellation_details
