93f80e17cdd8e1a12cf47a7bce57c55f6f5d8c9f
David Lonsdale <david.lonsdale@justice.gov.uk>
2026-09-22 16:41:51 +0100

    Addressing copilot feedback

--- spec/serializers/generic_event_serializer_spec.rb
+++ spec/serializers/generic_event_serializer_spec.rb
@@ -100,4 +100,22 @@ RSpec.describe GenericEventSerializer do
       expect(result).to eq(expected_json)
     end
   end
+
+  context 'when the details include summary fields' do
+    let(:event) { create(:event_per_prisoner_welfare) }
+    let(:adapter_options) { {} }
+
+    it 'sets has_details to true' do
+      expect(result.dig(:data, :attributes, :has_details)).to be(true)
+    end
+  end
+
+  context 'when the details do not include any summary fields' do
+    let(:event) { create(:event_per_court_ready_in_custody) }
+    let(:adapter_options) { {} }
+
+    it 'sets has_details to false' do
+      expect(result.dig(:data, :attributes, :has_details)).to be(false)
+    end
+  end
 end
